6e1bae7ec0b9deccbbce098346208e07e1214da0
john-hill <john.hill@sagebase.org>
2012-03-24 22:06:25 +0000

    Added annotations to the property editor and changed how the Register.json is loaded in the web client.

--- client/web/src/test/java/org/sagebionetworks/web/unitclient/widget/licenseddownloader/LicensedDownloaderTest.java
+++ client/web/src/test/java/org/sagebionetworks/web/unitclient/widget/licenseddownloader/LicensedDownloaderTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +25,8 @@ import org.sagebionetworks.repo.model.LocationData;
 import org.sagebionetworks.repo.model.Locationable;
 import org.sagebionetworks.repo.model.Study;
 import org.sagebionetworks.schema.adapter.JSONObjectAdapter;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
+import org.sagebionetworks.schema.adapter.org.json.AdapterFactoryImpl;
 import org.sagebionetworks.schema.adapter.org.json.JSONObjectAdapterImpl;
 import org.sagebionetworks.web.client.EntityTypeProvider;
 import org.sagebionetworks.web.client.GlobalApplicationState;
@@ -39,6 +42,7 @@ import org.sagebionetworks.web.shared.LicenseAgreement;
 import org.sagebionetworks.web.shared.exceptions.RestServiceException;
 import org.sagebionetworks.web.shared.users.UserData;
 import org.sagebionetworks.web.test.helper.AsyncMockStubber;
+import org.sagebionetworks.web.unitclient.RegisterConstantsStub;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
@@ -67,7 +71,7 @@ public class LicensedDownloaderTest {
 	
 	@SuppressWarnings("unchecked")
 	@Before
-	public void setup(){		
+	public void setup() throws UnsupportedEncodingException, JSONObjectAdapterException{		
 		mockView = Mockito.mock(LicensedDownloaderView.class);		
 		mockAuthenticationController = mock(AuthenticationController.class);
 		mockGlobalApplicationState = mock(GlobalApplicationState.class);
@@ -80,7 +84,7 @@ public class LicensedDownloaderTest {
 		// create entity type provider
 		String registryJson = SynapseClientImpl.getEntityTypeRegistryJson();
 		AsyncMockStubber.callSuccessWith(registryJson).when(mockSynapseClient).getEntityTypeRegistryJSON(any(AsyncCallback.class));
-		entityTypeProvider = new EntityTypeProvider(mockSynapseClient, new JSONObjectAdapterImpl());		
+		entityTypeProvider = new EntityTypeProvider(new RegisterConstantsStub(), new AdapterFactoryImpl());		
 
 		
 		licensedDownloader = new LicensedDownloader(mockView, mockAuthenticationController, mockGlobalApplicationState,
@@ -146,107 +150,6 @@ public class LicensedDownloaderTest {
 	}
 	
 
-//	@SuppressWarnings("unchecked")
-//	@Test
-//	public void testFindEulaId() throws Exception {
-//		// test for no eula in parent path
-//		resetMocks();
-//		configureTestFindEulaIdMocks();	
-//		licensedDownloader.findEulaId(entity, mockStringCallback);
-//		verify(mockStringCallback).onSuccess(null);
-//
-//		// test failure for 1st parent (Study) service retrieval
-//		resetMocks();
-//		configureTestFindEulaIdMocks();
-//		AsyncMockStubber.callFailureWith(new Throwable()).when(mockSynapseClient).getEntity(eq(parentEntity.getId()), any(AsyncCallback.class)); 				
-//		licensedDownloader.findEulaId(entity, mockStringCallback);
-//		verify(mockStringCallback).onFailure(any(Throwable.class));
-//		
-//		// test failure for 1st parent (Study) model creation
-//		resetMocks();
-//		configureTestFindEulaIdMocks();
-//		when(mockNodeModelCreator.createEntity(StudyEntityWrapper)).thenReturn(null); 				
-//		licensedDownloader.findEulaId(entity, mockStringCallback);
-//		verify(mockStringCallback).onFailure(any(Throwable.class));
-//
-//		// test finding eula Id in 1st parent (Study)
-//		resetMocks();
-//		configureTestFindEulaIdMocks();
-//		String eulaId = "eulaId";	
-//		licensedDownloader.findEulaId(entity, mockStringCallback);
-//		verify(mockStringCallback).onSuccess(eulaId);
-//		
-//	}
-//
-//	
-//	@SuppressWarnings("unchecked")
-//	@Test
-//	public void testLoadLicenseAgreement() throws Exception {
-//		LicenseAgreement licenseAgreement = new LicenseAgreement(eula1.getAgreement(), null, eula1.getId());
-//		
-//		// null model
-//		resetMocks();
-//		licensedDownloader.loadLicenseAgreement(null);		
-//		
-//		// Failure to get parent via synapse client		
-//		resetMocks();
-//		configureTestLoadMocks();
-//		AsyncMockStubber.callFailureWith(new Throwable("error message")).when(mockSynapseClient).getEntity(eq(parentEntity.getId()), any(AsyncCallback.class)); // fail for get parent
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).showDownloadFailure();
-//
-//		// Success of parental get but failure in Entity creation 
-//		resetMocks();
-//		configureTestLoadMocks();
-//		when(mockNodeModelCreator.createEntity(any(EntityWrapper.class))).thenReturn(null); // failed entity creation
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).showDownloadFailure();
-//
-//		// Study with null EULA 
-//		resetMocks();
-//		configureTestLoadMocks();
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).setLicenceAcceptanceRequired(false);		
-//		
-//		// model creation of EULA fails
-//		resetMocks();
-//		configureTestLoadMocks(); 
-//		when(mockNodeModelCreator.createEntity(anyString(), eq(Eula.class))).thenReturn(null); // null EULA object
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).showDownloadFailure();
-//	
-//		// License Acceptance failure
-//		resetMocks();
-//		configureTestLoadMocks();
-//		AsyncMockStubber.callFailureWith(new Throwable("error message")).when(mockLicenseService).hasAccepted(eq(user1.getEmail()), eq(eula1.getId()), eq(parentEntity.getId()), any(AsyncCallback.class)); // has not accepted
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).showDownloadFailure();
-//
-//		// Check all okay but with user not logged in and trying to show download
-//		resetMocks();
-//		configureTestLoadMocks();
-//		when(mockAuthenticationController.getLoggedInUser()).thenReturn(null); // null user
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).setUnauthorizedDownloads();		
-//		
-//		// License Accepted == False
-//		resetMocks();
-//		configureTestLoadMocks();
-//		AsyncMockStubber.callSuccessWith(false).when(mockLicenseService).hasAccepted(eq(user1.getEmail()), eq(eula1.getId()), eq(parentEntity.getId()), any(AsyncCallback.class)); // accepted == false
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).setLicenceAcceptanceRequired(true);
-//		verify(mockView).setLicenseHtml(licenseAgreement.getLicenseHtml());
-//
-//		
-//		// License Accepted == True
-//		resetMocks();
-//		configureTestLoadMocks();
-//		AsyncMockStubber.callSuccessWith(true).when(mockLicenseService).hasAccepted(eq(user1.getEmail()), eq(eula1.getId()), eq(parentEntity.getId()), any(AsyncCallback.class)); // accepted == true
-//		licensedDownloader.loadLicenseAgreement(entity);
-//		verify(mockView).setLicenceAcceptanceRequired(false);
-//		verify(mockView).setLicenseHtml(licenseAgreement.getLicenseHtml());
-//		
-//	}
 	
 	@SuppressWarnings("unchecked")
 	@Test
