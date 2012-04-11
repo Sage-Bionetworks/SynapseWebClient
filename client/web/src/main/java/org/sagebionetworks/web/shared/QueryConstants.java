c253192accb4af8bf228742431359431bf9c6e7c
dburdick <dburdick@gmail.com>
2012-04-11 18:18:41 +0000

    Query column fixes for new Entity types (datas, Robject, etc) PLFM-1192. Also added ordering to child type display

--- client/web/src/main/java/org/sagebionetworks/web/shared/QueryConstants.java
+++ client/web/src/main/java/org/sagebionetworks/web/shared/QueryConstants.java
@@ -7,7 +7,7 @@ public class QueryConstants {
 	 * 
 	 */
 	public enum ObjectType {
-		dataset(), layer(), project(), agreement(), eula(), analysis(), step(), code(), link(), entity();
+		dataset(), layer(), project(), agreement(), eula(), analysis(), step(), code(), link(), entity(), expressiondata(), genotypedata(), phenotypedata(), robject();
 	}
 
 	/**
