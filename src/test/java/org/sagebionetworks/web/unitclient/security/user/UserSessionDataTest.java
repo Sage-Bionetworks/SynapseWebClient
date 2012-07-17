b628dfa8cd90a64a5ccae6f1d1e4882a12b5bc18
jay-hodgson <jay.hodgson@sagebase.org>
2012-07-17 10:10:12 -0700

    SWC-107: move edit button to the right, and include UserSessionData unit test

---
+++ src/test/java/org/sagebionetworks/web/unitclient/security/user/UserSessionDataTest.java
@@ -0,0 +1,36 @@
+package org.sagebionetworks.web.unitclient.security.user;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.sagebionetworks.repo.model.UserProfile;
+import org.sagebionetworks.repo.model.UserSessionData;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
+import org.sagebionetworks.schema.adapter.org.json.EntityFactory;
+
+public class UserSessionDataTest {
+	
+	@Test
+	public void testToCookie(){
+		UserSessionData user = new UserSessionData();
+		UserProfile profile = new UserProfile();
+		profile.setDisplayName("Display Name");
+		profile.setOwnerId("1");
+		profile.setUserName("email@email.com");
+		user.setProfile(profile);
+		user.setSessionToken("token");
+		user.setIsSSO(false);
+		try {
+			String cookieString = EntityFactory.createJSONStringForEntity(user);
+			System.out.println(cookieString);
+			
+			UserSessionData copy = EntityFactory.createEntityFromJSONString(cookieString, UserSessionData.class);
+			assertNotNull(copy);
+			assertEquals(user, copy);
+		} catch (JSONObjectAdapterException e) {
+			assertNotNull("UserSessionData test failure due to JSON adapter exception. " + e, null);
+		}
+	}
+
+}
