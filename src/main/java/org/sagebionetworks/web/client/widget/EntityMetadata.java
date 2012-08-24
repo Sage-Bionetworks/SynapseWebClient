a948a25e1024f82b90e134daba781381a62dc909
Geoff Shannon <earthlingzephyr@gmail.com>
2012-08-24 14:47:54 -0700

    Start to expose versioning in UI

---
+++ src/main/java/org/sagebionetworks/web/client/widget/EntityMetadata.java
@@ -0,0 +1,73 @@
+package org.sagebionetworks.web.client.widget;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.dom.client.Text;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class EntityMetadata extends Composite {
+	interface EntityMetadataUiBinder extends UiBinder<Widget, EntityMetadata> {
+	}
+
+	private static EntityMetadataUiBinder uiBinder = GWT
+			.create(EntityMetadataUiBinder.class);
+
+	@UiField
+	HTMLPanel panel;
+
+	@UiField
+	SpanElement createName;
+	@UiField
+	SpanElement createDate;
+	@UiField
+	SpanElement modifyName;
+	@UiField
+	SpanElement modifyDate;
+	@UiField
+	SpanElement version;
+
+	@UiField
+	SpanElement previousVersions;
+
+	@UiField
+	Anchor allVersions;
+
+	public EntityMetadata() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	public void setCreateName(String text) {
+		createName.setInnerText(text);
+	}
+
+	public void setCreateDate(String text) {
+		createDate.setInnerText(text);
+	}
+
+	public void setModifyName(String text) {
+		modifyName.setInnerText(text);
+	}
+
+	public void setModifyDate(String text) {
+		modifyDate.setInnerText(text);
+	}
+
+	public void setVersionInfo(String text) {
+		version.setInnerText(text);
+	}
+
+	public void setAllVersionHref(String href) {
+		allVersions.setHref(href);
+	}
+
+	public void addToPreviousVersions(Widget widget, Text delim) {
+		previousVersions.appendChild(delim);
+		panel.add(widget, previousVersions);
+	}
+
+}
--- src/main/java/org/sagebionetworks/web/client/widget/entity/EntityViewUtils.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/EntityViewUtils.java
@@ -10,16 +10,14 @@ import org.sagebionetworks.web.client.DisplayUtils;
 import org.sagebionetworks.web.client.IconsImageBundle;
 import org.sagebionetworks.web.client.SynapseJSNIUtils;
 import org.sagebionetworks.web.client.model.EntityBundle;
-
-import com.extjs.gxt.ui.client.widget.Html;
+import org.sagebionetworks.web.client.widget.EntityMetadata;
 
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
-import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.InlineHTML;
-import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class EntityViewUtils {
@@ -31,23 +29,23 @@ public class EntityViewUtils {
 
 		SafeHtmlBuilder shb = new SafeHtmlBuilder();
 		shb.appendHtmlConstant("<h2>")
-		.appendHtmlConstant(AbstractImagePrototype.create(DisplayUtils.getSynapseIconForEntity(bundle.getEntity(), DisplayUtils.IconSize.PX24, iconsImageBundle)).getHTML()) 
+		.appendHtmlConstant(AbstractImagePrototype.create(DisplayUtils.getSynapseIconForEntity(bundle.getEntity(), DisplayUtils.IconSize.PX24, iconsImageBundle)).getHTML())
 		.appendHtmlConstant("&nbsp;")
 		.appendEscaped(bundle.getEntity().getName())
 		.appendHtmlConstant("&nbsp;(")
 		.appendEscaped(bundle.getEntity().getId())
-		.appendHtmlConstant(")")		
-		.appendHtmlConstant("</h2>");		
-		lc.add(new HTML(shb.toSafeHtml()));  
-		
+		.appendHtmlConstant(")")
+		.appendHtmlConstant("</h2>");
+		lc.add(new HTML(shb.toSafeHtml()));
+
 	    // Metadata
-	    lc.add(createMetadata(bundle.getEntity()));
+	    lc.add(createMetadata(bundle.getEntity(), iconsImageBundle));
 	    // the headers for description and property
-	   	  
+
 		if(canEdit && readOnly) {
 			HTML roContainer = new HTML("<h4 class=\"colored\"> " + DisplayConstants.READ_ONLY + " " +AbstractImagePrototype.create(iconsImageBundle.help16()).getHTML() + "</h4>");
-			roContainer.setWidth("100px");			
-			
+			roContainer.setWidth("100px");
+
 			Map<String, String> optionsMap = new TreeMap<String, String>();
 			optionsMap.put("title", DisplayConstants.WHY_VERSION_READ_ONLY_MODE);
 			optionsMap.put("data-placement", "right");
@@ -55,7 +53,7 @@ public class EntityViewUtils {
 			lc.add(roContainer);
 		}
 
-	    
+
 	    lc.layout();
 		return lc;
 	}
@@ -69,29 +67,16 @@ public class EntityViewUtils {
 	 * @param entity
 	 * @return
 	 */
-	private static Widget createMetadata(Entity entity) {
-		FlowPanel panel = new FlowPanel();
-		panel.getElement().setAttribute("style", "font-size: 80%;");
-
-		InlineLabel label = new InlineLabel();
-		label.setText("Added by: " + entity.getCreatedBy() + " on: "
-				+ String.valueOf(entity.getCreatedOn()));
-		panel.add(label);
-
-		panel.add(new InlineHTML("<br />"));
-
-		label = new InlineLabel();
-		label.setText("Last updated by: " + entity.getModifiedBy() + " on: "
-				+ String.valueOf(entity.getModifiedOn()));
-		panel.add(label);
+	private static Widget createMetadata(Entity entity, IconsImageBundle iconsImageBundle) {
+		EntityMetadata entityMetadata = new EntityMetadata();
+		entityMetadata.setCreateName(entity.getCreatedBy());
+		entityMetadata.setCreateDate(String.valueOf(entity.getCreatedOn()));
+		entityMetadata.setModifyName(entity.getModifiedBy());
+		entityMetadata.setModifyDate(String.valueOf(entity.getModifiedOn()));
 
 		if (entity instanceof Versionable) {
 			Versionable vb = (Versionable) entity;
-
-			panel.add(new InlineHTML("<br />"));
-			label = new InlineLabel();
 			StringBuilder sb = new StringBuilder();
-			sb.append("Version: ");
 			sb.append(vb.getVersionLabel());
 			sb.append(" (");
 			sb.append(vb.getVersionNumber());
@@ -101,11 +86,14 @@ public class EntityViewUtils {
 				sb.append(" - ");
 				sb.append(vb.getVersionComment());
 			}
-			label.setText(sb.toString());
-			panel.add(label);
-		}
+			entityMetadata.setVersionInfo(sb.toString());
 
-		return panel;
+			Document doc = Document.get();
+			entityMetadata.addToPreviousVersions(new Anchor("3"), doc.createTextNode(""));
+			entityMetadata.addToPreviousVersions(new Anchor("2"), doc.createTextNode(" | "));
+			entityMetadata.addToPreviousVersions(new Anchor("1"), doc.createTextNode(" | "));
+		}
+		return entityMetadata;
 	}
-	
+
 }
