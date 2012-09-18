49430637a304f493ffe1fa704fa0e9d2dc309beb
Jay Hodgson <jay.hodgson@sagebase.org>
2012-09-18 12:24:44 -0700

    SWC-140: simplify My Projects

--- src/main/java/org/sagebionetworks/web/client/widget/entity/browse/MyEntitiesBrowserViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/browse/MyEntitiesBrowserViewImpl.java
@@ -3,7 +3,6 @@ package org.sagebionetworks.web.client.widget.entity.browse;
 import java.util.List;
 
 import org.sagebionetworks.repo.model.EntityHeader;
-import org.sagebionetworks.web.client.DisplayConstants;
 import org.sagebionetworks.web.client.DisplayUtils;
 import org.sagebionetworks.web.client.IconsImageBundle;
 import org.sagebionetworks.web.client.SageImageBundle;
@@ -11,15 +10,8 @@ import org.sagebionetworks.web.client.events.EntitySelectedEvent;
 import org.sagebionetworks.web.client.events.EntitySelectedHandler;
 
 import com.extjs.gxt.ui.client.Style.Scroll;
-import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.Status;
-import com.extjs.gxt.ui.client.widget.button.Button;
-import com.extjs.gxt.ui.client.widget.toolbar.FillToolItem;
-import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
-import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
@@ -33,10 +25,6 @@ public class MyEntitiesBrowserViewImpl extends LayoutContainer implements MyEnti
 	private IconsImageBundle iconsImageBundle;
 	private EntityTreeBrowser entityTreeBrowser;
 	private EntitySelectedHandler entitySelectedHandler;
-	private Button onlyCreatedFilter;
-	private boolean onlyCreatedPressed;
-	private Status status;
-	private Button onlyCreatedButton;
 	
 	private ContentPanel cp;
 			
@@ -58,35 +46,6 @@ public class MyEntitiesBrowserViewImpl extends LayoutContainer implements MyEnti
 		cp.setWidth(WIDTH_PX);
 		cp.setScrollMode(Scroll.AUTO);				
 		
-		status = new Status();
-		status.setAutoWidth(true);
-		status.setBox(true);
-		status.setText(DisplayConstants.STATUS_CAN_EDIT);
-		
-		ToolBar toolbar = new ToolBar();
-		onlyCreatedPressed = false;
-		onlyCreatedButton = new Button(DisplayConstants.BUTTON_FILTER_ONLY_MY_CREATION, AbstractImagePrototype.create(iconsImageBundle.filter16()));
-		onlyCreatedButton.addSelectionListener(new SelectionListener<ButtonEvent>() {			
-			@Override
-			public void componentSelected(ButtonEvent ce) {
-				if(onlyCreatedPressed) {
-					presenter.loadUserUpdateable();
-					onlyCreatedPressed = false;
-				} else {
-					presenter.createdOnlyFilter();
-					onlyCreatedPressed = true;
-				}
-			}
-		});
-		toolbar.add(onlyCreatedButton);
-
-		toolbar.add(new FillToolItem());
-		
-		
-		toolbar.add(status);
-		
-		cp.setBottomComponent(toolbar);
-		
 		cp.add(entityTreeBrowser.asWidget());
 		
 		add(cp);
@@ -94,13 +53,6 @@ public class MyEntitiesBrowserViewImpl extends LayoutContainer implements MyEnti
 
 	@Override
 	public void setUpdatableEntities(List<EntityHeader> rootEntities) {
-		setToolbarStateUserUpdateable();
-		entityTreeBrowser.setRootEntities(rootEntities);		
-	}
-	
-	@Override
-	public void setCreatedEntities(List<EntityHeader> rootEntities) {
-		setToolbarStateCreatedBy();
 		entityTreeBrowser.setRootEntities(rootEntities);		
 	}
 	
@@ -154,21 +106,4 @@ public class MyEntitiesBrowserViewImpl extends LayoutContainer implements MyEnti
 		};
 		entityTreeBrowser.addEntitySelectedHandler(entitySelectedHandler);
 	}
-
-	private void setToolbarStateCreatedBy() {
-		if(onlyCreatedButton != null && status != null) {
-			onlyCreatedPressed = true;
-			onlyCreatedButton.setText(DisplayConstants.BUTTON_FILTER_USER_UPDATABLE);
-			status.setText(DisplayConstants.STATUS_CREATED_BY);
-		}
-	}
-
-	private void setToolbarStateUserUpdateable() {
-		if(onlyCreatedButton != null && status != null) {
-			onlyCreatedPressed = false;
-			onlyCreatedButton.setText(DisplayConstants.BUTTON_FILTER_ONLY_MY_CREATION);
-			status.setText(DisplayConstants.STATUS_CAN_EDIT);
-		}
-	}
-
 }
