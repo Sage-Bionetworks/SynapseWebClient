4f707d8579e56a59a04316b6d37712b181c00f9e
Jay Hodgson <jay.hodgson@sagebase.org>
2013-03-25 16:49:59 -0700

    remove file size cap!

--- src/main/java/org/sagebionetworks/web/server/servlet/FileHandleServlet.java
+++ src/main/java/org/sagebionetworks/web/server/servlet/FileHandleServlet.java
@@ -52,10 +52,6 @@ import com.google.inject.Inject;
  */
 public class FileHandleServlet extends HttpServlet {
 
-	public static final int MAX_TIME_OUT = 10 * 1000;
-	public static final long BYTES_PER_MEGABYTE = 1048576;
-	public static final long MAX_ATTACHMENT_MEGABYTES = 10;
-	public static final long MAX_ATTACHMENT_SIZE_IN_BYTES = MAX_ATTACHMENT_MEGABYTES*BYTES_PER_MEGABYTE; // 10 MB
 	private static Logger logger = Logger.getLogger(FileUpload.class.getName());
 	private static final long serialVersionUID = 1L;
 
@@ -209,7 +205,7 @@ public class FileHandleServlet extends HttpServlet {
                 File tempDir = Files.createTempDir();
 				File temp = new File(tempDir.getAbsolutePath() + File.separator + fileName);
 
-				ServiceUtils.writeToFile(temp, stream, MAX_ATTACHMENT_SIZE_IN_BYTES);
+				ServiceUtils.writeToFile(temp, stream, Long.MAX_VALUE);
 				try{
 					// Now upload the file
 					String contentType = item.getContentType();
