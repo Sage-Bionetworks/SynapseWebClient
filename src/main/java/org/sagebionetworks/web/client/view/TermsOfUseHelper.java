872275efe2ed7ec14cbe8837259a6ccaa006869c
David Burdick <dburdick@gmail.com>
2013-03-27 15:17:39 -0700

    SWC-377 and SWC-378 - Governance and ToU changes

--- src/main/java/org/sagebionetworks/web/client/view/TermsOfUseHelper.java
+++ src/main/java/org/sagebionetworks/web/client/view/TermsOfUseHelper.java
@@ -5,27 +5,37 @@ import org.sagebionetworks.web.client.widget.login.AcceptTermsOfUseCallback;
 import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.util.Padding;
 import com.extjs.gxt.ui.client.widget.Dialog;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.button.Button;
-import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.extjs.gxt.ui.client.widget.layout.FlowLayout;
+import com.extjs.gxt.ui.client.widget.layout.MarginData;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class TermsOfUseHelper {
 
+	private static final MarginData spacer = new MarginData(0, 15, 15, 15);
+	
 	public static void showTermsOfUse(String content, final AcceptTermsOfUseCallback callback) {
         final Dialog window = new Dialog();
         window.setMaximizable(false);
-        window.setSize(600, 700);
+        window.setWidth((int)(Window.getClientWidth() * .9));
+        window.setHeight((int)(Window.getClientHeight() * .9));
         window.setPlain(true); 
         window.setModal(true); 
         window.setHeading("Synapse Terms of Use"); 
-        window.setLayout(new FitLayout());
-        window.setScrollMode(Scroll.ALWAYS);
+        window.setLayout(new FlowLayout());
+        window.setScrollMode(Scroll.AUTO);
         window.okText = "I Agree";
         window.setButtons(Dialog.OKCANCEL);
         window.setHideOnButtonClick(true);
-        window.add(new HTML(SafeHtmlUtils.fromSafeConstant(content)));
+        LayoutContainer lc = new LayoutContainer();
+        lc.addStyleName("whiteBackground");
+        lc.add(new HTML(SafeHtmlUtils.fromSafeConstant(content)), spacer);
         // List for the button selection
         Button saveButton = window.getButtonById(Dialog.OK);
         saveButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
@@ -34,6 +44,7 @@ public class TermsOfUseHelper {
             	if (callback!=null) callback.accepted();
             }
         });
+        window.add(lc);
         // show the window
         window.show();		
 	}
