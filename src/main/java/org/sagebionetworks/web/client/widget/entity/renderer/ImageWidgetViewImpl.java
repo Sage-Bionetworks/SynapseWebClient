898d6302086fe43258c811312d1e7eb9649947b0
Jay Hodgson <jay.hodgson@sagebase.org>
2013-03-28 12:41:06 -0700

    new image parameter precondition check

--- src/main/java/org/sagebionetworks/web/client/widget/entity/renderer/ImageWidgetViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/renderer/ImageWidgetViewImpl.java
@@ -2,11 +2,13 @@ package org.sagebionetworks.web.client.widget.entity.renderer;
 
 import org.sagebionetworks.web.client.DisplayUtils;
 import org.sagebionetworks.web.client.SynapseJSNIUtils;
+import org.sagebionetworks.web.client.widget.entity.registration.WidgetConstants;
 import org.sagebionetworks.web.shared.WikiPageKey;
 
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.google.gwt.event.dom.client.LoadEvent;
 import com.google.gwt.event.dom.client.LoadHandler;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -31,12 +33,15 @@ public class ImageWidgetViewImpl extends LayoutContainer implements ImageWidgetV
 		
 		if (alignment != null) {
 			String trimmedAlignment = alignment.trim();
-			if ("left".equalsIgnoreCase(trimmedAlignment)) {
+			if (WidgetConstants.FLOAT_LEFT.equalsIgnoreCase(trimmedAlignment)) {
 				image.addStyleName("floatleft");
 				image.addStyleName("margin-right-10");
-			} else if ("right".equalsIgnoreCase(trimmedAlignment)) {
+			} else if (WidgetConstants.FLOAT_RIGHT.equalsIgnoreCase(trimmedAlignment)) {
 				image.addStyleName("floatright");
 				image.addStyleName("margin-left-10");
+			} else if (!WidgetConstants.FLOAT_NONE.equalsIgnoreCase(trimmedAlignment)) {
+				showError("Alignment value not recognized: " + alignment);
+				return;
 			}
 		}
 		//don't show until we have the correct size (otherwise it's initially shown at 100%, then scaled down!).
@@ -44,28 +49,36 @@ public class ImageWidgetViewImpl extends LayoutContainer implements ImageWidgetV
 		image.addLoadHandler(new LoadHandler() {
 			@Override
 			public void onLoad(LoadEvent event) {
-				float imageHeight = image.getHeight();
-				float imageWidth = image.getWidth();
-				if (scale != null && !"100".equals(scale)) {
-					//scale is specified
-					final float scaleFloat = Float.parseFloat(scale) * .01f;
-					// scale image
-					float scaledImageWidth = (imageWidth * scaleFloat);
-					//if the scaled width is too wide for the screen, then render the max width that we can
-					if (scaledImageWidth > MAX_IMAGE_WIDTH) {
+				try {
+					float imageHeight = image.getHeight();
+					float imageWidth = image.getWidth();
+					if (scale != null && !"100".equals(scale)) {
+						//scale is specified
+						final float scaleFloat = Float.parseFloat(scale) * .01f;
+						if (scaleFloat < 0) {
+							throw new IllegalArgumentException("Image scale must be positive.");
+						}
+						// scale image
+						float scaledImageWidth = (imageWidth * scaleFloat);
+						//if the scaled width is too wide for the screen, then render the max width that we can
+						if (scaledImageWidth > MAX_IMAGE_WIDTH) {
+							setImageToMaxSize(imageWidth, imageHeight);
+						}
+						else {
+							image.setWidth(scaledImageWidth + "px");
+							float scaledImageHeight = (imageHeight * scaleFloat);
+							image.setHeight(scaledImageHeight + "px");
+						}
+					}
+					else if (imageWidth > MAX_IMAGE_WIDTH){
+						//if scale is not specified (or if 100%), then only scale this image if it's too wide to fit in the screen
 						setImageToMaxSize(imageWidth, imageHeight);
 					}
-					else {
-						image.setWidth(scaledImageWidth + "px");
-						float scaledImageHeight = (imageHeight * scaleFloat);
-						image.setHeight(scaledImageHeight + "px");
-					}
+					image.setVisible(true);
+				} catch (Exception e) {
+					remove(image);
+					showError("Image failed to load: " + e.getMessage());
 				}
-				else if (imageWidth > MAX_IMAGE_WIDTH){
-					//if scale is not specified (or if 100%), then only scale this image if it's too wide to fit in the screen
-					setImageToMaxSize(imageWidth, imageHeight);
-				}
-				image.setVisible(true);
 			}
 			
 			private void setImageToMaxSize(float imageWidth, float imageHeight) {
@@ -76,6 +89,11 @@ public class ImageWidgetViewImpl extends LayoutContainer implements ImageWidgetV
 		
 		add(image);
 		this.layout(true);
+	}
+	
+	public void showError(String error) {
+		add(new HTML(error));
+		layout(true);
 	}
 	
 	@Override
