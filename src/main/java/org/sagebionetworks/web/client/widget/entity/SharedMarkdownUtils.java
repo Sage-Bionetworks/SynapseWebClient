1ec907ce5e11db8005808f0b1684df3e330a79ee
Jay Hodgson <jay.hodgson@sagebase.org>
2013-06-18 17:32:22 -0700

    SWC-676: move to shared utility class

---
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/SharedMarkdownUtils.java
@@ -0,0 +1,29 @@
+package org.sagebionetworks.web.client.widget.entity;
+
+import org.sagebionetworks.web.client.widget.entity.registration.WidgetConstants;
+import org.sagebionetworks.web.shared.WebConstants;
+
+public class SharedMarkdownUtils {
+
+	public static String getWikiSubpagesMarkdown() {
+		return WidgetConstants.WIDGET_START_MARKDOWN + WidgetConstants.WIKI_SUBPAGES_CONTENT_TYPE + WidgetConstants.WIDGET_END_MARKDOWN;
+	}
+
+	public static String getWidgetHTML(int widgetIndex, String suffix, String widgetProperties){
+		StringBuilder sb = new StringBuilder();
+		sb.append("<div id=\"");
+		sb.append(WebConstants.DIV_ID_WIDGET_PREFIX);
+		sb.append(widgetIndex);
+		sb.append(suffix);
+		sb.append("\" class=\"widgetContainer\" widgetParams=\"");
+		sb.append(widgetProperties);
+		sb.append("\">");
+		sb.append("</div>");
+	    return sb.toString();
+	}
+
+	public static String getDefaultWikiMarkdown() {
+		return getWidgetHTML(0, "", WidgetConstants.WIKI_SUBPAGES_CONTENT_TYPE);
+	}
+
+}
