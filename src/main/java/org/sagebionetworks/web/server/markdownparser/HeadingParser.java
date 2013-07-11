2d821fd312bc5cb0242cf049f2da2a112e57947b
Jay Hodgson <jay.hodgson@sagebase.org>
2013-07-11 14:43:00 -0700

    SWC-697: fix headings, lists (and in turn bold and italics), and change strikeout to double dash after looking at real data

--- src/main/java/org/sagebionetworks/web/server/markdownparser/HeadingParser.java
+++ src/main/java/org/sagebionetworks/web/server/markdownparser/HeadingParser.java
@@ -5,11 +5,13 @@ import java.util.regex.Pattern;
 
 public class HeadingParser extends BasicMarkdownElementParser  {
 	Pattern p1 = Pattern.compile(MarkdownRegExConstants.HEADING_REGEX, Pattern.DOTALL);;
+	boolean matchedLine;
 	
 	@Override
 	public void processLine(MarkdownElements line) {
 		Matcher m = p1.matcher(line.getMarkdown());
-		if (m.matches()) {
+		matchedLine = m.matches();
+		if (matchedLine) {
 			//looks like a heading
 			String prefix = m.group(1);
 			String hashes = m.group(2);
@@ -23,5 +25,9 @@ public class HeadingParser extends BasicMarkdownElementParser  {
 	@Override
 	public boolean isBlockElement() {
 		return true;
+	}
+	@Override
+	public boolean isInMarkdownElement() {
+		return matchedLine;
 	}
 }
