b81fef71be13cc5d3b313b25e4984d91136876d3
heidiso <heidi.so@sagebase.org>
2013-09-04 17:08:01 -0700

    SWC-827 [Markdown] Add escaping syntax for tildes

---
+++ src/main/java/org/sagebionetworks/web/server/markdownparser/TildeParser.java
@@ -0,0 +1,15 @@
+package org.sagebionetworks.web.server.markdownparser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TildeParser extends BasicMarkdownElementParser {
+	Pattern p = Pattern.compile(MarkdownRegExConstants.TILDE_ESCAPED_REGEX);
+	
+	@Override
+	public void processLine(MarkdownElements line) {
+		Matcher m = p.matcher(line.getMarkdown());
+		line.updateMarkdown(m.replaceAll("&#126;"));
+	}
+
+}
