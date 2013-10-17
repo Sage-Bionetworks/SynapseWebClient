13bad58ddf609e014da5120c186e4a473356d6ef
David Burdick <dburdick@gmail.com>
2013-10-17 16:08:27 -0700

    swc-899 - fix CL client link

--- src/main/java/org/sagebionetworks/web/client/ClientProperties.java
+++ src/main/java/org/sagebionetworks/web/client/ClientProperties.java
@@ -104,8 +104,8 @@ public class ClientProperties {
 	public static final String CLIENT_R_EXAMPLE_CODE_URL = "https://www.synapse.org/#!Synapse:syn1834618";
 	public static final String CLIENT_PYTHON_API_URL = "https://www.synapse.org/#!Synapse:syn1768504";
 	public static final String CLIENT_PYTHON_EXAMPLE_CODE_URL = "https://www.synapse.org/#!Synapse:syn1768504";
-	public static final String CLIENT_CL_API_URL = "https://github.com/Sage-Bionetworks/synapsePythonClient";
-	public static final String CLIENT_CL_EXAMPLE_CODE_URL = "https://github.com/Sage-Bionetworks/synapsePythonClient/tree/master/examples/provenance";
+	public static final String CLIENT_CL_API_URL = "https://www.synapse.org/#!Synapse:syn1768504";
+	public static final String CLIENT_CL_EXAMPLE_CODE_URL = "https://www.synapse.org/#!Synapse:syn1768504";
 	public static final String CLIENT_JAVA_API_URL = "https://github.com/Sage-Bionetworks/SynapseWebClient/blob/develop/src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java";
 	public static final String CLIENT_JAVA_EXAMPLE_CODE_URL = "https://github.com/Sage-Bionetworks/SynapseWebClient/blob/develop/src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java";	
 
