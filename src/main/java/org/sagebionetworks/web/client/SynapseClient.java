4001f2bb5082cf443e00597457cacff61633b1a1
heidiso <heidi.so@sagebase.org>
2013-11-12 16:26:23 -0800

    Add tests for the new client methods

--- src/main/java/org/sagebionetworks/web/client/SynapseClient.java
+++ src/main/java/org/sagebionetworks/web/client/SynapseClient.java
@@ -221,11 +221,8 @@ public interface SynapseClient extends RemoteService {
 	public String updateV2WikiPage(String ownerId, String ownerType, String wikiPageJson) throws RestServiceException;
 	public String restoreV2WikiPage(String ownerId, String ownerType, String wikiPageJson, Long versionToUpdate) throws RestServiceException;
 	public void deleteV2WikiPage(WikiPageKey key) throws RestServiceException;
-	
 	public String getV2WikiHeaderTree(String ownerId, String ownerType) throws RestServiceException;
-	
 	public String getV2WikiAttachmentHandles(WikiPageKey key) throws RestServiceException;
-	
 	public String getV2WikiHistory(WikiPageKey key, Long limit, Long offset) throws RestServiceException;
 	
 	public String getFileEndpoint() throws RestServiceException;
