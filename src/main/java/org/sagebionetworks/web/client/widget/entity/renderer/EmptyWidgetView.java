4bdb23f6d2a8b388fe714600d1508d9a83556d73
Jay Hodgson <jay.hodgson@sagebase.org>
2013-11-13 13:54:34 -0800

    with new no-op empty widget

---
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/renderer/EmptyWidgetView.java
@@ -0,0 +1,18 @@
+package org.sagebionetworks.web.client.widget.entity.renderer;
+
+import com.google.gwt.user.client.ui.IsWidget;
+
+public interface EmptyWidgetView extends IsWidget {
+
+	/**
+	 * Set the presenter.
+	 * @param presenter
+	 */
+	public void setPresenter(Presenter presenter);
+	
+	/**
+	 * Presenter interface
+	 */
+	public interface Presenter {
+	}
+}
---
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/renderer/EmptyWidgetViewImpl.java
@@ -0,0 +1,21 @@
+package org.sagebionetworks.web.client.widget.entity.renderer;
+
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+
+public class EmptyWidgetViewImpl extends SimplePanel implements EmptyWidgetView {
+	
+	@Inject
+	public EmptyWidgetViewImpl() {
+	}
+		
+	@Override
+	public Widget asWidget() {
+		return this;
+	}	
+
+	@Override 
+	public void setPresenter(Presenter presenter) {
+	}
+}
