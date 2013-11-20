69bb077761b5da83a765db1b8ed885d9b01c2fd8
Jay Hodgson <jay.hodgson@sagebase.org>
2013-11-19 17:29:41 -0800

    SWC-1056

--- src/main/java/org/sagebionetworks/web/client/DisplayUtils.java
+++ src/main/java/org/sagebionetworks/web/client/DisplayUtils.java
@@ -73,6 +73,7 @@ import org.sagebionetworks.web.client.utils.CallbackP;
 import org.sagebionetworks.web.client.utils.TOOLTIP_POSITION;
 import org.sagebionetworks.web.client.widget.Alert;
 import org.sagebionetworks.web.client.widget.Alert.AlertType;
+import org.sagebionetworks.web.client.widget.FitImage;
 import org.sagebionetworks.web.client.widget.entity.WidgetSelectionState;
 import org.sagebionetworks.web.client.widget.entity.browse.EntityFinder;
 import org.sagebionetworks.web.client.widget.entity.dialog.ANNOTATION_TYPE;
@@ -145,7 +146,6 @@ import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
@@ -1912,11 +1912,8 @@ public class DisplayUtils {
 		if (clickHandler != null)
 			headingHtml.addClickHandler(clickHandler);
 		
-		Image profilePicture = new Image(pictureUri);
+		FitImage profilePicture = new FitImage(pictureUri, 64, 64);
 		profilePicture.addStyleName("pull-left media-object");
-		profilePicture.setPixelSize(64,64);
-		profilePicture.setWidth("64px");
-		profilePicture.setHeight("64px");
 		profilePicture.addStyleName("imageButton");
 		if (clickHandler != null)
 			profilePicture.addClickHandler(clickHandler);
