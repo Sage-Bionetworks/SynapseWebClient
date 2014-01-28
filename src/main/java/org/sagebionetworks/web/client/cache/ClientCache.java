52bc512ac6378d94d40de35b1e58f89e67d9b99e
Jay Hodgson <jay.hodgson@sagebase.org>
2014-01-27 17:16:31 -0800

    comments only

--- src/main/java/org/sagebionetworks/web/client/cache/ClientCache.java
+++ src/main/java/org/sagebionetworks/web/client/cache/ClientCache.java
@@ -2,8 +2,29 @@ package org.sagebionetworks.web.client.cache;
 
 
 public interface ClientCache {
+	/**
+	 * Returns the value from storage, iff storage is supported by this browser and the key/value pair was put into storage
+	 * If you try to get a value that has expired, this method will return null.
+	 * @param key
+	 * @return
+	 */
 	String get(String key);
+	/**
+	 * The implementation uses sessionStorage, and is NOT SECURE.  Do not store sensitive data here.
+	 * Size only limited by system memory.  
+	 * The given key/value pair will survive as long as the originating window or tab, or 20 minutes (whichever is shorter).
+	 * 
+	 * @param key
+	 * @param value
+	 */
 	void put(String key, String value);
 	void put(String key, String value, Long expireTime);
+	
+	/**
+	 * Returns true iff storage is supported by this browser and the key/value pair was put into storage.
+	 * Even if the value is contained in the cache, it may be expired.
+	 * @param key
+	 * @return
+	 */
 	boolean contains(String key);
 }
