b7859db40a3734047dc043a219e7cdfe1c0e91c6
Jay Hodgson <jay.hodgson@sagebase.org>
2014-02-10 16:23:09 -0800

    fix

--- src/main/java/org/sagebionetworks/web/server/servlet/filter/GWTCacheControlFilter.java
+++ src/main/java/org/sagebionetworks/web/server/servlet/filter/GWTCacheControlFilter.java
@@ -13,15 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * very basic filter to browser scripts
+ * very basic filter to cache resources
  * 
  * @author jayhodgson
  * 
  */
 public class GWTCacheControlFilter implements Filter {
 	
+	public static final long CACHE_TIME=1000*60*60*6;  //6 hours
 	private FilterConfig filterConfig;
-	
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain filterChain) throws IOException, ServletException {
@@ -30,8 +30,7 @@ public class GWTCacheControlFilter implements Filter {
 		if (!requestURI.contains(".nocache.")) {
 			long today = new Date().getTime();
 			HttpServletResponse httpResponse = (HttpServletResponse) response;
-			//6 hours
-			httpResponse.setDateHeader("Expires", today+(1000*60*60*6));
+			httpResponse.setDateHeader("Expires", today+CACHE_TIME);
 		}
 		filterChain.doFilter(request, response);
 	}
