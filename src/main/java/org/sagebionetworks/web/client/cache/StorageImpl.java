c67c18e222c748494cbe0da8c5df08ce3bad09b1
Jay Hodgson <jay.hodgson@sagebase.org>
2014-03-20 13:10:00 -0700

    wrap web storage setItem in a try catch, and enable client-side logging (and make other logging option choices explicit)

--- src/main/java/org/sagebionetworks/web/client/cache/StorageImpl.java
+++ src/main/java/org/sagebionetworks/web/client/cache/StorageImpl.java
@@ -1,9 +1,13 @@
 package org.sagebionetworks.web.client.cache;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.google.gwt.storage.client.Storage;
 
 public class StorageImpl implements StorageWrapper {
 
+	private static Logger storageLogger = Logger.getLogger(StorageImpl.class.getName());
 	private Storage storage;
 	public StorageImpl() {
 		storage = Storage.getSessionStorageIfSupported();
@@ -31,8 +35,15 @@ public class StorageImpl implements StorageWrapper {
 
 	@Override
 	public void setItem(String key, String data) {
-		if (isStorageSupported())
-			storage.setItem(key, data);
+		if (isStorageSupported()) {
+			try {
+				storage.setItem(key, data);
+			} catch (Throwable e) {
+				//failed to set the item in the cache.
+				storageLogger.log(Level.SEVERE, "Failure to set item in the cache: " + e.getMessage());
+			}
+		}
+			
 	}
 	
 	@Override
