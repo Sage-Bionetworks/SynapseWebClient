56f70cf8b54cad69902ff0b08a843efd3ccb25fc
Jay Hodgson <jay.hodgson@sagebase.org>
2014-04-14 14:38:01 -0700

    tests

--- src/test/java/org/sagebionetworks/web/unitclient/presenter/LoginPresenterTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/presenter/LoginPresenterTest.java
@@ -1,9 +1,7 @@
 package org.sagebionetworks.web.unitclient.presenter;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -15,6 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.sagebionetworks.repo.model.UserPreferences;
 import org.sagebionetworks.repo.model.UserProfile;
 import org.sagebionetworks.repo.model.UserSessionData;
 import org.sagebionetworks.repo.model.auth.Session;
@@ -36,7 +35,9 @@ import org.sagebionetworks.web.client.presenter.LoginPresenter;
 import org.sagebionetworks.web.client.security.AuthenticationController;
 import org.sagebionetworks.web.client.transform.NodeModelCreator;
 import org.sagebionetworks.web.client.view.LoginView;
+import org.sagebionetworks.web.client.widget.login.AcceptTermsOfUseCallback;
 import org.sagebionetworks.web.shared.WebConstants;
+import org.sagebionetworks.web.shared.exceptions.NotFoundException;
 import org.sagebionetworks.web.test.helper.AsyncMockStubber;
 
 import com.google.gwt.event.shared.EventBus;
@@ -284,20 +285,64 @@ public class LoginPresenterTest {
 	}
 	
 	
-//	@Test 
-//	public void testSetPlaceSSOLoginNotSignedToU() {
-//		String fakeToken = "0e79b99-4bf8-4999-b3a2-5f8c0a9499eb";
-//		LoginPlace place = new LoginPlace(fakeToken);
-//		AsyncMockStubber.callSuccessWith("success").when(mockAuthenticationController).loginUserSSO(anyString(), any(AsyncCallback.class));		
-//		usd.getSession().setAcceptsTermsOfUse(false);
-//		AsyncMockStubber.callSuccessWith("tou").when(mockAuthenticationController).getTermsOfUse(any(AsyncCallback.class));
-// 
-// 		To be continued...
-//		
-//		loginPresenter.setPlace(place);
-//		verify(mockAuthenticationController).loginUserSSO(eq(fakeToken), any(AsyncCallback.class));
-//		verify(mockEventBus).fireEvent(any(PlaceChangeEvent.class));
-//	}
+	@Test 
+	public void testSetPlaceSSOLoginNotSignedToU() throws JSONObjectAdapterException {
+		UserProfile profile = new UserProfile();
+		profile.setOwnerId("1233");
+		profile.setUserName("valid-username");
+		setMyProfile(profile);
+
+		String fakeToken = "0e79b99-4bf8-4999-b3a2-5f8c0a9499eb";
+		LoginPlace place = new LoginPlace(fakeToken);
+		AsyncMockStubber.callSuccessWith("success").when(mockAuthenticationController).loginUserSSO(anyString(), any(AsyncCallback.class));		
+		usd.getSession().setAcceptsTermsOfUse(false);
+		AsyncMockStubber.callSuccessWith("tou").when(mockAuthenticationController).getTermsOfUse(any(AsyncCallback.class));
+		
+		//run the test
+		loginPresenter.setPlace(place);
+		
+		verify(mockAuthenticationController).loginUserSSO(eq(fakeToken), any(AsyncCallback.class));
+		
+		ArgumentCaptor<AcceptTermsOfUseCallback> argument = ArgumentCaptor.forClass(AcceptTermsOfUseCallback.class);
+		//shows terms of use
+		verify(mockView).showTermsOfUse(anyString(), argument.capture());
+		AcceptTermsOfUseCallback callback = argument.getValue();
+		
+		AsyncMockStubber.callSuccessWith(null).when(mockAuthenticationController).signTermsOfUse(anyBoolean(), any(AsyncCallback.class));
+		//user has not passed the certification quiz
+		AsyncMockStubber.callFailureWith(new NotFoundException()).when(mockSynapseClient).getCertifiedUserPassingRecord(anyString(),  any(AsyncCallback.class));
+		
+		//accept
+		callback.accepted();
+		
+		//make sure we check for certification
+		verify(mockSynapseClient).getCertifiedUserPassingRecord(anyString(),  any(AsyncCallback.class));
+		//and show the quiz reminder
+		verify(mockView).showQuizInfoUI();
+		
+		//user has preferences, but reminder is not set
+		Mockito.reset(mockView);
+		UserPreferences userPreferences = new UserPreferences();
+		userPreferences.setDontShowCertifiedUserReminder(null);
+		profile.setPreferences(userPreferences);
+		setMyProfile(profile);
+		callback.accepted();
+		verify(mockView).showQuizInfoUI();
+		
+		//opts to continue to show the quiz reminder
+		Mockito.reset(mockView);
+		userPreferences.setDontShowCertifiedUserReminder(false);
+		setMyProfile(profile);
+		callback.accepted();
+		verify(mockView).showQuizInfoUI();
+		
+		//opts to hide the quiz reminder
+		Mockito.reset(mockView);
+		userPreferences.setDontShowCertifiedUserReminder(true);
+		setMyProfile(profile);
+		callback.accepted();
+		verify(mockView, never()).showQuizInfoUI();
+	}
 
 	
 }
