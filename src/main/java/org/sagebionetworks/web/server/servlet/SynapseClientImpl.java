723851bf0d72d13b944296edd2ae7d10fb177039
Jay Hodgson <jay.hodgson@sagebase.org>
2014-06-17 16:53:56 -0700

    add new Governance help place

--- src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java
+++ src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java
@@ -2747,6 +2747,7 @@ public class SynapseClientImpl extends RemoteServiceServlet implements
 			tempMap.put(WebConstants.USER_CERTIFICATION_TUTORIAL, new org.sagebionetworks.web.shared.WikiPageKey(getSynapseProperty(WebConstants.TRUSTED_USER_ENTITY_ID_PROPERTY), ObjectType.ENTITY.toString(), getSynapseProperty(WebConstants.TRUSTED_USER_WIKI_ID_PROPERTY)));
 			tempMap.put(WebConstants.FORMATTING_GUIDE, new org.sagebionetworks.web.shared.WikiPageKey(getSynapseProperty(WebConstants.FORMATTING_GUIDE_ENTITY_ID_PROPERTY), ObjectType.ENTITY.toString(), getSynapseProperty(WebConstants.FORMATTING_GUIDE_WIKI_ID_PROPERTY)));
 			tempMap.put(WebConstants.CHALLENGE_PARTICIPATION_INFO, new org.sagebionetworks.web.shared.WikiPageKey(getSynapseProperty(WebConstants.CHALLENGE_PARTICIPATION_INFO_ENTITY_ID_PROPERTY), ObjectType.ENTITY.toString(), getSynapseProperty(WebConstants.CHALLENGE_PARTICIPATION_INFO_WIKI_ID_PROPERTY)));
+			tempMap.put(WebConstants.GOVERNANCE, new org.sagebionetworks.web.shared.WikiPageKey(getSynapseProperty(WebConstants.GOVERNANCE_ENTITY_ID_PROPERTY), ObjectType.ENTITY.toString(), getSynapseProperty(WebConstants.GOVERNANCE_WIKI_ID_PROPERTY)));
 			
 			pageName2WikiKeyMap = tempMap;
 		}
