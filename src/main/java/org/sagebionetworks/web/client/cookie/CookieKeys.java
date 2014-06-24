f6558a265f3b1ca47fbbdccfbfccd56d128d3dee
Geoff Shannon <geoffpshannon@gmail.com>
2014-06-24 12:28:54 -0700

    Change line endings of CookieKeys file

--- src/main/java/org/sagebionetworks/web/client/cookie/CookieKeys.java
+++ src/main/java/org/sagebionetworks/web/client/cookie/CookieKeys.java
@@ -2,27 +2,27 @@ package org.sagebionetworks.web.client.cookie;
 
 /**
  * The cookie key master.  Don't tell the cookie monster!
- * 
+ *
  * @author jmhill
  *
  */
 public class CookieKeys {
-	
+
 	/**
 	 * Key for the selected columns shown in the datasets table.
 	 */
 	public static String SELECTED_DATASETS_COLUMNS = "org.sagebionetworks.datasets.selected.columns";
-	
+
 	/**
 	 * Key for the selected filters applied on the datasets table.
 	 */
 	public static String APPLIED_DATASETS_FILTERS = "org.sagebionetworks.datasets.applied.filters";
-	
+
 	/**
 	 * Login token
 	 */
 	public static String USER_LOGIN_TOKEN = "org.sagbionetworks.security.user.login.token";
-	
+
 	/**
 	 * Login token
 	 */
@@ -32,17 +32,17 @@ public class CookieKeys {
 	 * Last Place in the app
 	 */
 	public static String LAST_PLACE = "org.sagbionetworks.synapse.place.last.place";
-	
+
 	/**
 	 * Current Place in the app
 	 */
 	public static String CURRENT_PLACE = "org.sagbionetworks.synapse.place.current.place";
-	
+
 	/**
 	 * LinkedIn requestToken key
 	 */
 	public static String LINKEDIN = "org.sagebionetworks.synapse.linkedin";
-	
+
 	/**
 	 * Get Satisfaction fastpass key
 	 */
