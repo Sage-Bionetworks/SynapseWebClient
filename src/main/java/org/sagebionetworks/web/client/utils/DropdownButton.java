239c8a03d701f8fcbc4f448eed184ad1148a31ac
Jay Hodgson <jay.hodgson@sagebase.org>
2014-07-18 11:37:00 -0700

    revert changes to dropdown button and UL panel

--- src/main/java/org/sagebionetworks/web/client/utils/DropdownButton.java
+++ src/main/java/org/sagebionetworks/web/client/utils/DropdownButton.java
@@ -2,28 +2,24 @@ package org.sagebionetworks.web.client.utils;
 
 import org.sagebionetworks.web.client.DisplayUtils.ButtonType;
 
-import com.google.gwt.event.dom.client.BlurEvent;
-import com.google.gwt.event.dom.client.BlurHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Hyperlink;
 
-public class DropdownButton extends FlowPanel {
+public class DropdownButton extends LayoutContainer {
 
-	private Anchor btn;
+	private Button btn;
 	private UnorderedListPanel ul;
 	int count = 0;
-	ClickHandler toggleClickHandler;
+	
 	public DropdownButton(String title, ButtonType type) {
 		this(title, type, null);
 	}
 	
 	public DropdownButton(String title, ButtonType type, String iconClass) {
-		addStyleName("dropdown");
-		btn = new Anchor();
+		btn = new Button();
 		btn.removeStyleName("gwt-Button");
 		btn.addStyleName("btn btn-" + type.toString().toLowerCase() + " dropdown-toggle");
 		String style = iconClass == null ? "" : " class=\"glyphicon " + iconClass+ "\"" ;
@@ -31,33 +27,23 @@ public class DropdownButton extends FlowPanel {
 				"<span" + style+ "></span> " + 
 				title +
 				" <span class=\"caret\"></span> "));
-//		btn.getElement().setAttribute("data-toggle", "dropdown");
+		btn.getElement().setAttribute("data-toggle", "dropdown");
 		
 		add(btn);
 		
 		ul = new UnorderedListPanel();
-		ul.addStyleName("displayInline dropdown-menu");
-		
+		ul.addStyleName("dropdown-menu");
+		ul.setAttribute("role", "menu");
 		add(ul);
-		ul.setVisible(false);
-		toggleClickHandler = new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				ul.setVisible(!ul.isVisible());
-			}
-		};
-		btn.addClickHandler(toggleClickHandler);
 	}
 
 	public void addMenuItem(Anchor a) {
 		ul.add(a);
-		a.addClickHandler(toggleClickHandler);
 		count++;
 	}
 	
 	public void addMenuItem(Hyperlink a) {
 		ul.add(a);
-		a.addClickHandler(toggleClickHandler);
 		count++;
 	}
 	
