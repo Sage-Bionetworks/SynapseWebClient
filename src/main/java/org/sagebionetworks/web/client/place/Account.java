16d56f2f05e963a2838f9f3a195ea0a271d21fe1
Jay Hodgson <jay.hodgson@sagebase.org>
2014-08-04 14:00:30 -0700

    fix for email validation on FF

--- src/main/java/org/sagebionetworks/web/client/place/Account.java
+++ src/main/java/org/sagebionetworks/web/client/place/Account.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.web.client.place;
 
+import com.google.gwt.http.client.URL;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceTokenizer;
 import com.google.gwt.place.shared.Prefix;
@@ -9,13 +10,20 @@ public class Account extends Place{
 	private String token;
 
 	public Account(String token) {
-		this.token = token;
+		if (_isFirefox())
+			this.token = URL.encodePathSegment(token);
+		else
+			this.token = token;
 	}
 
 	public String toToken() {
 		return token;
 	}
 	
+	private final static native boolean _isFirefox() /*-{ 
+		return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+	}-*/;
+
 	@Prefix("!Account")
 	public static class Tokenizer implements PlaceTokenizer<Account> {
         @Override
