0a0858335f2139a364a878c21e057af5f9070288
Jay Hodgson <jay.hodgson@sagebase.org>
2014-08-08 11:15:12 -0700

    description is not informative for new entity types.  only include if defined

--- src/main/java/org/sagebionetworks/web/client/widget/provenance/ProvViewUtil.java
+++ src/main/java/org/sagebionetworks/web/client/widget/provenance/ProvViewUtil.java
@@ -218,11 +218,13 @@ public class ProvViewUtil {
 				String val = map.get(key);
 				if(val == null) val = "";
 				val = val.length() > MAX_TOOL_TIP_VALUE_CHAR ? val.substring(0, MAX_TOOL_TIP_VALUE_CHAR-3) + "..." : val;
-				sb.appendHtmlConstant("<span class=\"boldText\">")
-				.appendEscaped(key + ":")
-				.appendHtmlConstant("</span> ")
-				.appendEscaped(val)
-				.appendHtmlConstant("<br/>");
+				if (!DisplayConstants.DESCRIPTION.equalsIgnoreCase(key) && val.length() > 0) {
+					sb.appendHtmlConstant("<span class=\"boldText\">")
+					.appendEscaped(key + ":")
+					.appendHtmlConstant("</span> ")
+					.appendEscaped(val)
+					.appendHtmlConstant("<br/>");
+				}
 			}
 		}		
 		return sb.toSafeHtml();
