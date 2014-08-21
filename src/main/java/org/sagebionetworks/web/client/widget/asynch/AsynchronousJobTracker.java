ff9b234f948c0fdc0e08aa5b6b02936f3da58076
john-hill <john.hill@sagebase.org>
2014-08-21 12:58:27 -0700

    added missing comments

--- src/main/java/org/sagebionetworks/web/client/widget/asynch/AsynchronousJobTracker.java
+++ src/main/java/org/sagebionetworks/web/client/widget/asynch/AsynchronousJobTracker.java
@@ -10,9 +10,21 @@ import org.sagebionetworks.repo.model.asynch.AsynchronousJobStatus;
  */
 public interface AsynchronousJobTracker {
 
+	/**
+	 * For each run, configure must be called with the data to track.
+	 * @param toTrack The status to track.
+	 * @param waitTimeMS The amount of time between progress checks.
+	 * @param handler
+	 */
 	public void configure(AsynchronousJobStatus toTrack, int waitTimeMS, UpdatingAsynchProgressHandler handler);
 	
+	/**
+	 * Call to start tracking the progress of a job.
+	 */
 	public void start();
 	
+	/**
+	 * Call to cancel tracking the progress of a job.
+	 */
 	public void cancel();
 }
