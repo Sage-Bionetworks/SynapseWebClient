1e8eecdc29944ce91d883a6dfbd585b6119dc54c
toddis <toddis@uw.edu>
2014-09-25 16:59:51 -0700

    Made own classes for UserGroupSuggestOracle and UserGroupSuggestionDisplay. Tried to convert from people ComboBox to UserGroupSuggestBox in UserTeamConfigEditorViewImpl, but couldn't get to 'clear' on save button.

--- src/main/java/org/sagebionetworks/web/client/widget/search/UserGroupSuggestBoxViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/search/UserGroupSuggestBoxViewImpl.java
@@ -1,13 +1,12 @@
 package org.sagebionetworks.web.client.widget.search;
 
 import org.gwtbootstrap3.client.ui.Button;
-import org.gwtbootstrap3.client.ui.ButtonGroup;
-import org.gwtbootstrap3.client.ui.gwt.HTMLPanel;
 import org.sagebionetworks.repo.model.UserGroupHeaderResponsePage;
 import org.sagebionetworks.web.client.DisplayUtils;
 import org.sagebionetworks.web.client.SageImageBundle;
 import org.sagebionetworks.web.client.widget.search.UserGroupSuggestOracle;
 import org.sagebionetworks.web.client.widget.search.UserGroupSuggestOracle.UserGroupSuggestion;
+import org.sagebionetworks.web.client.widget.search.UserGroupSuggestionDisplay;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -15,7 +14,6 @@ import com.google.gwt.event.dom.client.FocusEvent;
 import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.SuggestOracle;
@@ -159,75 +157,4 @@ public class UserGroupSuggestBoxViewImpl extends SuggestBox implements UserGroup
 	public UserGroupSuggestOracle getUserGroupSuggestOracle() {
 		return (UserGroupSuggestOracle) getSuggestOracle();
 	}
-	
-	
-	/*
-	 * SuggestionDisplay (for decorating the SuggestionBox Popup)
-	 */
-	public static class UserGroupSuggestionDisplay extends SuggestBox.DefaultSuggestionDisplay {
-		private SageImageBundle sageImageBundle;
-		
-		private Label resultsLabel;
-		private ButtonGroup buttonGroup;
-		private Button prevButton;
-		private Button nextButton;
-		
-		private Widget popupContents; // to save when loading.
-		
-		private HTMLPanel loadingPanel;
-		
-		public UserGroupSuggestionDisplay(SageImageBundle sageImageBundle) {
-			super();
-			this.sageImageBundle = sageImageBundle;
-		}
-		
-		@Override
-		protected Widget decorateSuggestionList(Widget suggestionList) {
-			setUpFields();
-			FlowPanel suggestList = new FlowPanel();
-			suggestList.add(suggestionList);
-			
-			FlowPanel pagingArea = new FlowPanel();
-			pagingArea.addStyleName("userGroupSuggestionPagingArea");
-			pagingArea.add(buttonGroup);
-			pagingArea.add(resultsLabel);
-			suggestList.add(pagingArea);
-			return suggestList;
-		}
-		
-		public Label getResultsLabel()	{	return resultsLabel;	}
-		public Button getPrevButton()	{	return prevButton;		}
-		public Button getNextButton()	{	return nextButton;		}
-		public Widget getPopupContents(){	return popupContents;	}
-		
-		
-		public void showLoading(UserGroupSuggestBoxViewImpl suggestBox) {
-			popupContents = getPopupPanel().getWidget();
-			if (loadingPanel == null) {
-				loadingPanel = new HTMLPanel(DisplayUtils.getLoadingHtml(sageImageBundle));
-				loadingPanel.setWidth(suggestBox.getOffsetWidth() + "px");
-			}
-			getPopupPanel().setWidget(loadingPanel);
-			getPopupPanel().showRelativeTo(suggestBox);
-		}
-		
-		public void hideLoading() {
-			getPopupPanel().setWidget(popupContents);
-		}
-		
-		
-		private void setUpFields() {
-			resultsLabel = new Label();
-			resultsLabel.addStyleName("userGroupSuggesionResultsLabel");
-			
-			prevButton = new Button("Prev");
-			prevButton.setEnabled(false);
-			nextButton = new Button("Next");
-			
-			buttonGroup = new ButtonGroup();
-			buttonGroup.addStyleName("btn-group btn-group-xs userGroupSuggestionPager");
-			buttonGroup.add(prevButton);
-			buttonGroup.add(nextButton);
-		}
-	} // end of inner class UserGroupSuggestionDisplay
 }
