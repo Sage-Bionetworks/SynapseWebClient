ddab0734c60cb79cc305270ece9e10a00b953002
Jay Hodgson <jay.hodgson@sagebase.org>
2015-04-03 15:19:03 -0700

    move checkbox up

--- src/main/java/org/sagebionetworks/web/client/widget/entity/EvaluationSubmitterViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/EvaluationSubmitterViewImpl.java
@@ -296,7 +296,7 @@ public class EvaluationSubmitterViewImpl implements EvaluationSubmitterView {
 	private Div getContributorRow(String principalId, boolean selectCheckbox) {
 		Div row = new Div();
 		InlineCheckBox cb = new InlineCheckBox();
-		cb.addStyleName("moveup-8");
+		cb.addStyleName("moveup-18");
 		cb.setValue(selectCheckbox);
 		cb.setEnabled(false);
 		row.add(cb);
