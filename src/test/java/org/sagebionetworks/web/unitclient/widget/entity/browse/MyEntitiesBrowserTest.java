d250a90f5a913984d733ee142a85e7d1425047c8
Jay Hodgson <jay.hodgson@sagebase.org>
2015-04-21 15:22:45 -0700

    fix for image tab problem, and uncaught exceptions around tooltips

---
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/browse/MyEntitiesBrowserTest.java
@@ -0,0 +1,19 @@
+package org.sagebionetworks.web.unitclient.widget.entity.browse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyEntitiesBrowserTest {
+
+	@Test
+	public void testLoadUserUpdateable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateGetMyProjectQuery() {
+		fail("Not yet implemented");
+	}
+
+}
