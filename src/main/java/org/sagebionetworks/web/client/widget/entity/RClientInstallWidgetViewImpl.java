25b6e4add8fa5a5d6557dc12c4a595525be327fc
Nicholas Anderson <nja4@cs.washington.edu>
2015-05-01 12:12:41 -0700

    Last initwidget simplification completed

--- src/main/java/org/sagebionetworks/web/client/widget/entity/RClientInstallWidgetViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/RClientInstallWidgetViewImpl.java
@@ -8,16 +8,13 @@ import com.google.inject.Inject;
 public class RClientInstallWidgetViewImpl extends com.google.gwt.user.client.ui.Composite {
 
 	public interface Binder extends UiBinder<Widget, RClientInstallWidgetViewImpl> {}
-
-	Widget widget;
 	
 	@Inject
 	public RClientInstallWidgetViewImpl(Binder uiBinder) {
-		widget = uiBinder.createAndBindUi(this);
-		initWidget(widget);
+		initWidget(uiBinder.createAndBindUi(this));
 	}
 	
 	public Widget asWidget() {
-		return widget;
+		return this;
 	}
 }
