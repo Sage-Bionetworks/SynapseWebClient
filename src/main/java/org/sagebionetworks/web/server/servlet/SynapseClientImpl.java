81eab6788e6a5cd0479e4c968d3b408e27592902
Nicholas Anderson <nja4@cs.washington.edu>
2015-05-08 11:43:46 -0700

    Removed commented out lines

--- src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java
+++ src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java
@@ -1775,32 +1775,6 @@ public class SynapseClientImpl extends RemoteServiceServlet implements
 		}
 	}
 	
-//	@Override
-//	public List<TeamRequestBundle> getTeamsRequestsBundleForUser(String userId)
-//			throws RestServiceException {
-//		org.sagebionetworks.client.SynapseClient synapseClient = createSynapseClient();
-//		try {
-//			org.sagebionetworks.reflection.model.PaginatedResults<Team> teams = synapseClient.getTeamsForUser(
-//					userId, MAX_LIMIT, ZERO_OFFSET);
-//			List<Team> teamList = teams.getResults();
-//
-//			Collections.sort(teamList, new Comparator<Team>() {
-//		        @Override
-//		        public int compare(Team o1, Team o2) {
-//		        	return o1.getName().compareToIgnoreCase(o2.getName());
-//		        }
-//			});
-//			List<TeamRequestBundle> withRequests = new ArrayList<TeamRequestBundle>(teamList.size());
-//			for (Team team: teamList) {
-//				Long openRequestCount = getOpenRequestCount(userId, team.getId());
-//				withRequests.add(new TeamRequestBundle(team, openRequestCount == null ? 0 : openRequestCount));
-//			}
-//			return withRequests;
-//		} catch (SynapseException e) {
-//			throw ExceptionUtil.convertSynapseException(e);
-//		} 
-//	}	
-	
 	@Override
 	public List<TeamRequestBundle> getTeamsForUser(String userId, boolean includeOpenRequests)
 			throws RestServiceException {
