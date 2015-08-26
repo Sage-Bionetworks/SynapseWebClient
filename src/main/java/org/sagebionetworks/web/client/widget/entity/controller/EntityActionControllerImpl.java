3903106508bc864867686afc77554bd8ad19922b
Nicholas Anderson <nja4@cs.washington.edu>
2015-08-26 00:08:23 -0700

    Migrated changes from an old branch I never finished; should do some more testing

--- src/main/java/org/sagebionetworks/web/client/widget/entity/controller/EntityActionControllerImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/controller/EntityActionControllerImpl.java
@@ -147,6 +147,10 @@ public class EntityActionControllerImpl implements EntityActionController, Actio
 		actionMenu.addControllerWidget(uploader.asWidget());
 		if (!isUserAuthenticated) {
 			actionMenu.setToolsButtonVisible(false);
+			if (permissions.getCanPublicRead()) {
+				configureAnnotations();
+				configureFileHistory();
+			}
 		} else {
 			actionMenu.setToolsButtonVisible(true);
 			// Setup the actions
