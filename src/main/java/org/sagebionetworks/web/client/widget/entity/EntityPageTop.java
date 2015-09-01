91ad740168946306929d230d6f7b07d405e0e236
Nick Anderson <nja4@uw.edu>
2015-08-31 22:15:43 -0700

    Removed error message

--- src/main/java/org/sagebionetworks/web/client/widget/entity/EntityPageTop.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/EntityPageTop.java
@@ -328,8 +328,6 @@ public class EntityPageTop implements EntityPageTopView.Presenter, SynapseWidget
 		if (bundle.getEntity() instanceof Project) {
 			setArea(EntityArea.WIKI, wikiPageId);
 			view.configureProjectActionMenu(bundle, wikiPageId);
-		} else {
-			DisplayUtils.showErrorMessage("Failed to handle Wiki reload.");
 		}
 	}
 	
