1c37a1fa30e4c3e5572e3d7b7fee95ab921b2d19
Jay Hodgson <jay.hodgson@sagebase.org>
2015-11-17 09:52:07 -0800

    test verified badge pass through

--- src/test/java/org/sagebionetworks/web/unitclient/presenter/ProfilePresenterTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/presenter/ProfilePresenterTest.java
@@ -1419,12 +1419,22 @@ public class ProfilePresenterTest {
 	public void testVerificationUIInitApproved() {
 		setupVerificationState(VerificationStateEnum.APPROVED, null);
 		when(mockUserBundle.getIsVerified()).thenReturn(true);
+		String fName = "Luke";
+		String lName = "Skywalker";
+		String company = "Rebel Alliance";
+		String orcId = "http://orcid/address";
+		String location= "Jundland Wastes, Tatooine";
+		when(mockVerificationSubmission.getFirstName()).thenReturn(fName);
+		when(mockVerificationSubmission.getLastName()).thenReturn(lName);
+		when(mockVerificationSubmission.getCompany()).thenReturn(company);
+		when(mockVerificationSubmission.getOrcid()).thenReturn(orcId);
+		when(mockVerificationSubmission.getLocation()).thenReturn(location);
 		
 		//not the owner of this profile, but is ACT
 		viewProfile("123", "456");
-				
+		
 		//user bundle reported that target user is verified
-		verify(mockView).showVerifiedBadge(null, null, null, null, null);
+		verify(mockView).showVerifiedBadge(fName, lName, location, company, orcId);
 		verify(mockUserProfileClient).getMyOwnUserBundle(eq(ProfilePresenter.IS_ACT_MEMBER), any(AsyncCallback.class));
 		verify(mockView).setVerificationDetailsButtonVisible(true);
 	}
