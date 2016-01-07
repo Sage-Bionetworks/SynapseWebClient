359cc858a3e9e83acfaad58eb7aa452a6b3cf7cf
Kimyen <kimyen0816@gmail.com>
2016-01-06 16:40:40 -0800

    check for alpha mode

--- src/main/java/org/sagebionetworks/web/client/widget/entity/tabs/DiscussionTab.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/tabs/DiscussionTab.java
@@ -1,5 +1,7 @@
 package org.sagebionetworks.web.client.widget.entity.tabs;
 
+import org.sagebionetworks.web.client.DisplayUtils;
+import org.sagebionetworks.web.client.cookie.CookieProvider;
 import org.sagebionetworks.web.client.place.Synapse;
 import org.sagebionetworks.web.client.place.Synapse.EntityArea;
 import org.sagebionetworks.web.client.utils.CallbackP;
@@ -9,14 +11,17 @@ import com.google.inject.Inject;
 public class DiscussionTab implements DiscussionTabView.Presenter{
 	Tab tab;
 	DiscussionTabView view;
+	CookieProvider cookies;
 
 	@Inject
 	public DiscussionTab(
 			DiscussionTabView view,
-			Tab tab
+			Tab tab,
+			CookieProvider cookies
 			) {
 		this.view = view;
 		this.tab = tab;
+		this.cookies = cookies;
 		tab.configure("Discussion", view.asWidget());
 	}
 
@@ -26,7 +31,7 @@ public class DiscussionTab implements DiscussionTabView.Presenter{
 
 	public void configure(String entityId, String entityName) {
 		tab.setEntityNameAndPlace(entityName, new Synapse(entityId, null, EntityArea.DISCUSSION, null));
-		tab.setTabListItemVisible(false);
+		tab.setTabListItemVisible(DisplayUtils.isInTestWebsite(cookies));
 	}
 
 	public Tab asTab(){
--- src/test/java/org/sagebionetworks/web/unitclient/widget/entity/tabs/DiscussionTabTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/tabs/DiscussionTabTest.java
@@ -3,7 +3,6 @@ package org.sagebionetworks.web.unitclient.widget.entity.tabs;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
@@ -12,6 +11,8 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.web.client.DisplayUtils;
+import org.sagebionetworks.web.client.cookie.CookieProvider;
 import org.sagebionetworks.web.client.place.Synapse;
 import org.sagebionetworks.web.client.place.Synapse.EntityArea;
 import org.sagebionetworks.web.client.utils.CallbackP;
@@ -28,13 +29,16 @@ public class DiscussionTabTest {
 	DiscussionTabView mockView;
 	@Mock
 	CallbackP<Tab> mockOnClickCallback;
+	@Mock
+	CookieProvider mockCookies;
 	
 	DiscussionTab tab;
 	
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
-		tab = new DiscussionTab(mockView, mockTab);
+		tab = new DiscussionTab(mockView, mockTab, mockCookies);
+		Mockito.when(mockCookies.getCookie(DisplayUtils.SYNAPSE_TEST_WEBSITE_COOKIE_KEY)).thenReturn("not null");
 	}
 
 	@Test
@@ -54,8 +58,7 @@ public class DiscussionTabTest {
 		String entityName = "discussion project test";
 		tab.configure(entityId, entityName);
 
-		verify(mockTab).setTabListItemVisible(false);
-		verify(mockTab, never()).setTabListItemVisible(true);
+		verify(mockTab).setTabListItemVisible(true);
 
 		ArgumentCaptor<Synapse> captor = ArgumentCaptor.forClass(Synapse.class);
 		verify(mockTab).setEntityNameAndPlace(eq(entityName), captor.capture());
@@ -64,6 +67,15 @@ public class DiscussionTabTest {
 		assertNull(place.getVersionNumber());
 		assertEquals(EntityArea.DISCUSSION, place.getArea());
 		assertNull(place.getAreaToken());
+	}
+
+	@Test
+	public void testNotInTestWebsite() {
+		Mockito.when(mockCookies.getCookie(DisplayUtils.SYNAPSE_TEST_WEBSITE_COOKIE_KEY)).thenReturn(null);
+		String entityId = "syn1"; 
+		String entityName = "discussion project test";
+		tab.configure(entityId, entityName);
+		verify(mockTab).setTabListItemVisible(false);
 	}
 
 	@Test
