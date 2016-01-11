aefe8a2009d423f3ec031496097a9c428d0003f2
Kimyen <kimyen0816@gmail.com>
2016-01-11 11:10:22 -0800

    thread widget

---
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/discussion/ThreadWidgetTest.java
@@ -0,0 +1,49 @@
+package org.sagebionetworks.web.unitclient.widget.discussion;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.web.client.PortalGinInjector;
+import org.sagebionetworks.web.client.widget.discussion.ReplyWidget;
+import org.sagebionetworks.web.client.widget.discussion.ThreadWidget;
+import org.sagebionetworks.web.client.widget.discussion.ThreadWidgetView;
+
+import com.google.gwt.user.client.ui.Widget;
+
+public class ThreadWidgetTest {
+
+	@Mock
+	ThreadWidgetView mockView;
+	@Mock
+	PortalGinInjector mockGinInjector;
+	@Mock
+	ReplyWidget mockReplyWidget;
+
+	ThreadWidget threadWidget;
+
+	@Before
+	public void before() {
+		MockitoAnnotations.initMocks(this);
+		Mockito.when(mockGinInjector.getReplyWidget()).thenReturn(mockReplyWidget);
+		threadWidget = new ThreadWidget(mockView, mockGinInjector);
+	}
+
+	@Test
+	public void testConfigure() {
+		Mockito.verify(mockView).setPresenter(threadWidget);
+		// configure
+		Mockito.verify(mockView).clear();
+		Mockito.verify(mockView, Mockito.times(2)).addReply((Widget) Mockito.any());
+		Mockito.verify(mockGinInjector, Mockito.times(2)).getReplyWidget();
+		Mockito.verify(mockReplyWidget, Mockito.times(2)).configure();
+	}
+
+	@Test
+	public void asWidgetTest() {
+		threadWidget.asWidget();
+		Mockito.verify(mockView).setPresenter(threadWidget);
+		Mockito.verify(mockView).asWidget();
+	}
+}
