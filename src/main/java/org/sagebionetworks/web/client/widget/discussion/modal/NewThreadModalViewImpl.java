9f0fd61d802b0a027323e9eba25dd946f58d515f
Kimyen <kimyen0816@gmail.com>
2016-01-11 14:22:11 -0800

    use bootstrap object

--- src/main/java/org/sagebionetworks/web/client/widget/discussion/modal/NewThreadModalViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/discussion/modal/NewThreadModalViewImpl.java
@@ -2,13 +2,13 @@ package org.sagebionetworks.web.client.widget.discussion.modal;
 
 import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.Modal;
+import org.gwtbootstrap3.client.ui.TextArea;
+import org.gwtbootstrap3.client.ui.TextBox;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.TextArea;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
