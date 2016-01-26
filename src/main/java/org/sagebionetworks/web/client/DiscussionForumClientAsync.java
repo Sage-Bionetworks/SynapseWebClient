aaf333d9e8255557b2e0b05440e14765bddad774
Kimyen <kimyen0816@gmail.com>
2016-01-25 16:42:30 -0800

    remove no longer supported APIs

--- src/main/java/org/sagebionetworks/web/client/DiscussionForumClientAsync.java
+++ src/main/java/org/sagebionetworks/web/client/DiscussionForumClientAsync.java
@@ -7,7 +7,6 @@ import org.sagebionetworks.repo.model.discussion.DiscussionReplyOrder;
 import org.sagebionetworks.repo.model.discussion.DiscussionThreadBundle;
 import org.sagebionetworks.repo.model.discussion.DiscussionThreadOrder;
 import org.sagebionetworks.repo.model.discussion.Forum;
-import org.sagebionetworks.repo.model.discussion.MessageURL;
 import org.sagebionetworks.repo.model.discussion.UpdateReplyMessage;
 import org.sagebionetworks.repo.model.discussion.UpdateThreadMessage;
 import org.sagebionetworks.repo.model.discussion.UpdateThreadTitle;
@@ -51,8 +50,4 @@ public interface DiscussionForumClientAsync{
 			AsyncCallback<DiscussionReplyBundle> callback);
 
 	void markReplyAsDeleted(String replyId, AsyncCallback<Void> callback);
-
-	void getThreadUrl(String threadId, AsyncCallback<MessageURL> callback);
-
-	void getReplyUrl(String replyId, AsyncCallback<MessageURL> callback);
 }
