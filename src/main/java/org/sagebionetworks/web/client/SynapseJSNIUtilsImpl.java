f9aed906706df276adb3f8d73b50b59c12d4d413
Jay Hodgson <jay.hodgson@sagebase.org>
2016-02-25 17:18:22 -0800

    SWC-2911: with Synapse heading plugin

--- src/main/java/org/sagebionetworks/web/client/SynapseJSNIUtilsImpl.java
+++ src/main/java/org/sagebionetworks/web/client/SynapseJSNIUtilsImpl.java
@@ -588,9 +588,11 @@ public class SynapseJSNIUtilsImpl implements SynapseJSNIUtils {
 		if (!$wnd.markdownitSingleton) {
 			$wnd.markdownitSingleton = $wnd.markdownit()
 				.set({ html: false, breaks: true });
+			$wnd.markdownitSingleton.disable([ 'heading' ]);
 			$wnd.markdownitSingleton
 				.use($wnd.markdownitSub)
-				.use($wnd.markdownitSup);
+				.use($wnd.markdownitSup)
+				.use($wnd.markdownitSynapseHeading);
 			
 			$wnd.markdownitSingleton
 				.set({
