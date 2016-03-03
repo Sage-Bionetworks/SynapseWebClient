8db779292892a71dc5a28efeea42217e6a4e5e52
Jay Hodgson <jay.hodgson@sagebase.org>
2016-03-03 09:46:45 -0800

    pulled out table js into it's own plugin

--- src/main/java/org/sagebionetworks/web/client/MarkdownItImpl.java
+++ src/main/java/org/sagebionetworks/web/client/MarkdownItImpl.java
@@ -106,13 +106,6 @@ public class MarkdownItImpl implements MarkdownIt {
 				$wnd.md.utils.urlWithoutProtocolRE = new RegExp(
 						'^(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)');
 			}
-			if (!$wnd.md.utils.tableClassStartRE) {
-				$wnd.md.utils.tableClassStartRE = new RegExp(
-						'^\\s*{[|]{1}\\s+class\\s*=\\s*"\\s*(.*)"\\s*');
-			}
-			if (!$wnd.md.utils.tableClassEndRE) {
-				$wnd.md.utils.tableClassEndRE = new RegExp('^\\s*[|]{1}}\\s*');
-			}
 		}
 
 		function link(state, silent) {
@@ -263,173 +256,6 @@ public class MarkdownItImpl implements MarkdownIt {
 			state.pos = pos;
 			state.posMax = max;
 			return true;
-		}
-		;
-
-		function getLine(state, line) {
-			var pos = state.bMarks[line] + state.blkIndent, max = state.eMarks[line];
-
-			return state.src.substr(pos, max - pos);
-		}
-
-		function escapedSplit(str) {
-			var result = [], pos = 0, max = str.length, ch, escapes = 0, lastPos = 0, backTicked = false, lastBackTick = 0;
-
-			ch = str.charCodeAt(pos);
-
-			while (pos < max) {
-				if (ch === 0x60 && (escapes % 2 === 0)) { // `
-					backTicked = !backTicked;
-					lastBackTick = pos;
-				} else if (ch === 0x7c && (escapes % 2 === 0) && !backTicked) { // |
-					result.push(str.substring(lastPos, pos));
-					lastPos = pos + 1;
-				} else if (ch === 0x5c) { // \
-					escapes++;
-				} else {
-					escapes = 0;
-				}
-
-				pos++;
-
-				// If there was an un-closed backtick, go back to just after
-				// the last backtick, but as if it was a normal character
-				if (pos === max && backTicked) {
-					backTicked = false;
-					pos = lastBackTick + 1;
-				}
-
-				ch = str.charCodeAt(pos);
-			}
-
-			result.push(str.substring(lastPos));
-
-			return result;
-		}
-
-		function table(state, startLine, endLine, silent) {
-			var ch, lineText, pos, i, nextLine, columns, columnCount, token, t, tableLines, tbodyLines, classNames, tableBodyStartLine, headerLine;
-			// should have at least two lines (!!! Synapse change, used to be 3 due to required ---|---|--- line).  Header and single row.
-			if (startLine + 1 > endLine) {
-				return false;
-			}
-
-			pos = state.bMarks[startLine] + state.tShift[startLine];
-			if (pos >= state.eMarks[startLine]) {
-				return false;
-			}
-			ch = state.src.charCodeAt(pos);
-
-			lineText = getLine(state, startLine);
-			//look for optional class definition start, like '{| class="border"'
-			if ($wnd.md.utils.tableClassStartRE.test(lineText)) {
-				//this table definition includes class names, so the start marker is {| and end marker will be |} 
-				classNames = lineText.match($wnd.md.utils.tableClassStartRE)[1];
-				headerLine = startLine + 1;
-			} else {
-				headerLine = startLine;
-			}
-
-			if (state.sCount[headerLine] < state.blkIndent) {
-				return false;
-			}
-
-			pos = state.bMarks[headerLine] + state.tShift[headerLine];
-			if (pos >= state.eMarks[headerLine]) {
-				return false;
-			}
-
-			//read column headers
-			lineText = getLine(state, headerLine).trim();
-			if (lineText.indexOf('|') === -1) {
-				return false;
-			}
-			columns = escapedSplit(lineText.replace(/^\||\|$/g, ''));
-
-			// header row will define an amount of columns in the entire table,
-			// and align row shouldn't be smaller than that (the rest of the rows can)
-			columnCount = columns.length;
-
-			if (silent) {
-				return true;
-			}
-
-			token = state.push('table_open', 'table', 1);
-			token.map = tableLines = [ startLine, 0 ];
-			if (classNames) {
-				token.attrs = [ [ 'class', ' ' + classNames + ' ' ] ];
-				//start line of the table (header) is really the second line.
-				startLine++;
-			}
-
-			lineText = getLine(state, headerLine + 1).trim();
-
-			//If this line is of the form ---|---|---, then we have column headers and we should skip this line.  Else, no column headers and we should skip to the body.
-			if (/^[-:| ]+$/.test(lineText)) {
-				//we have column headers
-				tableBodyStartLine = headerLine + 2;
-				token = state.push('thead_open', 'thead', 1);
-				token.map = [ startLine, startLine + 1 ];
-
-				token = state.push('tr_open', 'tr', 1);
-				token.map = [ startLine, startLine + 1 ];
-
-				for (i = 0; i < columns.length; i++) {
-					token = state.push('th_open', 'th', 1);
-					token.map = [ startLine, startLine + 1 ];
-
-					token = state.push('inline', '', 0);
-					token.content = columns[i].trim();
-					token.map = [ startLine, startLine + 1 ];
-					token.children = [];
-
-					token = state.push('th_close', 'th', -1);
-				}
-
-				token = state.push('tr_close', 'tr', -1);
-				token = state.push('thead_close', 'thead', -1);
-			} else {
-				//no column headers
-				tableBodyStartLine = headerLine;
-			}
-
-			token = state.push('tbody_open', 'tbody', 1);
-			token.map = tbodyLines = [ tableBodyStartLine, 0 ];
-
-			for (nextLine = tableBodyStartLine; nextLine < endLine; nextLine++) {
-				if (state.sCount[nextLine] < state.blkIndent) {
-					break;
-				}
-
-				lineText = getLine(state, nextLine).trim();
-				if ($wnd.md.utils.tableClassEndRE.test(lineText)) {
-					//end of table with class definitions. Include this line in the table definition
-					nextLine++;
-					break;
-				}
-				if (lineText.indexOf('|') === -1) {
-					break;
-				}
-				columns = escapedSplit(lineText.replace(/^\||\|$/g, ''));
-
-				token = state.push('tr_open', 'tr', 1);
-				for (i = 0; i < columnCount; i++) {
-					token = state.push('td_open', 'td', 1);
-
-					token = state.push('inline', '', 0);
-					token.content = columns[i] ? columns[i].trim() : '';
-					token.children = [];
-
-					token = state.push('td_close', 'td', -1);
-				}
-				token = state.push('tr_close', 'tr', -1);
-			}
-			token = state.push('tbody_close', 'tbody', -1);
-			token = state.push('table_close', 'table', -1);
-
-			tableLines[1] = tbodyLines[1] = nextLine;
-			state.line = nextLine;
-			return true;
 		};
 
 		function initMarkdownIt() {
@@ -441,9 +267,11 @@ public class MarkdownItImpl implements MarkdownIt {
 				typographer: true
 			});
 			$wnd.md.disable([ 'heading' ]);
-			$wnd.md.use($wnd.markdownitSub).use($wnd.markdownitSup).use(
-					$wnd.markdownitCentertext).use(
-					$wnd.markdownitSynapseHeading);
+			$wnd.md.use($wnd.markdownitSub)
+				.use($wnd.markdownitSup)
+				.use($wnd.markdownitCentertext)
+				.use($wnd.markdownitSynapseHeading)
+				.use($wnd.markdownitSynapseTable);
 
 			$wnd.md.set({
 				highlight : function(str, lang) {
@@ -461,9 +289,6 @@ public class MarkdownItImpl implements MarkdownIt {
 			initMarkdownTableStyle();
 			initREs();
 			$wnd.md.inline.ruler.at('link', link);
-			var rulesCanBeTerminated = [ 'paragraph', 'reference' ];
-			$wnd.md.block.ruler.at('table', table, { alt: (rulesCanBeTerminated).slice() });
-			
 		}
 
 		if (!$wnd.md) {
