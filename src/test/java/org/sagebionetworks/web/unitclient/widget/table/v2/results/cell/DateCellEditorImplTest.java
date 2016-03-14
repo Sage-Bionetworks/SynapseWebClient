c5346e201612eddd946018a0ddde1d738e413801
Jay Hodgson <jay.hodgson@sagebase.org>
2016-03-14 14:20:01 -0700

    SWC-2944: keep track of the original value.  if the only thing that changes in the web client editor is the milliseconds, then return the original (revert the change)

--- src/test/java/org/sagebionetworks/web/unitclient/widget/table/v2/results/cell/DateCellEditorImplTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/table/v2/results/cell/DateCellEditorImplTest.java
@@ -52,8 +52,33 @@ public class DateCellEditorImplTest {
 	
 	@Test
 	public void testGetValueReal(){
+		//get value from the editor, setValue is never called.
 		Date old = new Date(123);
 		when(mockView.getValue()).thenReturn(old);
 		assertEquals("123", editor.getValue());
 	}
+	
+	@Test
+	public void testSetValueMilliSecondLoss(){
+		//SWC-2944: Simulate precision loss by the view (Bootstrap extras DateTimePicker to be more specific).
+		String sValue = Long.toString(1457401179123L);
+		editor.setValue(sValue);
+		when(mockView.getValue()).thenReturn(new Date(1457401179000L));
+		
+		//the view has lost the milliseconds, should return the original value
+		assertEquals(sValue, editor.getValue());
+	}
+	
+	@Test
+	public void testSetValueViewHasDifferentDate(){
+		//SWC-2944
+		String sValue = Long.toString(1457401179123L);
+		editor.setValue(sValue);
+		Long newDate = 1457487584000L;
+		when(mockView.getValue()).thenReturn(new Date(newDate));
+		
+		//the user has selected a different value in the view, verify that it is not overwritten by the original value
+		assertEquals(Long.toString(newDate), editor.getValue());
+	}
+
 }
