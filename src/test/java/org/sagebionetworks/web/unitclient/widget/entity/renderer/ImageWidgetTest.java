c7f4e0a1e606f2851d377f2641da5b1fd49894b2
Jay Hodgson <jay.hodgson@sagebase.org>
2016-05-17 11:55:44 -0700

    added tests

--- src/test/java/org/sagebionetworks/web/unitclient/widget/entity/renderer/ImageWidgetTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/renderer/ImageWidgetTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -49,4 +50,24 @@ public class ImageWidgetTest {
 		widget.configure(wikiKey,descriptor, null, null);
 		verify(mockView).configure(any(WikiPageKey.class), anyString(), anyString(), anyString(), anyString(), anyBoolean(), any(Long.class));
 	}
+	
+	@Test
+	public void testConfigureDefaultResponsive() {
+		descriptor.put(WidgetConstants.IMAGE_WIDGET_RESPONSIVE_KEY, null);
+		widget.configure(wikiKey,descriptor, null, null);
+		verify(mockView, never()).addStyleName(ImageWidget.MAX_WIDTH_NONE);
+	}
+	@Test
+	public void testConfigureResponsive() {
+		descriptor.put(WidgetConstants.IMAGE_WIDGET_RESPONSIVE_KEY, Boolean.TRUE.toString());
+		widget.configure(wikiKey,descriptor, null, null);
+		verify(mockView, never()).addStyleName(ImageWidget.MAX_WIDTH_NONE);
+	}
+	@Test
+	public void testConfigureNotResponsive() {
+		descriptor.put(WidgetConstants.IMAGE_WIDGET_RESPONSIVE_KEY, Boolean.FALSE.toString());
+		widget.configure(wikiKey,descriptor, null, null);
+		verify(mockView).addStyleName(ImageWidget.MAX_WIDTH_NONE);
+	}
+	
 }
