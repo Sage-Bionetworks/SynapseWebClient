e7b20b39e4a6323a2b3bc1edd87e55bee748e901
Jay Hodgson <jay.hodgson@sagebase.org>
2016-05-26 11:23:32 -0700

    test table entity creation

--- src/test/java/org/sagebionetworks/web/unitclient/widget/table/modal/fileview/CreateTableViewWizardStep1Test.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/table/modal/fileview/CreateTableViewWizardStep1Test.java
@@ -4,13 +4,16 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static junit.framework.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.repo.model.Entity;
 import org.sagebionetworks.repo.model.table.FileView;
 import org.sagebionetworks.repo.model.table.TableEntity;
 import org.sagebionetworks.web.client.SynapseClientAsync;
@@ -25,7 +28,13 @@ import org.sagebionetworks.web.client.widget.table.modal.wizard.ModalPage.ModalP
 import org.sagebionetworks.web.test.helper.AsyncMockStubber;
 import static org.sagebionetworks.web.client.widget.table.modal.CreateTableModalWidgetImpl.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
+
+
 
 public class CreateTableViewWizardStep1Test {
 
@@ -39,35 +48,62 @@ public class CreateTableViewWizardStep1Test {
 	SynapseClientAsync mockSynapseClient;
 	String parentId;
 	CreateTableViewWizardStep1 widget;
-	
+	List<String> scopeIds;
 	@Before
 	public void before(){
 		MockitoAnnotations.initMocks(this);
 		mockSynapseClient = Mockito.mock(SynapseClientAsync.class);
-		
+		scopeIds = Collections.singletonList("3");
+		when(mockEntityContainerListWidget.getEntityIds()).thenReturn(scopeIds);
 		widget = new CreateTableViewWizardStep1(mockView, mockSynapseClient, mockEntityContainerListWidget);
 		widget.setModalPresenter(mockWizardPresenter);
 		parentId = "syn123";
-		widget.configure(parentId, TableType.view);
 	}
 	
 	@Test
 	public void testNullName(){
+		widget.configure(parentId, TableType.view);
 		when(mockView.getName()).thenReturn(null);
 		widget.onPrimary();
 		verify(mockWizardPresenter).setErrorMessage(CreateTableViewWizardStep1.NAME_MUST_INCLUDE_AT_LEAST_ONE_CHARACTER);
-		verify(mockSynapseClient, never()).createEntity(any(TableEntity.class), any(AsyncCallback.class));
+		verify(mockSynapseClient, never()).createEntity(any(Entity.class), any(AsyncCallback.class));
 	}
 	
 	@Test
-	public void testCreateHappy(){
+	public void testCreateFileView(){
+		widget.configure(parentId, TableType.view);
+		verify(mockView).setScopeWidgetVisible(true);
 		String tableName = "a name";
 		FileView table = new FileView();
 		table.setName(tableName);
 		table.setId("syn57");
-		AsyncMockStubber.callSuccessWith(table).when(mockSynapseClient).createEntity(any(TableEntity.class), any(AsyncCallback.class));
+		ArgumentCaptor<Entity> captor = ArgumentCaptor.forClass(Entity.class);
+		AsyncMockStubber.callSuccessWith(table).when(mockSynapseClient).createEntity(captor.capture(), any(AsyncCallback.class));
 		when(mockView.getName()).thenReturn(tableName);
 		widget.onPrimary();
+		FileView capturedFileView = (FileView)captor.getValue();
+		assertEquals(scopeIds, capturedFileView.getScopeIds());
+		assertEquals(FileView.class.getName(), capturedFileView.getEntityType());
+		verify(mockWizardPresenter, never()).setErrorMessage(anyString());
+		
+		//TODO: go to next step
+		verify(mockWizardPresenter).onFinished();
+	}
+	
+	@Test
+	public void testCreateTable(){
+		widget.configure(parentId, TableType.table);
+		verify(mockView).setScopeWidgetVisible(false);
+		String tableName = "a name";
+		TableEntity table = new TableEntity();
+		table.setName(tableName);
+		table.setId("syn57");
+		ArgumentCaptor<Entity> captor = ArgumentCaptor.forClass(Entity.class);
+		AsyncMockStubber.callSuccessWith(table).when(mockSynapseClient).createEntity(captor.capture(), any(AsyncCallback.class));
+		when(mockView.getName()).thenReturn(tableName);
+		widget.onPrimary();
+		TableEntity capturedTable = (TableEntity)captor.getValue();
+		assertEquals(TableEntity.class.getName(), capturedTable.getEntityType());
 		verify(mockWizardPresenter, never()).setErrorMessage(anyString());
 		
 		//TODO: go to next step
@@ -76,12 +112,10 @@ public class CreateTableViewWizardStep1Test {
 	
 	@Test
 	public void testCreateFailed(){
+		widget.configure(parentId, TableType.view);
 		String tableName = "a name";
-		FileView table = new FileView();
-		table.setName(tableName);
-		table.setId("syn57");
 		String error = "name already exists";
-		AsyncMockStubber.callFailureWith(new Throwable(error)).when(mockSynapseClient).createEntity(any(TableEntity.class), any(AsyncCallback.class));
+		AsyncMockStubber.callFailureWith(new Throwable(error)).when(mockSynapseClient).createEntity(any(Entity.class), any(AsyncCallback.class));
 		when(mockView.getName()).thenReturn(tableName);
 		widget.onPrimary();
 		verify(mockWizardPresenter).setErrorMessage(error);
@@ -89,7 +123,6 @@ public class CreateTableViewWizardStep1Test {
 
 		//TODO: should not go to the next step
 		verify(mockWizardPresenter, never()).onFinished();
-
 	}
 	
 }
