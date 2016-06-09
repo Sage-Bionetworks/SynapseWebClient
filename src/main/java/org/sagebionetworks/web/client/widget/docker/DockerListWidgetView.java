c23a2c5e3037323edfe4ecfd819e376c20f1da8e
Kimyen <kimyen0816@gmail.com>
2016-06-09 14:32:39 -0700

    Empty DockerListWidget

---
+++ src/main/java/org/sagebionetworks/web/client/widget/docker/DockerListWidgetView.java
@@ -0,0 +1,12 @@
+package org.sagebionetworks.web.client.widget.docker;
+
+import org.sagebionetworks.web.client.SynapseView;
+
+import com.google.gwt.user.client.ui.IsWidget;
+
+public interface DockerListWidgetView extends IsWidget, SynapseView {
+
+	public interface Presenter {
+		
+	}
+}
