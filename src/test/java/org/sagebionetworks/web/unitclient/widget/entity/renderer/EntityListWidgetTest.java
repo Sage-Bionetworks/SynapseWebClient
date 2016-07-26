a04fe6ecc298f3eb79374602e9a23b61fb5c8da8
Jay Hodgson <jay.hodgson@sagebase.org>
2016-07-26 15:32:49 -0700

    update renderer test

--- src/test/java/org/sagebionetworks/web/unitclient/widget/entity/renderer/EntityListWidgetTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/renderer/EntityListWidgetTest.java
@@ -89,10 +89,26 @@ public class EntityListWidgetTest {
 		records.add(record456);			
 		String encoded = EntityListUtil.recordsToString(records);
 		descriptor.put(WidgetConstants.ENTITYLIST_WIDGET_LIST_KEY, encoded);
-				
-		widget.configure(null, descriptor, null, null);
 		
-		verify(mockView).configure();	
+		widget.configure(null, descriptor, null, null);
+		//expect to show the description if no param is specified (backwards compatible)
+		boolean showDescription = true;
+		verify(mockView).configure(showDescription);	
+		verify(mockView).setEntityGroupRecordDisplay(eq(0), any(EntityGroupRecordDisplay.class), eq(true));
+	}
+	
+	@Test
+	public void testConfigureHideDescription() {		
+		List<EntityGroupRecord> records = new ArrayList<EntityGroupRecord>();
+		records.add(record456);			
+		String encoded = EntityListUtil.recordsToString(records);
+		descriptor.put(WidgetConstants.ENTITYLIST_WIDGET_LIST_KEY, encoded);
+		descriptor.put(WidgetConstants.ENTITYLIST_WIDGET_SHOW_DESCRIPTION_KEY, Boolean.FALSE.toString());
+		
+		widget.configure(null, descriptor, null, null);
+		//expect to show the description if no param is specified (backwards compatible)
+		boolean showDescription = false;
+		verify(mockView).configure(showDescription);	
 		verify(mockView).setEntityGroupRecordDisplay(eq(0), any(EntityGroupRecordDisplay.class), eq(true));
 	}
 	
