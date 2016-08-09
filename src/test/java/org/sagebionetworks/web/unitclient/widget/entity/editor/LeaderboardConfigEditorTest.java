cdeb0939882dcff5e1951ff76d33bd85d35dc89a
Jay Hodgson <jay.hodgson@sagebase.org>
2016-08-09 09:27:51 -0700

    test leaderboard editor.  minimal, as most calls are pass through

---
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/editor/LeaderboardConfigEditorTest.java
@@ -0,0 +1,24 @@
+package org.sagebionetworks.web.unitclient.widget.entity.editor;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.web.client.ClientProperties;
+import org.sagebionetworks.web.client.widget.entity.editor.LeaderboardConfigEditor;
+import org.sagebionetworks.web.client.widget.entity.editor.QueryTableConfigEditor;
+
+public class LeaderboardConfigEditorTest {
+		
+	LeaderboardConfigEditor editor;
+	@Mock
+	QueryTableConfigEditor mockQueryTableEditor;
+	
+	@Test
+	public void testSetServicePrefix(){
+		MockitoAnnotations.initMocks(this);
+		editor = new LeaderboardConfigEditor(mockQueryTableEditor);
+		verify(mockQueryTableEditor).setServicePrefix(ClientProperties.EVALUATION_QUERY_SERVICE_PREFIX);
+	}
+}
