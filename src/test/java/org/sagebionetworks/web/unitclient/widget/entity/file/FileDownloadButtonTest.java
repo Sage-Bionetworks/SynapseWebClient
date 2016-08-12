0cc96caa1877239b43dc24c1f1e6558d3adf7da3
Jay Hodgson <jay.hodgson@sagebase.org>
2016-08-12 13:53:14 -0700

    tests in progress

---
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/file/FileDownloadButtonTest.java
@@ -0,0 +1,50 @@
+package org.sagebionetworks.web.unitclient.widget.entity.file;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.web.client.SynapseClientAsync;
+
+public class FileDownloadButtonTest {
+	@Mock
+	SynapseClientAsync mockSynapseClient;
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testConfigure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFireEntityUpdatedEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryForSftpLoginInstructions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOnLicensedDownloadClick() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOnAuthorizedDirectDownloadClicked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSize() {
+		fail("Not yet implemented");
+	}
+
+}
