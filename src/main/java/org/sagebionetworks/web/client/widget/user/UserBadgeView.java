770f80d1efa963e7cd9691c57785e49034f7191a
Jay Hodgson <jay.hodgson@sagebase.org>
2016-09-14 14:44:13 -0700

    lazy load user badge

--- src/main/java/org/sagebionetworks/web/client/widget/user/UserBadgeView.java
+++ src/main/java/org/sagebionetworks/web/client/widget/user/UserBadgeView.java
@@ -1,11 +1,12 @@
 package org.sagebionetworks.web.client.widget.user;
 
 import org.sagebionetworks.web.client.SynapseView;
+import org.sagebionetworks.web.client.widget.lazyload.SupportsLazyLoadInterface;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.IsWidget;
 
-public interface UserBadgeView extends IsWidget, SynapseView {
+public interface UserBadgeView extends IsWidget, SynapseView, SupportsLazyLoadInterface {
 
 	/**
 	 * Set the presenter.
