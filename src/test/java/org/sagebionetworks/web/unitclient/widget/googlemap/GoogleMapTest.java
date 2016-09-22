47c50a3d03a1e58640534981e553c2ef8d1b8ee1
Jay Hodgson <jay.hodgson@sagebase.org>
2016-09-21 17:40:40 -0700

    add Google map tests

--- src/test/java/org/sagebionetworks/web/unitclient/widget/googlemap/GoogleMapTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/googlemap/GoogleMapTest.java
@@ -17,11 +17,12 @@ import org.sagebionetworks.web.client.widget.entity.controller.SynapseAlert;
 import org.sagebionetworks.web.client.widget.googlemap.GoogleMap;
 import org.sagebionetworks.web.client.widget.googlemap.GoogleMapView;
 import org.sagebionetworks.web.client.widget.lazyload.LazyLoadHelper;
-import org.sagebionetworks.web.shared.WebConstants;
+import org.sagebionetworks.web.test.helper.RequestBuilderMockStubber;
 
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.ui.Widget;
 
 public class GoogleMapTest {
@@ -31,18 +32,21 @@ public class GoogleMapTest {
 	@Mock
 	SynapseJSNIUtils mockSynapseJSNIUtils;
 	@Mock
-	RequestBuilderWrapper mockRequestBuilderWrapper;
+	RequestBuilderWrapper mockRequestBuilder;
 	@Mock
 	SynapseAlert mockSynapseAlert;
 	@Mock
 	PortalGinInjector mockPortalGinInjector;
 	@Mock
 	LazyLoadHelper mockLazyLoadHelper;
+	@Mock
+	Response mockResponse;
 	
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		map = new GoogleMap(mockView, mockSynapseJSNIUtils, mockRequestBuilderWrapper, mockSynapseAlert, mockPortalGinInjector, mockLazyLoadHelper);
+		GoogleMap.isLoaded = true;
+		map = new GoogleMap(mockView, mockSynapseJSNIUtils, mockRequestBuilder, mockSynapseAlert, mockPortalGinInjector, mockLazyLoadHelper);
 	}
 
 	@Test
@@ -59,15 +63,60 @@ public class GoogleMapTest {
 	}
 	
 	@Test
-	public void testConfigure() throws RequestException {
+	public void testConfigureNotLoaded() throws RequestException {
+		GoogleMap.isLoaded = false;
 		map.configure();
 		verify(mockLazyLoadHelper).setIsConfigured();
-		verifyZeroInteractions(mockRequestBuilderWrapper);
+		verifyZeroInteractions(mockRequestBuilder);
 		simulateInView();
-		//verify attempt to load script
-		verify(mockRequestBuilderWrapper).configure(RequestBuilder.GET, GoogleMap.GOOGLE_MAP_URL);
-		verify(mockRequestBuilderWrapper).sendRequest(anyString(), any(RequestCallback.class));
-		reset(mockRequestBuilderWrapper);
+		//verify attempt to load
+		verify(mockRequestBuilder).configure(RequestBuilder.GET, GoogleMap.GOOGLE_MAP_URL);
+		verify(mockRequestBuilder).sendRequest(anyString(), any(RequestCallback.class));
 	}
 	
+	@Test
+	public void testConfigure() throws RequestException {
+		map.configure();
+		
+		when(mockResponse.getStatusCode()).thenReturn(Response.SC_OK);
+		String data = "map data";
+		when(mockResponse.getText()).thenReturn(data);
+		RequestBuilderMockStubber.callOnResponseReceived(null, mockResponse)
+			.when(mockRequestBuilder).sendRequest(anyString(), any(RequestCallback.class));
+
+		simulateInView();
+		//verify attempt to load all data
+		verify(mockRequestBuilder).configure(RequestBuilder.GET, GoogleMap.ALL_POINTS_URL);
+		verify(mockRequestBuilder).sendRequest(anyString(), any(RequestCallback.class));
+		verify(mockView).showMap(data);
+	}
+	
+	@Test
+	public void testConfigureTeam() throws RequestException {
+		String teamId = "1234987";
+		map.configure(teamId);
+		
+		when(mockResponse.getStatusCode()).thenReturn(Response.SC_OK);
+		String data = "map data";
+		when(mockResponse.getText()).thenReturn(data);
+		RequestBuilderMockStubber.callOnResponseReceived(null, mockResponse)
+			.when(mockRequestBuilder).sendRequest(anyString(), any(RequestCallback.class));
+
+		simulateInView();
+		//verify attempt to load team data
+		String expectedTeamMapDataUrl = GoogleMap.S3_PREFIX + teamId + ".json";
+		verify(mockRequestBuilder).configure(RequestBuilder.GET, expectedTeamMapDataUrl);
+		verify(mockRequestBuilder).sendRequest(anyString(), any(RequestCallback.class));
+		verify(mockView).showMap(data);
+	}
+	
+	@Test
+	public void testFailedRequest() throws RequestException {
+		Exception ex = new Exception("failure to load");
+		RequestBuilderMockStubber.callOnError(null, ex)
+			.when(mockRequestBuilder).sendRequest(anyString(), any(RequestCallback.class));
+		map.configure();
+		simulateInView();
+		verify(mockSynapseAlert).handleException(ex);
+	}
 }
