a6a3208c2f9f8fc37f7e97c616973ef2caf9b84e
Jay Hodgson <jay.hodgson@sagebase.org>
2016-10-10 15:11:11 -0700

    remove S3FileCopy request and response async job objects and update synapse repo version

--- src/main/java/org/sagebionetworks/web/shared/asynch/AsynchType.java
+++ src/main/java/org/sagebionetworks/web/shared/asynch/AsynchType.java
@@ -4,10 +4,8 @@ import org.sagebionetworks.repo.model.asynch.AsynchronousRequestBody;
 import org.sagebionetworks.repo.model.asynch.AsynchronousResponseBody;
 import org.sagebionetworks.repo.model.file.BulkFileDownloadRequest;
 import org.sagebionetworks.repo.model.file.BulkFileDownloadResponse;
-import org.sagebionetworks.repo.model.file.S3FileCopyRequest;
-import org.sagebionetworks.repo.model.file.S3FileCopyResults;
 import org.sagebionetworks.repo.model.table.AppendableRowSetRequest;
-import org.sagebionetworks.repo.model.table.*;
+import org.sagebionetworks.repo.model.table.DownloadFromTableRequest;
 import org.sagebionetworks.repo.model.table.DownloadFromTableResult;
 import org.sagebionetworks.repo.model.table.QueryBundleRequest;
 import org.sagebionetworks.repo.model.table.QueryNextPageToken;
@@ -36,7 +34,6 @@ public enum AsynchType implements IsSerializable{
 	TableCSVUpload(UploadToTableRequest.class, UploadToTableResult.class),
 	TableCSVUploadPreview(UploadToTablePreviewRequest.class, UploadToTablePreviewResult.class),
 	TableCSVDownload(DownloadFromTableRequest.class, DownloadFromTableResult.class),
-	S3FileCopy(S3FileCopyRequest.class, S3FileCopyResults.class),
 	BulkFileDownload(BulkFileDownloadRequest.class,BulkFileDownloadResponse.class),
 	TableTransaction(TableUpdateTransactionRequest.class, TableUpdateTransactionResponse.class);
 	
