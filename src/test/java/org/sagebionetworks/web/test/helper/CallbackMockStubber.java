4974445772d67bc24269f960d745b8ce2502920d
Jay Hodgson <jay.hodgson@sagebase.org>
2016-10-24 10:00:10 -0700

    functional cancel control widget, with new helper class to invoke callback

---
+++ src/test/java/org/sagebionetworks/web/test/helper/CallbackMockStubber.java
@@ -0,0 +1,26 @@
+package org.sagebionetworks.web.test.helper;
+
+import static org.mockito.Mockito.doAnswer;
+
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.mockito.stubbing.Stubber;
+import org.sagebionetworks.web.client.utils.Callback;
+
+public class CallbackMockStubber {
+	public static Stubber invokeCallback() {
+		return doAnswer(new Answer<Void>() {
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				final Object[] args = invocation.getArguments();
+				for (Object arg : args) {
+					if (arg instanceof Callback) {
+						((Callback) arg).invoke();
+						return null;
+					}
+				}
+				return null;
+			}
+		});
+	}
+}
