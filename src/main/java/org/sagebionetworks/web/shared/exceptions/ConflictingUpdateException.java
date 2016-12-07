ba349196836d70a2b2cf29dfbf46df82a160c026
Karen Altergott <kmaltergott@gmail.com>
2016-12-07 14:41:22 -0800

    Strange issue

--- src/main/java/org/sagebionetworks/web/shared/exceptions/ConflictingUpdateException.java
+++ src/main/java/org/sagebionetworks/web/shared/exceptions/ConflictingUpdateException.java
@@ -1,6 +1,8 @@
 package org.sagebionetworks.web.shared.exceptions;
 
-public class ConflictingUpdateException extends RestServiceException {
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class ConflictingUpdateException extends RestServiceException implements IsSerializable {
 	
 	private static final long serialVersionUID = 1L;
 
