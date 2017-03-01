f2f7fd097e6f12330852b737ec946c9e31928825
Karen Altergott <kmaltergott@gmail.com>
2017-03-01 14:46:46 -0800

    Added files for Project Display; added constant for tools menu

---
+++ src/main/java/org/sagebionetworks/web/client/widget/display/ProjectDisplay.java
@@ -0,0 +1,71 @@
+package org.sagebionetworks.web.client.widget.display;
+
+import org.sagebionetworks.repo.model.EntityBundle;
+import org.sagebionetworks.web.client.SynapseClientAsync;
+import org.sagebionetworks.web.client.cookie.CookieProvider;
+import org.sagebionetworks.web.client.events.EntityUpdatedHandler;
+import org.sagebionetworks.web.client.widget.entity.controller.SynapseAlert;
+
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+
+public class ProjectDisplay implements ProjectDisplayView {
+	ProjectDisplayView view;
+	SynapseClientAsync synapseClient;
+	SynapseAlert synAlert;
+	EntityUpdatedHandler entityUpdatedHandler;
+	EntityBundle entityBundle;
+	CookieProvider cookies;
+	
+	@Inject
+	public ProjectDisplay(ProjectDisplayView view,
+			SynapseClientAsync synapseClient, 
+			SynapseAlert synAlert, 
+			CookieProvider cookies) {
+		this.view = view;
+		this.synapseClient = synapseClient;
+		this.synAlert = synAlert;
+		this.cookies = cookies;
+		view.setSynAlertWidget(synAlert);
+	}
+	
+	
+	@Override
+	public Widget asWidget() {
+		return view.asWidget();
+	}
+	
+	public void show() {
+		view.show();
+	}
+	
+	public void hide() {
+		view.hide();
+	}
+	
+	@Override
+	public void clear() {
+		synAlert.clear();
+		view.clear();
+	}
+
+	@Override
+	public void onSave() {
+		synAlert.clear();
+	}
+
+
+	@Override
+	public void setSynAlertWidget(IsWidget asWidget) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setPresenter(Presenter presenter) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/main/java/org/sagebionetworks/web/client/widget/display/ProjectDisplayViewImpl.java
@@ -0,0 +1,102 @@
+package org.sagebionetworks.web.client.widget.display;
+
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.Modal;
+import org.gwtbootstrap3.client.ui.Radio;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+
+public class ProjectDisplayViewImpl implements ProjectDisplayView {
+	
+	public interface ProjectDisplayViewImplUiBinder 
+			extends UiBinder<Widget, ProjectDisplayViewImpl> {}
+	
+	@UiField
+	Modal modal;
+	
+	@UiField
+	SimplePanel synAlertPanel;
+	
+	@UiField
+	Radio wikiButton;
+	@UiField
+	Radio filesButton;
+	@UiField
+	Radio tablesButton;
+	@UiField
+	Radio challengeButton;
+	@UiField
+	Radio discussionButton;
+	
+	@UiField
+	Button saveButton;
+	
+	@UiField
+	Button cancelButton;
+	
+	
+	Widget widget;
+	Presenter presenter;
+	
+	@Inject
+	public ProjectDisplayViewImpl(ProjectDisplayViewImplUiBinder binder) {
+		widget = binder.createAndBindUi(this);
+		saveButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				presenter.onSave();
+			}
+		});
+		cancelButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				modal.hide();
+			}
+		});
+		
+	}
+	
+	@Override
+	public Widget asWidget() {
+		return widget;
+	}
+	
+	@Override
+	public void setSynAlertWidget(IsWidget synAlert) {
+		synAlertPanel.setWidget(synAlert);
+	}
+	
+	@Override
+	public void setPresenter(Presenter presenter) {
+		this.presenter = presenter;
+	}
+	
+	@Override
+	public void clear() {
+		
+	}
+	
+	@Override
+	public void hide() {
+		modal.hide();
+	}
+	
+	@Override
+	public void show() {
+		modal.show();
+	}
+
+	@Override
+	public void onSave() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
