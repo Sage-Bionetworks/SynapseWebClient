893dfe009dadf8bcba0cb7d69b1de9af7e1c47c7
Jay Hodgson <jay.hodgson@sagebase.org>
2017-03-17 10:08:41 -0700

    test create access requirement button

--- src/main/java/org/sagebionetworks/web/client/widget/accessrequirements/CreateAccessRequirementButton.java
+++ src/main/java/org/sagebionetworks/web/client/widget/accessrequirements/CreateAccessRequirementButton.java
@@ -15,6 +15,8 @@ import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
 public class CreateAccessRequirementButton implements IsWidget {
+	public static final String CREATE_NEW_ACCESS_REQUIREMENT_BUTTON_TEXT = "Create New Access Requirement";
+	public static final String EDIT_ACCESS_REQUIREMENT_BUTTON_TEXT = "Edit Access Requirement";
 	public Button button;
 	public IsACTMemberAsyncHandler isACTMemberAsyncHandler;
 	public PortalGinInjector ginInjector;
@@ -29,9 +31,7 @@ public class CreateAccessRequirementButton implements IsWidget {
 		this.isACTMemberAsyncHandler = isACTMemberAsyncHandler;
 		this.ginInjector = ginInjector;
 		button.setVisible(false);
-		
 		button.addStyleName("margin-left-10");
-		
 		button.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
@@ -41,21 +41,20 @@ public class CreateAccessRequirementButton implements IsWidget {
 				} else if (ar != null) {
 					wizard.configure(ar);
 				}
-				
 				wizard.showModal(null);
 			}
 		});
 	}	
 	
 	public void configure(AccessRequirement ar) {
-		button.setText("Edit Access Requirement");
+		button.setText(EDIT_ACCESS_REQUIREMENT_BUTTON_TEXT);
 		this.subject = null;
 		this.ar = ar;
 		showIfACTMember();
 	}
 	
 	public void configure(RestrictableObjectDescriptor subject) {
-		button.setText("Create New Access Requirement");
+		button.setText(CREATE_NEW_ACCESS_REQUIREMENT_BUTTON_TEXT);
 		this.subject = subject;
 		this.ar = null;
 		showIfACTMember();
@@ -63,7 +62,6 @@ public class CreateAccessRequirementButton implements IsWidget {
 	
 	private void showIfACTMember() {
 		isACTMemberAsyncHandler.isACTMember(new CallbackP<Boolean>() {
-			
 			@Override
 			public void invoke(Boolean isACTMember) {
 				button.setVisible(isACTMember);
