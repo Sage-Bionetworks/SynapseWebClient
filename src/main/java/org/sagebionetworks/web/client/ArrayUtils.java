10675a06d6040af5b4d3003636ba8ebb95394885
Jay Hodgson <jay.hodgson@sagebase.org>
2017-05-15 14:50:43 -0700

    clean imports

--- src/main/java/org/sagebionetworks/web/client/ArrayUtils.java
+++ src/main/java/org/sagebionetworks/web/client/ArrayUtils.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class ArrayUtils {
 
 	public static double[] getDoubleArray(List<String> l) {
+		if (l == null) {
+			return null;
+		}
 		double[] d = new double[l.size()];
 		for (int i = 0; i < l.size(); i++) {
 			d[i] = Double.valueOf(l.get(i));
@@ -13,6 +16,9 @@ public class ArrayUtils {
 	}
 
 	public static String[] getStringArray(List<String> l) {
+		if (l == null) {
+			return null;
+		}
 		String[] d = new String[l.size()];
 		for (int i = 0; i < l.size(); i++) {
 			d[i] = l.get(i);
