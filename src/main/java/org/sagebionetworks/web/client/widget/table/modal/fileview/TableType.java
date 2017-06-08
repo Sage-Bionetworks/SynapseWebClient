3bd6dc95373987c19a9624054d246466139971ca
Jay Hodgson <jay.hodgson@sagebase.org>
2017-06-08 16:06:32 -0700

    correct indentation

--- src/main/java/org/sagebionetworks/web/client/widget/table/modal/fileview/TableType.java
+++ src/main/java/org/sagebionetworks/web/client/widget/table/modal/fileview/TableType.java
@@ -3,17 +3,17 @@ package org.sagebionetworks.web.client.widget.table.modal.fileview;
 import org.sagebionetworks.repo.model.table.ViewType;
 
 public enum TableType {
-    table(null),
-    fileview(ViewType.file),
-    projectview(ViewType.project);
+	table(null),
+	fileview(ViewType.file),
+	projectview(ViewType.project);
 
 	private ViewType viewType;
-	
-    TableType(ViewType viewType) {
-    	this.viewType = viewType;
-    }
-    
-    public ViewType getViewType() {
+
+	TableType(ViewType viewType) {
+		this.viewType = viewType;
+	}
+
+	public ViewType getViewType() {
 		return viewType;
 	}
 }
