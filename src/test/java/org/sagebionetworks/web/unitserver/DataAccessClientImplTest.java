6fedea412afe8a849a3bf7b45574ddd3f655e711
Jay Hodgson <jay.hodgson@sagebase.org>
2017-07-07 12:28:31 -0700

    SWC-3701

--- src/test/java/org/sagebionetworks/web/unitserver/DataAccessClientImplTest.java
+++ src/test/java/org/sagebionetworks/web/unitserver/DataAccessClientImplTest.java
@@ -1,20 +1,14 @@
 package org.sagebionetworks.web.unitserver;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.sagebionetworks.client.SynapseClient;
 import org.sagebionetworks.client.exceptions.SynapseException;
-import org.sagebionetworks.repo.model.dataaccess.AccessRequirementStatus;
 import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
 import org.sagebionetworks.web.server.servlet.DataAccessClientImpl;
 import org.sagebionetworks.web.server.servlet.ServiceUrlProvider;
@@ -31,12 +25,7 @@ public class DataAccessClientImplTest {
 	ServiceUrlProvider mockUrlProvider;
 	@Mock
 	SynapseClient mockSynapse;
-	@Mock
-	AccessRequirementStatus mockAccessRequirementStatus;
 	DataAccessClientImpl dataAccessClient;
-	public static final String AR_ID1 = "8765";
-	public static final String AR_ID2 = "5678";
-	public static final boolean IS_APPROVED = false;
 	@Before
 	public void before() throws SynapseException, JSONObjectAdapterException {
 		MockitoAnnotations.initMocks(this);
@@ -45,20 +34,10 @@ public class DataAccessClientImplTest {
 		dataAccessClient.setTokenProvider(mockTokenProvider);
 		dataAccessClient.setServiceUrlProvider(mockUrlProvider);
 		when(mockSynapseProvider.createNewClient()).thenReturn(mockSynapse);
-		when(mockSynapse.getAccessRequirementStatus(anyString())).thenReturn(mockAccessRequirementStatus);
-		when(mockAccessRequirementStatus.getIsApproved()).thenReturn(IS_APPROVED);
 	}
 	
-	@Test
-	public void testGetAccessRequirementStatus() throws RestServiceException, SynapseException {
-		List<String> accessRequirementIds = new ArrayList<String>();
-		accessRequirementIds.add(AR_ID1);
-		accessRequirementIds.add(AR_ID2);
-		List<Boolean> response = dataAccessClient.getAccessRequirementStatus(accessRequirementIds);
-		verify(mockSynapse).getAccessRequirementStatus(AR_ID1);
-		verify(mockSynapse).getAccessRequirementStatus(AR_ID1);
-		for (Boolean isApproved : response) {
-			assertEquals(IS_APPROVED, isApproved);
-		}
-	}
+//	TODO: add test for your method if it's more than a thin wrapper.
+//	@Test
+//	public void test() throws RestServiceException, SynapseException {
+//	}
 }
