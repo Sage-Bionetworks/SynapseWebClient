08d0ed937a4c71a0a32929da5c1be89e42732ccc
Jay Hodgson <jay.hodgson@sagebase.org>
2017-07-24 14:39:00 -0700

    SWC-3494: have predictable behavior after deletion of team.

--- src/test/java/org/sagebionetworks/web/unitclient/widget/entity/team/controller/TeamDeleteModalWidgetTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/team/controller/TeamDeleteModalWidgetTest.java
@@ -1,19 +1,26 @@
 package org.sagebionetworks.web.unitclient.widget.entity.team.controller;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.sagebionetworks.repo.model.Team;
 import org.sagebionetworks.web.client.GlobalApplicationState;
+import org.sagebionetworks.web.client.PlaceChanger;
 import org.sagebionetworks.web.client.SynapseClientAsync;
+import org.sagebionetworks.web.client.place.Profile;
+import org.sagebionetworks.web.client.place.Synapse.ProfileArea;
 import org.sagebionetworks.web.client.security.AuthenticationController;
 import org.sagebionetworks.web.client.utils.Callback;
 import org.sagebionetworks.web.client.widget.entity.controller.SynapseAlert;
@@ -23,35 +30,51 @@ import org.sagebionetworks.web.client.widget.team.controller.TeamLeaveModalWidge
 import org.sagebionetworks.web.client.widget.team.controller.TeamLeaveModalWidgetView;
 import org.sagebionetworks.web.test.helper.AsyncMockStubber;
 
+import com.google.gwt.place.shared.Place;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 
 public class TeamDeleteModalWidgetTest {
 
 	TeamDeleteModalWidget presenter;
+	@Mock
 	SynapseAlert mockSynAlert;
-	GlobalApplicationState mockGlobalApplicationState;
+	@Mock
 	SynapseClientAsync mockSynapseClient;
+	@Mock
 	TeamDeleteModalWidgetView mockView;
+	@Mock
 	Callback mockRefreshCallback;
+	@Mock
+	PlaceChanger mockPlaceChanger;
+	@Mock
 	Team mockTeam;
 	
 	String userId = "userId";
 	String teamId = "teamId";
 	Exception caught = new Exception("this is an exception");
-
+	
+	@Mock
+	AuthenticationController mockAuthController;
+	@Mock
+	GlobalApplicationState mockGlobalApplicationState;
+	@Captor
+	ArgumentCaptor<Place> placeCaptor;
+	
 	@Before
 	public void setup() {
-		mockSynAlert = mock(SynapseAlert.class);
-		mockGlobalApplicationState = mock(GlobalApplicationState.class);
-		mockSynapseClient = mock(SynapseClientAsync.class);
-		mockView = mock(TeamDeleteModalWidgetView.class);
+		MockitoAnnotations.initMocks(this);
 		mockRefreshCallback = mock(Callback.class);
-		mockTeam = mock(Team.class);
 		when(mockTeam.getId()).thenReturn(teamId);
-		presenter = new TeamDeleteModalWidget(mockSynAlert, mockSynapseClient, mockGlobalApplicationState, mockView);
+		presenter = new TeamDeleteModalWidget(mockSynAlert, 
+				mockSynapseClient, 
+				mockGlobalApplicationState, 
+				mockView, 
+				mockAuthController);
 		presenter.setRefreshCallback(mockRefreshCallback);
 		presenter.configure(mockTeam);
+		when(mockGlobalApplicationState.getPlaceChanger()).thenReturn(mockPlaceChanger);
+		when(mockAuthController.getCurrentUserPrincipalId()).thenReturn(userId);
 	}
 	
 	@Test
@@ -68,7 +91,11 @@ public class TeamDeleteModalWidgetTest {
 		verify(mockSynAlert).clear();
 		verify(mockSynapseClient).deleteTeam(eq(teamId), any(AsyncCallback.class));
 		verify(mockView).showInfo(anyString(), anyString());
-		verify(mockGlobalApplicationState).gotoLastPlace();
+		verify(mockPlaceChanger).goTo(placeCaptor.capture());
+		Place gotoPlace = placeCaptor.getValue();
+		assertTrue(gotoPlace instanceof Profile);
+		assertEquals(userId, ((Profile)gotoPlace).getUserId());
+		assertEquals(ProfileArea.TEAMS, ((Profile)gotoPlace).getArea());
 	}
 	
 	@Test
