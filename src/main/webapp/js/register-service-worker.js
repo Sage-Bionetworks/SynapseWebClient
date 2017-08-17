f2ed7a26536fe010b5c204604a5ce88ba977d0d2
Jay Hodgson <jay.hodgson@sagebase.org>
2017-08-17 15:37:51 -0700

    fix uncaught js exception, move initial loading UI up (above rootPanel), and remove web worker

--- src/main/webapp/js/register-service-worker.js
+++
@@ -1,7 +0,0 @@
-if (navigator.serviceWorker) {
-	navigator.serviceWorker.register('/sw.js').then(function(registration) {
-		console.log('ServiceWorker registration successful with scope:',  registration.scope);
-	}).catch(function(error) {
-		console.log('ServiceWorker registration failed:', error);
-	});
-}
