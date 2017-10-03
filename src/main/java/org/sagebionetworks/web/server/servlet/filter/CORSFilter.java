9cb52fbbad1c015866767456acb4d7a5b284de25
Jay Hodgson <jay.hodgson@sagebase.org>
2017-10-02 18:18:58 -0700

    format

--- src/main/java/org/sagebionetworks/web/server/servlet/filter/CORSFilter.java
+++ src/main/java/org/sagebionetworks/web/server/servlet/filter/CORSFilter.java
@@ -7,43 +7,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
- * Enabling CORS support  - Access-Control-Allow-Origin
- * @author zeroows@gmail.com
- * 
- * <code>
- 	<!-- Add this to your web.xml to enable "CORS" -->
-	<filter>
-	  <filter-name>cors</filter-name>
-	  <filter-class>org.sagebionetworks.web.server.servlet.filter.CORSFilter</filter-class>
-	</filter>
-	  
-	<filter-mapping>
-	  <filter-name>cors</filter-name>
-	  <url-pattern>/*</url-pattern>
-	</filter-mapping>
- * </code>
+ * CORS filter, Access-Control-Allow-Origin
  */
 public class CORSFilter extends OncePerRequestFilter {
-	private static final Log LOG = LogFactory.getLog(CORSFilter.class);
-
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
-		
+
 		if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
-			LOG.trace("Sending Header....");
-			// CORS "pre-flight" request
 			response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//			response.addHeader("Access-Control-Allow-Headers", "Authorization");
-            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+			// response.addHeader("Access-Control-Allow-Headers",
+			// "Authorization");
+			response.addHeader("Access-Control-Allow-Headers", "Content-Type");
 			response.addHeader("Access-Control-Max-Age", "1");
 		}
-		
+
 		filterChain.doFilter(request, response);
 	}
 
