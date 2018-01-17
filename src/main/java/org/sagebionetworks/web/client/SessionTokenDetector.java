331bff7b166779cfe2737c1018f5b5705078c0b4
Jay Hodgson <jay.hodgson@sagebase.org>
2018-01-16 17:22:13 -0800

    use gwt wrapper for tests

--- src/main/java/org/sagebionetworks/web/client/SessionTokenDetector.java
+++ src/main/java/org/sagebionetworks/web/client/SessionTokenDetector.java
@@ -6,25 +6,26 @@ import org.sagebionetworks.web.client.cookie.CookieKeys;
 import org.sagebionetworks.web.client.cookie.CookieProvider;
 import org.sagebionetworks.web.client.security.AuthenticationController;
 
-import com.google.gwt.user.client.Timer;
 import com.google.inject.Inject;
 
 public class SessionTokenDetector {
 
-	private static final int INTERVAL_MS = 1000*5; //check every 5 seconds
-	Timer timer;
+	private static final int INTERVAL_MS = 1000*10; //check every 10 seconds
 	CookieProvider cookies;
 	AuthenticationController authController;
 	GlobalApplicationState globalAppState;
+	GWTWrapper gwt;
 	boolean isSessionToken;
 	@Inject
 	public SessionTokenDetector(
 			CookieProvider cookies, 
 			AuthenticationController authController,
-			GlobalApplicationState globalAppState) {
+			GlobalApplicationState globalAppState,
+			GWTWrapper gwt) {
 		this.cookies = cookies;
 		this.authController = authController;
 		this.globalAppState = globalAppState;
+		this.gwt = gwt;
 		initializeSessionTokenState();
 	}
 	
@@ -32,22 +33,18 @@ public class SessionTokenDetector {
 		return cookies.getCookie(CookieKeys.USER_LOGIN_TOKEN) != null;
 	}
 	public void start() {
-		timer  = new Timer() {	
-			@Override
-			public void run() {
-				boolean isNowSessionToken = isSessionTokenCookie();
-				if (!Objects.equals(isSessionToken, isNowSessionToken)) {
-					// session token state mismatch, update the app state (by loading the current session data, or clearing) and refresh the page (not an app reload).
-					if (isNowSessionToken) {
-						authController.reloadUserSessionData();
-					} else {
-						authController.logoutUser();
-					}
-					globalAppState.refreshPage();
+		gwt.scheduleFixedDelay(() -> {
+			boolean isNowSessionToken = isSessionTokenCookie();
+			if (!Objects.equals(isSessionToken, isNowSessionToken)) {
+				// session token state mismatch, update the app state (by loading the current session data, or clearing) and refresh the page (not an app reload).
+				if (isNowSessionToken) {
+					authController.reloadUserSessionData();
+				} else {
+					authController.logoutUser();
 				}
+				globalAppState.refreshPage();
 			}
-		};
-		timer.scheduleRepeating(INTERVAL_MS);		
+		}, INTERVAL_MS);
 	}
 	
 	public void initializeSessionTokenState() {
