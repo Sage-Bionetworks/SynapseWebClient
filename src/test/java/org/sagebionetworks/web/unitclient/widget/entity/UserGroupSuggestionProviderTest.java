27aee59a41f71ea1d25b0a4b23e06e9f2a0533d1
Jay Hodgson <jay.hodgson@sagebase.org>
2018-02-27 15:14:52 -0800

    SWC-3993:  filter out anonymous, public, all authenticated users from search results

--- src/test/java/org/sagebionetworks/web/unitclient/widget/entity/UserGroupSuggestionProviderTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/UserGroupSuggestionProviderTest.java
@@ -2,11 +2,11 @@ package org.sagebionetworks.web.unitclient.widget.entity;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +14,16 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.sagebionetworks.repo.model.UserGroupHeader;
 import org.sagebionetworks.repo.model.UserGroupHeaderResponsePage;
 import org.sagebionetworks.repo.model.principal.TypeFilter;
+import org.sagebionetworks.web.client.GlobalApplicationState;
 import org.sagebionetworks.web.client.SynapseJavascriptClient;
 import org.sagebionetworks.web.client.widget.search.SynapseSuggestionBundle;
 import org.sagebionetworks.web.client.widget.search.UserGroupSuggestionProvider;
+import org.sagebionetworks.web.shared.PublicPrincipalIds;
 import org.sagebionetworks.web.test.helper.AsyncMockStubber;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -27,20 +31,29 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public class UserGroupSuggestionProviderTest {
 
 	UserGroupSuggestionProvider presenter;
+	@Mock
 	SynapseJavascriptClient mockSynapseJavascriptClient;
+	@Mock
 	AsyncCallback<SynapseSuggestionBundle> mockCallback;
-
+	@Mock
+	GlobalApplicationState mockGlobalApplicationState;
+	@Mock
+	PublicPrincipalIds mockPublicPrincipalIds;
 	int offset = 0;
 	int pageSize = 10;
 	int width = 568;
 	String prefix = "test";
-	
-	
+	public static final Long ANONYMOUS_USER_ID = 11L;
+	public static final Long AUTHENTICATED_USERS_ID = 12L;
+	public static final Long PUBLIC_ACL_ID = 13L;
 	@Before
 	public void setup() {
-		mockCallback = mock(AsyncCallback.class);
-		mockSynapseJavascriptClient = mock(SynapseJavascriptClient.class);
-		presenter = new UserGroupSuggestionProvider(mockSynapseJavascriptClient);
+		MockitoAnnotations.initMocks(this);
+		when(mockGlobalApplicationState.getPublicPrincipalIds()).thenReturn(mockPublicPrincipalIds);
+		when(mockPublicPrincipalIds.getAnonymousUserPrincipalId()).thenReturn(ANONYMOUS_USER_ID);
+		when(mockPublicPrincipalIds.getAuthenticatedAclPrincipalId()).thenReturn(AUTHENTICATED_USERS_ID);
+		when(mockPublicPrincipalIds.getPublicAclPrincipalId()).thenReturn(PUBLIC_ACL_ID);
+		presenter = new UserGroupSuggestionProvider(mockSynapseJavascriptClient, mockGlobalApplicationState);
 	}
 	
 	@Test
@@ -77,9 +90,25 @@ public class UserGroupSuggestionProviderTest {
 		head1.setFirstName("Test");
 		head1.setLastName("Two");
 		
+		UserGroupHeader head3 = new UserGroupHeader();
+		head3.setOwnerId(ANONYMOUS_USER_ID.toString());
+		head3.setFirstName("Anonymous");
+		
+		UserGroupHeader head4 = new UserGroupHeader();
+		head4.setOwnerId(AUTHENTICATED_USERS_ID.toString());
+		head4.setFirstName("Authenticated Users");
+		
+		UserGroupHeader head5 = new UserGroupHeader();
+		head5.setOwnerId(PUBLIC_ACL_ID.toString());
+		head5.setFirstName("Public");
+		
+		
 		List<UserGroupHeader> children = new ArrayList<UserGroupHeader>();
 		children.add(head1);
 		children.add(head2);
+		children.add(head3);
+		children.add(head4);
+		children.add(head5);
 		
 		testPage.setChildren(children);
 		
