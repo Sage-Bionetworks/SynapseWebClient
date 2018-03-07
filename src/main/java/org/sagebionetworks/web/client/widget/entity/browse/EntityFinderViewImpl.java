6d5884de4cc200fe974b5ad22ff4bd1617fdfdd9
Jay Hodgson <jay.hodgson@sagebase.org>
2018-03-07 10:14:42 -0800

    lazy construct tree and tree item map

--- src/main/java/org/sagebionetworks/web/client/widget/entity/browse/EntityFinderViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/browse/EntityFinderViewImpl.java
@@ -202,6 +202,7 @@ public class EntityFinderViewImpl implements EntityFinderView {
 
 	@Override
 	public void clear() {
+		selectedRef.clear();
 		presenter.clearSelectedEntities();
 		updateSelectedView();
 		selectedRef.clear();
