7d93f908d2b07fa368ec28a27271783b616e0955
Jay Hodgson <jay.hodgson@sagebase.org>
2018-03-30 13:58:46 -0700

    ability to set highlight in button link editor, and added button preview

--- src/main/java/org/sagebionetworks/web/client/widget/entity/editor/ButtonLinkConfigView.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/editor/ButtonLinkConfigView.java
@@ -8,24 +8,9 @@ import org.sagebionetworks.web.shared.WikiPageKey;
 import com.google.gwt.user.client.ui.IsWidget;
 
 public interface ButtonLinkConfigView extends IsWidget, WidgetEditorView {
-
-	/**
-	 * Set the presenter.
-	 * @param presenter
-	 */
-	public void setPresenter(Presenter presenter);
-	public void configure(WikiPageKey wikiKey, Map<String, String> widgetDescriptor);
-	public void setLinkUrl(String url);
-	public String getLinkUrl();
-	
-	public void setName(String name);
-	public String getName();
-	
-	
-	/**
-	 * Presenter interface
-	 */
-	public interface Presenter {
-
-	}
+	void configure(WikiPageKey wikiKey, Map<String, String> widgetDescriptor);
+	String getLinkUrl();
+	String getName();
+	void setIsHighlightButtonStyle(boolean isHighlight);
+	boolean isHighlightButtonStyle();
 }
--- src/main/java/org/sagebionetworks/web/client/widget/entity/editor/ButtonLinkConfigViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/editor/ButtonLinkConfigViewImpl.java
@@ -2,29 +2,56 @@ package org.sagebionetworks.web.client.widget.entity.editor;
 
 import java.util.Map;
 
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.Radio;
 import org.gwtbootstrap3.client.ui.TextBox;
+import org.gwtbootstrap3.client.ui.constants.ButtonType;
 import org.sagebionetworks.web.client.DisplayUtils;
 import org.sagebionetworks.web.client.ValidationUtils;
+import org.sagebionetworks.web.shared.WebConstants;
 import org.sagebionetworks.web.shared.WidgetConstants;
 import org.sagebionetworks.web.shared.WikiPageKey;
 
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
 public class ButtonLinkConfigViewImpl implements ButtonLinkConfigView {
-	private Presenter presenter;
 	public interface ButtonLinkConfigViewImplUiBinder extends UiBinder<Widget, ButtonLinkConfigViewImpl> {}
 	@UiField
 	TextBox urlField;
 	@UiField
 	TextBox nameField;
+	@UiField
+	Radio infoButtonStyle;
+	@UiField
+	Radio defaultButtonStyle;
+	
+	@UiField
+	Button previewButton;
 	Widget widget;
 	
 	@Inject
 	public ButtonLinkConfigViewImpl(ButtonLinkConfigViewImplUiBinder binder) {
 		widget = binder.createAndBindUi(this);
+		
+		ClickHandler updatePreviewClickHandler = event -> {
+			updatePreviewButton();
+		}; 
+		infoButtonStyle.addClickHandler(updatePreviewClickHandler);
+		defaultButtonStyle.addClickHandler(updatePreviewClickHandler);
+		
+		nameField.addKeyUpHandler(event -> {
+			updatePreviewButton();
+		});
+	}
+	
+	private void updatePreviewButton() {
+		ButtonType type = isHighlightButtonStyle() ? ButtonType.INFO : ButtonType.DEFAULT;
+		previewButton.setType(type);
+		previewButton.setText(getName());
 	}
 	
 	@Override
@@ -41,6 +68,13 @@ public class ButtonLinkConfigViewImpl implements ButtonLinkConfigView {
 		String url = descriptor.get(WidgetConstants.LINK_URL_KEY);
 		if (url != null)
 			urlField.setValue(url);
+		boolean isHighlight = false;
+		if (descriptor.containsKey(WebConstants.HIGHLIGHT_KEY)){
+			isHighlight = Boolean.parseBoolean(descriptor.get(WebConstants.HIGHLIGHT_KEY));
+		}
+		setIsHighlightButtonStyle(isHighlight);
+		
+		updatePreviewButton();
 	}
 
 	@Override
@@ -55,11 +89,6 @@ public class ButtonLinkConfigViewImpl implements ButtonLinkConfigView {
 	public Widget asWidget() {
 		return widget;
 	}	
-
-	@Override 
-	public void setPresenter(Presenter presenter) {
-		this.presenter = presenter;
-	}
 		
 	@Override
 	public void showErrorMessage(String message) {
@@ -89,21 +118,18 @@ public class ButtonLinkConfigViewImpl implements ButtonLinkConfigView {
 	}
 	
 	@Override
-	public void setLinkUrl(String url) {
-		urlField.setValue(url);
-	}
-	
-	@Override
 	public String getName() {
 		return nameField.getValue();
 	}
+	
 	@Override
-	public void setName(String name) {
-		nameField.setValue(name);
+	public boolean isHighlightButtonStyle() {
+		return infoButtonStyle.getValue();
 	}
 	
-	/*
-	 * Private Methods
-	 */
-
+	@Override
+	public void setIsHighlightButtonStyle(boolean isHighlight) {
+		infoButtonStyle.setValue(isHighlight, true);
+		defaultButtonStyle.setValue(!isHighlight, true);
+	}
 }
