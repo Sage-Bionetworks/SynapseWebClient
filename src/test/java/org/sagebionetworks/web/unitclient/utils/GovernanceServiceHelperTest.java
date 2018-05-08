3fcbe96416ecada0d9fb254d3c9706b58f8a3d79
Jay Hodgson <jay.hodgson@sagebase.org>
2018-05-08 12:52:38 -0700

    remove more dead code

--- src/test/java/org/sagebionetworks/web/unitclient/utils/GovernanceServiceHelperTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/utils/GovernanceServiceHelperTest.java
@@ -1,14 +1,10 @@
 package org.sagebionetworks.web.unitclient.utils;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,17 +12,13 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.sagebionetworks.repo.model.ACTAccessRequirement;
 import org.sagebionetworks.repo.model.AccessApproval;
-import org.sagebionetworks.repo.model.AccessRequirement;
 import org.sagebionetworks.repo.model.LockAccessRequirement;
 import org.sagebionetworks.repo.model.ManagedACTAccessRequirement;
 import org.sagebionetworks.repo.model.PostMessageContentAccessRequirement;
 import org.sagebionetworks.repo.model.TermsOfUseAccessRequirement;
 import org.sagebionetworks.web.client.SynapseClientAsync;
-import org.sagebionetworks.web.client.utils.APPROVAL_TYPE;
 import org.sagebionetworks.web.client.utils.GovernanceServiceHelper;
-import org.sagebionetworks.web.client.utils.RESTRICTION_LEVEL;
 import org.sagebionetworks.web.test.helper.AsyncMockStubber;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -86,61 +78,6 @@ public class GovernanceServiceHelperTest {
 		
 		AccessApproval capturedWrapper = captor.getValue();
 		assertEquals(accessRequirementId, capturedWrapper.getRequirementId());
-	}
-	
-	@Test
-	public void testSelectAccessRequirement() throws Exception {
-		assertEquals(null, GovernanceServiceHelper.selectAccessRequirement(null, null));
-		Collection<AccessRequirement> allARs = new ArrayList<AccessRequirement>();
-		Collection<AccessRequirement> unmetARs = new ArrayList<AccessRequirement>();
-		assertEquals(null, GovernanceServiceHelper.selectAccessRequirement(allARs, unmetARs));
-		TermsOfUseAccessRequirement tou1 = new TermsOfUseAccessRequirement();
-		allARs.add(tou1);
-		assertTrue(tou1==GovernanceServiceHelper.selectAccessRequirement(allARs, unmetARs));
-		TermsOfUseAccessRequirement tou2 = new TermsOfUseAccessRequirement();
-		allARs.add(tou2);
-		unmetARs.add(tou2);
-		assertTrue(tou2==GovernanceServiceHelper.selectAccessRequirement(allARs, unmetARs));
-	}
-	
-	@Test
-	public void testAccessRequirementApprovalType() throws Exception {
-		assertEquals(APPROVAL_TYPE.NONE, 
-				GovernanceServiceHelper.accessRequirementApprovalType(null));
-		assertEquals(APPROVAL_TYPE.USER_AGREEMENT, 
-				GovernanceServiceHelper.accessRequirementApprovalType(new TermsOfUseAccessRequirement()));
-		assertEquals(APPROVAL_TYPE.ACT_APPROVAL, 
-				GovernanceServiceHelper.accessRequirementApprovalType(new ACTAccessRequirement()));
-		assertEquals(APPROVAL_TYPE.ACT_APPROVAL, 
-				GovernanceServiceHelper.accessRequirementApprovalType(new LockAccessRequirement()));
-		assertEquals(APPROVAL_TYPE.POST_MESSAGE, 
-				GovernanceServiceHelper.accessRequirementApprovalType(new PostMessageContentAccessRequirement()));
-	}
-	
-	@Test 
-	public void testEntityRestrictionLevel() throws Exception {
-		assertEquals(RESTRICTION_LEVEL.OPEN, 
-				GovernanceServiceHelper.entityRestrictionLevel(null));
-		Collection<AccessRequirement> ars = new ArrayList<AccessRequirement>();
-		assertEquals(RESTRICTION_LEVEL.OPEN, 
-				GovernanceServiceHelper.entityRestrictionLevel(ars));
-		ars.add(new TermsOfUseAccessRequirement());
-		assertEquals(RESTRICTION_LEVEL.RESTRICTED, 
-				GovernanceServiceHelper.entityRestrictionLevel(ars));
-		ars.add(new ACTAccessRequirement());
-		assertEquals(RESTRICTION_LEVEL.CONTROLLED, 
-				GovernanceServiceHelper.entityRestrictionLevel(ars));
-		ars.add(new TermsOfUseAccessRequirement());
-		assertEquals(RESTRICTION_LEVEL.CONTROLLED, 
-				GovernanceServiceHelper.entityRestrictionLevel(ars));
-	}
-	
-	@Test 
-	public void testLockEntityRestrictionLevel() throws Exception {
-		Collection<AccessRequirement> ars = new ArrayList<AccessRequirement>();
-		ars.add(new LockAccessRequirement());
-		assertEquals(RESTRICTION_LEVEL.CONTROLLED, 
-				GovernanceServiceHelper.entityRestrictionLevel(ars));
 	}
 	
 	@Test 
