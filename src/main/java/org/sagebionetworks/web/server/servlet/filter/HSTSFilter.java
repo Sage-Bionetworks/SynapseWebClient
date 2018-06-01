3dbf6b6fd39c0374d83fdb31053cfd9acde7be08
Jay Hodgson <jay.hodgson@sagebase.org>
2018-06-01 12:01:07 -0700

    always include the hsts response header

--- src/main/java/org/sagebionetworks/web/server/servlet/filter/HSTSFilter.java
+++ src/main/java/org/sagebionetworks/web/server/servlet/filter/HSTSFilter.java
@@ -1,8 +1,6 @@
 package org.sagebionetworks.web.server.servlet.filter;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.Date;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,7 +8,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -25,15 +22,9 @@ public class HSTSFilter implements Filter {
 	
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain filterChain) throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
-		URL requestURL = new URL(httpRequest.getRequestURL().toString());
-		long now = new Date().getTime();
-		httpResponse.setDateHeader("Date", now);
-		String hostName = requestURL.getHost();
-		if (!(hostName.equals("127.0.0.1") || hostName.toLowerCase().equals("localhost"))) {
-			httpResponse.setHeader("Strict-Transport-Security", "max-age=" + MAX_AGE_SECONDS);
-		}
+		//PLFM-4966: add hsts header to response.  It only has an effect if connection is https, so this will have no impact in the client dev environment.
+		httpResponse.setHeader("Strict-Transport-Security", "max-age=" + MAX_AGE_SECONDS);
 		filterChain.doFilter(request, response);
 	}
 	
