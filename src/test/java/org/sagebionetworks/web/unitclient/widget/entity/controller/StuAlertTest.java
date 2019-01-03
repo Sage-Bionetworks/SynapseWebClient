3dc1ad018323c83bbb5e061a3316874875e2c5ed
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-01-03 10:05:47 -0800

    SWC-4474: remove Request Access functionality from the web

--- src/test/java/org/sagebionetworks/web/unitclient/widget/entity/controller/StuAlertTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/controller/StuAlertTest.java
@@ -1,8 +1,5 @@
 package org.sagebionetworks.web.unitclient.widget.entity.controller;
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -13,21 +10,14 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.sagebionetworks.repo.model.UserProfile;
-import org.sagebionetworks.repo.model.UserSessionData;
 import org.sagebionetworks.web.client.GWTWrapper;
-import org.sagebionetworks.web.client.SynapseClientAsync;
 import org.sagebionetworks.web.client.security.AuthenticationController;
 import org.sagebionetworks.web.client.widget.entity.controller.StuAlert;
 import org.sagebionetworks.web.client.widget.entity.controller.StuAlertView;
 import org.sagebionetworks.web.client.widget.entity.controller.SynapseAlert;
 import org.sagebionetworks.web.shared.exceptions.ForbiddenException;
 import org.sagebionetworks.web.shared.exceptions.NotFoundException;
-import org.sagebionetworks.web.shared.exceptions.ReadOnlyModeException;
-import org.sagebionetworks.web.shared.exceptions.SynapseDownException;
 import org.sagebionetworks.web.shared.exceptions.UnknownErrorException;
-import org.sagebionetworks.web.test.helper.AsyncMockStubber;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class StuAlertTest {
 
@@ -39,8 +29,6 @@ public class StuAlertTest {
 	SynapseAlert mockSynapseAlert;
 	StuAlert widget;
 	@Mock
-	SynapseClientAsync mockSynapseClient;
-	@Mock
 	GWTWrapper mockGWT;
 	@Mock
 	UserProfile mockProfile;
@@ -51,15 +39,12 @@ public class StuAlertTest {
 	@Before
 	public void before(){
 		MockitoAnnotations.initMocks(this);
-		widget = new StuAlert(mockView, mockSynapseClient, mockSynapseAlert, mockGWT, mockAuthenticationController);
+		widget = new StuAlert(mockView, mockSynapseAlert, mockGWT, mockAuthenticationController);
 		when(mockAuthenticationController.getCurrentUserProfile()).thenReturn(mockProfile);
 		when(mockAuthenticationController.getCurrentUserPrincipalId()).thenReturn(USER_ID);
 		when(mockProfile.getOwnerId()).thenReturn(USER_ID);
-		AsyncMockStubber.callSuccessWith(null).when(mockSynapseClient).sendMessageToEntityOwner(anyString(), anyString(), anyString(), anyString(), any(AsyncCallback.class));
-		
 		when(mockGWT.getHostPageBaseURL()).thenReturn(HOST_PAGE_URL);
 		when(mockAuthenticationController.isLoggedIn()).thenReturn(true);
-		verify(mockView).setPresenter(widget);
 	}
 	
 	@Test
@@ -126,28 +111,6 @@ public class StuAlertTest {
 		String entityId = "syn123";
 		widget.show403(entityId);
 		verify(mockView).clearState();
-		verify(mockView).showRequestAccessUI();
 		assertEquals(entityId, widget.getEntityId());
 	}
-	
-	@Test
-	public void testOnRequestAccess() {
-		widget.onRequestAccess();
-		verify(mockView).showRequestAccessButtonLoading();
-		verify(mockSynapseClient).sendMessageToEntityOwner(anyString(), anyString(), stringCaptor.capture(), anyString(), any(AsyncCallback.class));
-		String messageBody = stringCaptor.getValue();
-		assertTrue(messageBody.contains("#!Profile:"+USER_ID));
-		verify(mockView).showInfo(anyString());
-		verify(mockView).hideRequestAccessUI();
-	}
-	@Test
-	public void testOnRequestAccessFailure() {
-		Exception ex = new Exception("ex");
-		AsyncMockStubber.callFailureWith(ex).when(mockSynapseClient).sendMessageToEntityOwner(anyString(), anyString(), anyString(), anyString(), any(AsyncCallback.class));
-		widget.onRequestAccess();
-		verify(mockView).showRequestAccessButtonLoading();
-		verify(mockSynapseClient).sendMessageToEntityOwner(anyString(), anyString(), anyString(), anyString(), any(AsyncCallback.class));
-		verify(mockSynapseAlert).handleException(ex);
-	}
-		
 }
