ce38d9a79187b948090312197abc5dfc7e10efb5
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-02-27 13:27:15 -0800

    clean imports

--- src/main/java/org/sagebionetworks/web/client/presenter/users/PasswordResetPresenter.java
+++ src/main/java/org/sagebionetworks/web/client/presenter/users/PasswordResetPresenter.java
@@ -20,7 +20,6 @@ import org.sagebionetworks.web.client.widget.entity.controller.SynapseAlert;
 import org.sagebionetworks.web.shared.exceptions.NotFoundException;
 
 import com.google.gwt.activity.shared.AbstractActivity;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
