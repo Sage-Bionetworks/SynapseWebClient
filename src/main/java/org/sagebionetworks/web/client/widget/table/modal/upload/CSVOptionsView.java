5fb946ada7543890acf7d073f6a5c589de9ad270
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-05-08 13:04:40 -0700

    SWC-4370

--- src/main/java/org/sagebionetworks/web/client/widget/table/modal/upload/CSVOptionsView.java
+++ src/main/java/org/sagebionetworks/web/client/widget/table/modal/upload/CSVOptionsView.java
@@ -1,7 +1,5 @@
 package org.sagebionetworks.web.client.widget.table.modal.upload;
 
-import org.sagebionetworks.web.client.widget.table.modal.upload.CSVOptionsView.Presenter;
-
 import com.google.gwt.user.client.ui.IsWidget;
 /**
  * A view of the CSV options used for CSV file upload.
@@ -18,6 +16,11 @@ public interface CSVOptionsView extends IsWidget {
 		void onSeparatorChanged();
 
 		/**
+		 * Called when the escape character option changes.
+		 */
+		void onEscapeCharacterChanged();
+
+		/**
 		 * Refresh the preview upon request.
 		 */
 		void onRefreshPreview();
@@ -28,13 +31,25 @@ public interface CSVOptionsView extends IsWidget {
 	 * @param delimiter
 	 */
 	void setSeparator(Delimiter delimiter);
+	
+	/**
+	 * Backslash, or other.
+	 * @param delimiter
+	 */
+	void setEscapeCharacter(EscapeCharacter character);
 
 	/**
 	 * For the case of other, the text of the separator.
 	 * @param separator
 	 */
 	void setOtherSeparatorValue(String separator);
-	
+
+	/**
+	 * For the case of other escape character, the text of the character.
+	 * @param separator
+	 */
+	void setOtherEscapeCharacterValue(String character);
+
 	/**
 	 * Enable/disable the text box for the other value.
 	 * @param enabled
@@ -42,9 +57,21 @@ public interface CSVOptionsView extends IsWidget {
 	void setOtherSeparatorTextEnabled(boolean enabled);
 	
 	/**
+	 * Enable/disable the text box for the other value.
+	 * @param enabled
+	 */
+	void setOtherEscapeCharacterTextEnabled(boolean enabled);
+
+	
+	/**
 	 * Clear the text in the other separtor box.
 	 */
 	void clearOtherSeparatorText();
+
+	/**
+	 * Clear the text in the other separtor box.
+	 */
+	void clearOtherEscapeCharacterText();
 
 	/**
 	 * Comma, tab, or other.
@@ -53,11 +80,23 @@ public interface CSVOptionsView extends IsWidget {
 	Delimiter getSeparator();
 
 	/**
+	 * Backslash, or other.
+	 * @return
+	 */
+	EscapeCharacter getEscapeCharacter();
+
+	/**
 	 * For the case of other, the text of the separator.
 	 * @return
 	 */
 	String getOtherSeparatorValue();
-	
+
+	/**
+	 * For the case of other, the text of the escape character.
+	 * @return
+	 */
+	String getOtherEscapeCharacterValue();
+
 	/**
 	 * Bind the presenter to the view.
 	 * 
--- src/main/java/org/sagebionetworks/web/client/widget/table/modal/upload/CSVOptionsViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/table/modal/upload/CSVOptionsViewImpl.java
@@ -5,9 +5,6 @@ import org.gwtbootstrap3.client.ui.CheckBox;
 import org.gwtbootstrap3.client.ui.Radio;
 import org.gwtbootstrap3.client.ui.TextBox;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -30,6 +27,12 @@ public class CSVOptionsViewImpl implements CSVOptionsView {
 	CheckBox firstLineHeader;
 	@UiField
 	Button refreshButton;
+	@UiField
+	Radio escapeCharacterBackslashRadio;
+	@UiField
+	Radio escapeCharacterOtherRadio;
+	@UiField
+	TextBox escapeCharacterOtherTextBox;
 	
 	Widget widget;
 	
@@ -53,10 +56,24 @@ public class CSVOptionsViewImpl implements CSVOptionsView {
 			otherRadio.setValue(true);
 		}
 	}
+	
+	@Override
+	public void setEscapeCharacter(EscapeCharacter character) {
+		if(EscapeCharacter.BACKSLASH.equals(character)) {
+			escapeCharacterBackslashRadio.setValue(true);
+		} else {
+			escapeCharacterOtherRadio.setValue(true);
+		}
+	}
 
 	@Override
 	public void setOtherSeparatorValue(String separator) {
 		this.otherTextBox.setText(separator);
+	}
+	
+	@Override
+	public void setOtherEscapeCharacterValue(String character) {
+		this.escapeCharacterOtherTextBox.setText(character);
 	}
 
 	@Override
@@ -69,39 +86,42 @@ public class CSVOptionsViewImpl implements CSVOptionsView {
 			return Delimiter.OTHER;
 		}
 	}
+	@Override
+	public EscapeCharacter getEscapeCharacter() {
+		if (escapeCharacterBackslashRadio.getValue()) {
+			return EscapeCharacter.BACKSLASH;
+		} else {
+			return EscapeCharacter.OTHER;
+		}
+	}
 
 	@Override
 	public String getOtherSeparatorValue() {
 		return this.otherTextBox.getValue();
 	}
+	
+	@Override
+	public String getOtherEscapeCharacterValue() {
+		return this.escapeCharacterOtherTextBox.getValue();
+	}
 
 	@Override
 	public void setPresenter(final Presenter presenter) {
-		commaRadio.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-			@Override
-			public void onValueChange(ValueChangeEvent<Boolean> event) {
-				presenter.onSeparatorChanged();
-			}
+		ValueChangeHandler<Boolean> separatorChangedHandler = event -> {
+			presenter.onSeparatorChanged();
+		};
+		commaRadio.addValueChangeHandler(separatorChangedHandler);
+		tabRadio.addValueChangeHandler(separatorChangedHandler);
+		otherRadio.addValueChangeHandler(separatorChangedHandler);
+		refreshButton.addClickHandler(event -> {
+			presenter.onRefreshPreview();
 		});
-		tabRadio.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-			@Override
-			public void onValueChange(ValueChangeEvent<Boolean> event) {
-				presenter.onSeparatorChanged();
-			}
-		});
-		otherRadio.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-			@Override
-			public void onValueChange(ValueChangeEvent<Boolean> event) {
-				presenter.onSeparatorChanged();
-			}
-		});
-		refreshButton.addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				presenter.onRefreshPreview();
-			}
-		});
+		
+		ValueChangeHandler<Boolean> escapeCharacterChangedHandler = event -> {
+			presenter.onEscapeCharacterChanged();
+		};
+		escapeCharacterBackslashRadio.addValueChangeHandler(escapeCharacterChangedHandler);
+		escapeCharacterOtherRadio.addValueChangeHandler(escapeCharacterChangedHandler);
 	}
 
 	@Override
@@ -110,8 +130,16 @@ public class CSVOptionsViewImpl implements CSVOptionsView {
 	}
 
 	@Override
+	public void setOtherEscapeCharacterTextEnabled(boolean enabled) {
+		this.escapeCharacterOtherTextBox.setEnabled(enabled);
+	}
+	@Override
 	public void clearOtherSeparatorText() {
 		this.otherTextBox.clear();
+	}
+	@Override
+	public void clearOtherEscapeCharacterText() {
+		this.escapeCharacterOtherTextBox.clear();
 	}
 
 	@Override
