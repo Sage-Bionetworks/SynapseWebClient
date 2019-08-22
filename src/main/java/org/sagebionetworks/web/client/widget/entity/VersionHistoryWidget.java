261cc5177e81f6058147aa8021dae0314a7a5a81
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-08-22 15:17:48 -0700

    SWC-4939: edit current version info is only shown if the entity is not of type Table

--- src/main/java/org/sagebionetworks/web/client/widget/entity/VersionHistoryWidget.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/VersionHistoryWidget.java
@@ -9,7 +9,7 @@ import org.sagebionetworks.repo.model.Entity;
 import org.sagebionetworks.repo.model.EntityBundle;
 import org.sagebionetworks.repo.model.VersionInfo;
 import org.sagebionetworks.repo.model.VersionableEntity;
-import org.sagebionetworks.repo.model.table.Table;
+import org.sagebionetworks.repo.model.table.TableEntity;
 import org.sagebionetworks.web.client.DisplayConstants;
 import org.sagebionetworks.web.client.GlobalApplicationState;
 import org.sagebionetworks.web.client.SynapseClientAsync;
@@ -161,7 +161,7 @@ public class VersionHistoryWidget implements VersionHistoryWidgetView.Presenter,
 						currentVersion = results.get(0).getVersionNumber();
 						boolean isCurrentVersion = versionNumber == null || currentVersion.equals(versionNumber);
 						view.setEntityBundle(bundle.getEntity(), !isCurrentVersion);
-						view.setEditVersionInfoButtonVisible(isCurrentVersion && canEdit);
+						view.setEditVersionInfoButtonVisible(isCurrentVersion && canEdit && !(bundle.getEntity() instanceof TableEntity) );
 					}
 					if (versionNumber == null && currentOffset == 0 && results.size() > 0) {
 						versionNumber = results.get(0).getVersionNumber();
