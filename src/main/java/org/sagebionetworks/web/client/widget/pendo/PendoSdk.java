22c82693d39d593a3a4fa85d51a086907c6c43fa
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-09-09 12:42:47 -0700

    PLFM-5717: issue reported around initialization of pendo. this reduces the calls to initialize pendo to the minimum (one for anonymous, one for the logged in user)

--- src/main/java/org/sagebionetworks/web/client/widget/pendo/PendoSdk.java
+++ src/main/java/org/sagebionetworks/web/client/widget/pendo/PendoSdk.java
@@ -1,9 +1,7 @@
 package org.sagebionetworks.web.client.widget.pendo;
 
 import org.sagebionetworks.web.client.GWTWrapper;
-import org.sagebionetworks.web.client.utils.Callback;
 
-import com.google.gwt.core.shared.GWT;
 import com.google.inject.Inject;
 
 /**
@@ -13,23 +11,25 @@ import com.google.inject.Inject;
  */
 public class PendoSdk {
 	GWTWrapper gwt;
-	Callback initializeCallback;
 	String userId, email;
+	
 	@Inject
 	public PendoSdk(GWTWrapper gwt) {
 		this.gwt = gwt;
-		this.initializeCallback = () -> {
-			initialize(userId, email);
-		};
 	}
 
 	public void initialize(String userId, String email) {
-		this.userId = userId;
-		this.email = email;
+		if (userId.equals(this.userId) && email.equals(this.email)) {
+			return;
+		}
 		if (_isLoaded()) {
-			_initialize(userId, email);	
+			this.userId = userId;
+			this.email = email;
+			_initialize(userId, email);
 		} else {
-			gwt.scheduleExecution(initializeCallback, 200);
+			gwt.scheduleExecution(() -> {
+				initialize(userId, email);
+			}, 200);
 		}
 	}
 	
@@ -41,31 +41,32 @@ public class PendoSdk {
 			String userId,
 			String synapseEmail) /*-{
 		// Call this whenever information about your visitors becomes available
-	    // Please use Strings, Numbers, or Bools for value types.
-	    try {
-		    	$wnd.pendo.initialize({
-		      visitor: {
-		        id: userId,   // Required if user is logged in
-		        email: synapseEmail// Optional
-		        // role:         // Optional
-		
-		        // You can add any additional visitor level key-values here,
-		        // as long as it's not one of the above reserved names.
-		      },
-		
-		      account: {
-		        // id:           'ACCOUNT-UNIQUE-ID' // Highly recommended
-		        // name:         // Optional
-		        // planLevel:    // Optional
-		        // planPrice:    // Optional
-		        // creationDate: // Optional
-		
-		        // You can add any additional account level key-values here,
-		        // as long as it's not one of the above reserved names.
-		      }
-		    });
-	    } catch(err) {
-	    		console.error(err);
-	    }
+		// Please use Strings, Numbers, or Bools for value types.
+		try {
+			$wnd.pendo.initialize({
+				visitor : {
+					id : userId, // Required if user is logged in
+					email : synapseEmail
+					// Optional
+					// role:         // Optional
+	
+					// You can add any additional visitor level key-values here,
+					// as long as it's not one of the above reserved names.
+				},
+
+				account : {
+					// id:           'ACCOUNT-UNIQUE-ID' // Highly recommended
+					// name:         // Optional
+					// planLevel:    // Optional
+					// planPrice:    // Optional
+					// creationDate: // Optional
+	
+					// You can add any additional account level key-values here,
+					// as long as it's not one of the above reserved names.
+				}
+			});
+		} catch (err) {
+			console.error(err);
+		}
 	}-*/;
 }
