9a229371b7ea1a55d0af86a447059622a9e4e311
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-10-17 16:39:21 -0700

    for now, do not expose the reason details

--- src/main/java/org/sagebionetworks/web/client/widget/QuarantinedEmailModal.java
+++ src/main/java/org/sagebionetworks/web/client/widget/QuarantinedEmailModal.java
@@ -1,13 +1,12 @@
 package org.sagebionetworks.web.client.widget;
 
+import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.Modal;
-import org.gwtbootstrap3.client.ui.html.Div;
 import org.sagebionetworks.web.client.GlobalApplicationState;
 import org.sagebionetworks.web.client.place.Profile;
 import org.sagebionetworks.web.client.place.Synapse.ProfileArea;
 import org.sagebionetworks.web.client.security.AuthenticationController;
-import org.sagebionetworks.web.client.widget.entity.controller.SynapseAlert;
-import org.gwtbootstrap3.client.ui.Button;
+
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.IsWidget;
@@ -19,19 +18,13 @@ public class QuarantinedEmailModal implements IsWidget {
 	public interface Binder extends UiBinder<Widget, QuarantinedEmailModal> {}
 	@UiField
 	Button accountSettingsLink;
-	@UiField
-	Div synAlertContainer;
-	SynapseAlert synAlert;
 	Modal widget;
 
 	@Inject
 	public QuarantinedEmailModal(Binder binder, 
 			AuthenticationController authController,
-			GlobalApplicationState globalAppState,
-			SynapseAlert synAlert){
+			GlobalApplicationState globalAppState){
 		widget = (Modal)binder.createAndBindUi(this);
-		this.synAlert = synAlert;
-		synAlertContainer.add(synAlert);
 		accountSettingsLink.addClickHandler(event -> {
 			globalAppState.getPlaceChanger().goTo(new Profile(authController.getCurrentUserPrincipalId(), ProfileArea.SETTINGS));
 			widget.hide();
@@ -39,7 +32,7 @@ public class QuarantinedEmailModal implements IsWidget {
 	}
 	
 	public void show(String detailedReason) {
-		synAlert.showError(detailedReason);
+		// could show detailed reason in a synAlert if it was informative : synAlert.showError(detailedReason);
 		widget.show();
 	}
 	
