c4562d6823d4dc6bc9f9cffa1023bb610f0a667b
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-11-25 16:50:39 -0800

    remove debugger

--- src/main/java/org/sagebionetworks/web/client/widget/table/v2/results/cell/EntityIdCellRenderer.java
+++ src/main/java/org/sagebionetworks/web/client/widget/table/v2/results/cell/EntityIdCellRenderer.java
@@ -6,7 +6,6 @@ import org.sagebionetworks.web.client.SynapseJSNIUtils;
 import org.sagebionetworks.web.client.widget.asynch.EntityHeaderAsyncHandler;
 import org.sagebionetworks.web.client.widget.asynch.VersionedEntityHeaderAsyncHandler;
 import org.sagebionetworks.web.shared.table.CellAddress;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
@@ -32,7 +31,6 @@ public class EntityIdCellRenderer implements Cell, TakesAddressCell {
 		this.view = view;
 		this.entityHeaderAsyncHandler = entityHeaderAsyncHandler;
 		this.versionedHeaderAsyncHandler = versionedHeaderAsyncHandler;
-		GWT.debugger();
 		this.jsniUtils = jsniUtils;
 	}
 
