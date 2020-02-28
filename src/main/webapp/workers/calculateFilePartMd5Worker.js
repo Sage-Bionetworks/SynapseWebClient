80d2150d332ee873b5472113af889951a6e61ac3
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-02-28 11:09:36 -0800

    SWC-5087: add additional logging (output calculated md5 to console) and use lets instead of vars (in the hope to help gc)

--- src/main/webapp/workers/calculateFilePartMd5Worker.js
+++ src/main/webapp/workers/calculateFilePartMd5Worker.js
@@ -1,27 +1,29 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/spark-md5/3.0.0/spark-md5.min.js');
 
 onmessage = function(e) {
-	var file = e.data.file;
-	var currentChunk = e.data.currentChunk;
-	var chunkSize = e.data.chunkSize;
-	var blobSlice = file.slice || file.mozSlice || file.webkitSlice;
-	spark = new SparkMD5.ArrayBuffer();
-	frOnload = function (e) {
+	let file = e.data.file;
+	let currentChunk = e.data.currentChunk;
+	let chunkSize = e.data.chunkSize;
+	let blobSlice = file.slice || file.mozSlice || file.webkitSlice;
+	let spark = new SparkMD5.ArrayBuffer();
+	let frOnload = function (e) {
 		spark.append(e.target.result); // append array buffer
 		// Call instance method setMD5() on md5Callback with the final md5
-		postMessage(spark.end());
+		let md5 = spark.end();
+		console.log("finished loading file part (to calculate md5): " + md5);
+		postMessage(md5);
 	};
-	frOnerror = function () {
+	let frOnerror = function () {
 		console.warn("unable to calculate file part md5");
 		postMessage(null);
 	};
 	
-	loadPart = function () {
-		var fileReader = new FileReader();
+	let loadPart = function () {
+		let fileReader = new FileReader();
 		fileReader.onload = frOnload;
 		fileReader.onerror = frOnerror;
-		var start = currentChunk * chunkSize,
-		end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+		let start = currentChunk * chunkSize,
+			end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
 	
 		console.log("MD5 file part: loading chunk: start=", start, " end=", end);
 		fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
