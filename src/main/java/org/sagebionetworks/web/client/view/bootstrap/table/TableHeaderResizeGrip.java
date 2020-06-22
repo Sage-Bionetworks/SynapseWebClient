ee71ea10f9c7a4484c26da636d73c7efa63a2cd7
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-06-22 16:09:54 -0700

    functional resize grip

--- src/main/java/org/sagebionetworks/web/client/view/bootstrap/table/TableHeaderResizeGrip.java
+++ src/main/java/org/sagebionetworks/web/client/view/bootstrap/table/TableHeaderResizeGrip.java
@@ -5,26 +5,31 @@ import org.sagebionetworks.web.client.SynapseJSNIUtilsImpl;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
 import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class TableHeaderResizeGrip extends Div {
 	public static Element curCol = null, nxtCol = null;
 	public static int curColWidth = -1, nxtColWidth = -1, pageX = -1;
-	
 	// TODO: add MOUSE_MOVE_HANDLER and MOUSE_UP_HANDLER to Document!
 	public static final MouseMoveHandler MOUSE_MOVE_HANDLER = event -> {
 		if (curCol != null) {
 			int diffX = event.getClientX() - pageX;
-			if (nxtCol != null) {
-				nxtCol.setAttribute("style", "width: " + (nxtColWidth - (diffX))+"px;");
+			if (nxtCol != null && diffX > 0) {
+				updateWidth(nxtCol, nxtColWidth - diffX);
 			}
-			curCol.setAttribute("style", "width: " + (curColWidth - (diffX))+"px;");
-			
-			SynapseJSNIUtilsImpl._consoleLog("MOUSE_MOVE_HANDLER: curCol is not null.");
-			SynapseJSNIUtilsImpl._consoleLog("diffX="+diffX);
+			updateWidth(curCol, curColWidth + diffX);
 		}
 	};
+	
+	public static void updateWidth(Element thElement, int newWidth) {
+		String newWidthPx = newWidth + "px;";
+		thElement.setAttribute("style", "width: " + newWidthPx + "min-width: " + newWidthPx);
+	}
+	
 	public static final MouseUpHandler MOUSE_UP_HANDLER = event -> {
 		curCol = null;
 		nxtCol = null;
@@ -33,26 +38,25 @@ public class TableHeaderResizeGrip extends Div {
 		curColWidth = -1;
 	};
 	
+	static {
+		RootPanel.get().addDomHandler(MOUSE_MOVE_HANDLER, MouseMoveEvent.getType());
+		RootPanel.get().addDomHandler(MOUSE_UP_HANDLER, MouseUpEvent.getType());
+	}
 	
 	public TableHeaderResizeGrip() {
 		makeResizable();
 	}
 
 	public static final MouseDownHandler MOUSE_DOWN_HANDLER = event -> {
-		// TODO: get the parent TH?
-		SynapseJSNIUtilsImpl._consoleLog("MOUSE_DOWN_HANDLER: set curCol nxtCol.");
-
-		curCol = ((Div)event.getSource()).getElement();
+		// get the parent th element
+		curCol = ((Div)event.getSource()).getElement().getParentElement();
+		// and the next th element over
 		nxtCol = curCol.getNextSiblingElement();
 		pageX = event.getClientX();
 		curColWidth = curCol.getOffsetWidth();
 		if (nxtCol != null) {
 			nxtColWidth = nxtCol.getOffsetWidth();
 		}
-		SynapseJSNIUtilsImpl._consoleLog("pageX="+pageX);
-		SynapseJSNIUtilsImpl._consoleLog("curColWidth="+curColWidth);
-		SynapseJSNIUtilsImpl._consoleLog("nxtColWidth="+nxtColWidth);
-
 	};
 	
 	public void makeResizable() {
