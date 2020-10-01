c91e0d484b0b04b104cb376b899dfdb6f80a0ecd
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-10-01 14:41:42 -0700

    fix existing tests

--- src/test/java/org/sagebionetworks/web/unitclient/widget/entity/team/BigTeamBadgeTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/widget/entity/team/BigTeamBadgeTest.java
@@ -1,17 +1,22 @@
 package org.sagebionetworks.web.unitclient.widget.entity.team;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.sagebionetworks.web.client.SynapseJSNIUtils;
 import org.sagebionetworks.web.client.SynapseJavascriptClient;
 import org.sagebionetworks.web.client.security.AuthenticationController;
 import org.sagebionetworks.web.client.widget.entity.renderer.TeamMemberCountWidget;
 import org.sagebionetworks.web.client.widget.team.BigTeamBadge;
 import org.sagebionetworks.web.client.widget.team.BigTeamBadgeView;
+import org.sagebionetworks.web.test.helper.AsyncMockStubber;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BigTeamBadgeTest {
@@ -22,13 +27,17 @@ public class BigTeamBadgeTest {
 	@Mock
 	SynapseJavascriptClient mockJsClient;
 	@Mock
+	SynapseJSNIUtils mockJsniUtils;
+	@Mock
 	TeamMemberCountWidget mockTeamMemberCountWidget;
 
 	BigTeamBadge presenter;
-
+	public static final String TEAM_ICON_URL = "http://team.icon.png";
+	
 	@Before
 	public void setUp() throws Exception {
-		presenter = new BigTeamBadge(mockView, mockJsClient, mockAuthenticationController, mockTeamMemberCountWidget);
+		presenter = new BigTeamBadge(mockView, mockJsClient, mockJsniUtils, mockAuthenticationController, mockTeamMemberCountWidget);
+		AsyncMockStubber.callSuccessWith(TEAM_ICON_URL).when(mockJsClient).getTeamPicturePreviewURL(anyString(), any(AsyncCallback.class));
 	}
 
 	@Test
