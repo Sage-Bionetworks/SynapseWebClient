01bfc68bb6669d101ef2ba0b09ad8466d87480e1
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2020-11-17 12:28:22 -0500

    Add place for personal access token management

---
+++ src/main/java/org/sagebionetworks/web/client/view/PersonalAccessTokensViewImpl.java
@@ -0,0 +1,99 @@
+package org.sagebionetworks.web.client.view;
+
+import org.gwtbootstrap3.client.ui.Anchor;
+import org.gwtbootstrap3.client.ui.html.Div;
+import org.sagebionetworks.web.client.DisplayUtils;
+import org.sagebionetworks.web.client.SynapseJSNIUtils;
+import org.sagebionetworks.web.client.place.Profile;
+import org.sagebionetworks.web.client.place.Synapse;
+import org.sagebionetworks.web.client.presenter.PersonalAccessTokensPresenter;
+import org.sagebionetworks.web.client.security.AuthenticationController;
+import org.sagebionetworks.web.client.widget.header.Header;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+
+public class PersonalAccessTokensViewImpl extends Composite implements PersonalAccessTokensView {
+
+	public interface PersonalAccessTokensViewImplUiBinder extends UiBinder<Widget, PersonalAccessTokensViewImpl> {
+	}
+
+	@UiField
+	Div container = new Div();
+	@UiField
+	Anchor backToSettingsAnchor = new Anchor();
+
+	private PersonalAccessTokensPresenter presenter;
+	private AuthenticationController authController;
+	private Header headerWidget;
+	private SynapseJSNIUtils jsniUtils;
+
+	@Inject
+	public PersonalAccessTokensViewImpl(PersonalAccessTokensViewImplUiBinder uiBinder, AuthenticationController authenticationController, Header headerWidget, SynapseJSNIUtils jsniUtils) {
+		initWidget(uiBinder.createAndBindUi(this));
+
+		this.jsniUtils = jsniUtils;
+		this.authController = authenticationController;
+		this.headerWidget = headerWidget;
+		headerWidget.configure();
+
+		backToSettingsAnchor.addClickHandler(event -> presenter.goTo(new Profile(authenticationController.getCurrentUserPrincipalId(), Synapse.ProfileArea.SETTINGS)));
+	}
+
+
+	@Override
+	public void render() {
+		_showPersonalAccessTokensComponent(container.getElement(), authController.getCurrentUserSessionToken());
+	}
+
+	private static native void _showPersonalAccessTokensComponent(Element el, String sessionToken) /*-{
+		try {
+			var props = {
+			  	title: "Personal Access Tokens",
+				body: "Issue personal access tokens to access your Synapse resources in the command line clients. A personal access token will expire if it is unused for 180 consecutive days. You may create up to 100 personal access tokens.",
+				token: sessionToken,
+			};
+			$wnd.ReactDOM.render($wnd.React.createElement(
+					$wnd.SRC.SynapseComponents.AccessTokenPage, props, null),
+					el);
+		} catch (err) {
+			console.error(err);
+		}
+	}-*/;
+
+
+
+
+	@Override
+	public void setPresenter(PersonalAccessTokensPresenter presenter) {
+		this.presenter = presenter;
+		headerWidget.refresh();
+		render();
+	}
+
+
+	@Override
+	public void showErrorMessage(String message) {
+		DisplayUtils.showErrorMessage(message);
+	}
+
+	@Override
+	public void showLoading() {}
+
+	@Override
+	public void showInfo(String message) {
+		DisplayUtils.showInfo(message);
+	}
+
+
+	@Override
+	public void clear() {
+		jsniUtils.unmountComponentAtNode(container.getElement());
+		render();
+	}
+
+}
---
+++ src/test/java/org/sagebionetworks/web/client/place/PersonalAccessTokenPlaceTest.java
@@ -0,0 +1,24 @@
+package org.sagebionetworks.web.client.place;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonalAccessTokenPlaceTest {
+    PersonalAccessTokenPlace.Tokenizer tokenizer = new PersonalAccessTokenPlace.Tokenizer();
+    String testToken;
+
+    @Before
+    public void setup() {
+        testToken = "0";
+    }
+
+    @Test
+    public void testToToken() {
+        String testToken = this.testToken;
+        PersonalAccessTokenPlace place = tokenizer.getPlace(testToken);
+
+        Assert.assertEquals(this.testToken, place.toToken());
+        Assert.assertEquals(testToken, tokenizer.getToken(place));
+    }
+}
