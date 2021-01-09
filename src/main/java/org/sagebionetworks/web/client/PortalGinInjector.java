a373d40490246b1f9d39ec83ec25a2b923f705ce
Ziming Dong <ziming.dong@sagebase.org>
2021-01-08 17:11:55 -0800

    cleanup again after merge

--- src/main/java/org/sagebionetworks/web/client/PortalGinInjector.java
+++ src/main/java/org/sagebionetworks/web/client/PortalGinInjector.java
@@ -173,7 +173,6 @@ import org.sagebionetworks.web.client.widget.entity.renderer.TeamMemberRowWidget
 import org.sagebionetworks.web.client.widget.entity.renderer.TeamMembersWidget;
 import org.sagebionetworks.web.client.widget.entity.renderer.VideoWidget;
 import org.sagebionetworks.web.client.widget.entity.renderer.WikiFilesPreviewWidget;
-import org.sagebionetworks.web.client.widget.entity.tabs.ChallengeTab;
 import org.sagebionetworks.web.client.widget.entity.tabs.ChallengeTabView;
 import org.sagebionetworks.web.client.widget.entity.tabs.DiscussionTabView;
 import org.sagebionetworks.web.client.widget.entity.tabs.DockerTabView;
