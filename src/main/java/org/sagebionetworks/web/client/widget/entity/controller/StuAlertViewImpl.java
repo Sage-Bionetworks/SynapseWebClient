d439fb1736a0ddb05f2af5eb704eebc9e73d18b0
Jay Hodgson <jay.hodgson@sagebase.org>
2021-03-10 15:30:46 -0800

    change to use a single react component div container

--- src/main/java/org/sagebionetworks/web/client/widget/entity/controller/StuAlertViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/controller/StuAlertViewImpl.java
@@ -9,7 +9,6 @@ import org.sagebionetworks.web.client.widget.ReactComponentDiv;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
@@ -21,14 +20,12 @@ public class StuAlertViewImpl implements StuAlertView {
 	Widget widget;
 
 	@UiField
-	ReactComponentDiv httpCode403;
-	@UiField
-	ReactComponentDiv httpCode404;
+	ReactComponentDiv errorPageContainer;
 	@UiField
 	Div synAlertContainer;
 	Widget synAlertWidget;
 	Div container = new Div();
-
+	boolean is404, is403;
 	@Inject
 	public StuAlertViewImpl() {}
 
@@ -46,8 +43,9 @@ public class StuAlertViewImpl implements StuAlertView {
 	public void clearState() {
 		container.setVisible(false);
 		if (widget != null) {
-			httpCode403.setVisible(false);
-			httpCode404.setVisible(false);
+			is404 = false;
+			is403 = false;
+			errorPageContainer.setVisible(false);
 		}
 	}
 
@@ -57,23 +55,38 @@ public class StuAlertViewImpl implements StuAlertView {
 			widget = b.createAndBindUi(this);
 			synAlertContainer.add(synAlertWidget);
 			container.add(widget);
+			widget.addAttachHandler(event -> {
+				if (event.isAttached()) {
+					updateErrorPage();
+				}
+			});
 		}
 	}
 
+	private void updateErrorPage() {
+		if (is404) {
+			DownViewImpl._createSRCErrorPage(errorPageContainer.getElement(), ErrorPageType.unavailable.name(), "Sorry, this page isn’t available.", "The link you followed may be broken, or the page may have been removed.");
+			errorPageContainer.setVisible(true);
+		}
+		if (is403) {
+			DownViewImpl._createSRCErrorPage(errorPageContainer.getElement(), ErrorPageType.noAccess.name(), "Sorry, no access to this page.", "You are not authorized to access the page requested.");
+			errorPageContainer.setVisible(true);
+		}
+	}
 	@Override
 	public void show403() {
+		is403 = true;
 		lazyConstruct();
 		container.setVisible(true);
-		httpCode403.setVisible(true);
- 		DownViewImpl._createSRCErrorPage(httpCode403.getElement(), ErrorPageType.noAccess.name(), "Sorry, no access to this page.", "You are not authorized to access the page requested.");
+		updateErrorPage();
 	}
 
 	@Override
 	public void show404() {
+		is404 = true;
 		lazyConstruct();
 		container.setVisible(true);
-		httpCode404.setVisible(true);
-		DownViewImpl._createSRCErrorPage(httpCode404.getElement(), ErrorPageType.unavailable.name(), "Sorry, this page isn’t available.", "The link you followed may be broken, or the page may have been removed.");
+		updateErrorPage();
 	}
 
 	@Override
