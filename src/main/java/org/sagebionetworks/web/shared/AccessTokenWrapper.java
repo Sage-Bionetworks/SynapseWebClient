44175b05302b85166ea84c038525cbb4129f20f9
Jay Hodgson <jay.hodgson@sagebase.org>
2021-05-14 10:00:27 -0700

    maintain same structure in our AccessTokenWrapper as the old Session backend object to make our migration life easier

--- src/main/java/org/sagebionetworks/web/shared/AccessTokenWrapper.java
+++ src/main/java/org/sagebionetworks/web/shared/AccessTokenWrapper.java
@@ -1,4 +1,5 @@
 package org.sagebionetworks.web.shared;
+
 import org.sagebionetworks.schema.ObjectSchema;
 import org.sagebionetworks.schema.adapter.JSONEntity;
 import org.sagebionetworks.schema.adapter.JSONObjectAdapter;
@@ -12,102 +13,97 @@ import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
  */
 public class AccessTokenWrapper implements JSONEntity {
 
-    private final static String _KEY_TOKEN = "token";
-    private String token;
-    
-    public AccessTokenWrapper() {
-    }
+	// maintaining the "sessionToken" key to ease migration pain (production portals do not all need to update in order to use the init session servlet)
+	private final static String _KEY_TOKEN = "sessionToken";
+	private String token;
 
-    public AccessTokenWrapper(JSONObjectAdapter adapter)
-        throws JSONObjectAdapterException
-    {
-        super();
-        if (adapter == null) {
-            throw new IllegalArgumentException(ObjectSchema.OBJECT_ADAPTER_CANNOT_BE_NULL);
-        }
-        initializeFromJSONObject(adapter);
-    }
+	public AccessTokenWrapper() {
+	}
 
-    public String getToken() {
-        return token;
-    }
+	public AccessTokenWrapper(JSONObjectAdapter adapter) throws JSONObjectAdapterException {
+		super();
+		if (adapter == null) {
+			throw new IllegalArgumentException(ObjectSchema.OBJECT_ADAPTER_CANNOT_BE_NULL);
+		}
+		initializeFromJSONObject(adapter);
+	}
 
-    public AccessTokenWrapper setToken(String token) {
-        this.token = token;
-        return this;
-    }
+	public String getToken() {
+		return token;
+	}
 
-    @Override
-    public JSONObjectAdapter initializeFromJSONObject(JSONObjectAdapter adapter)
-        throws JSONObjectAdapterException
-    {
-        if (adapter == null) {
-            throw new IllegalArgumentException(ObjectSchema.OBJECT_ADAPTER_CANNOT_BE_NULL);
-        }
-        if (!adapter.isNull(_KEY_TOKEN)) {
-            token = adapter.getString(_KEY_TOKEN);
-        } else {
-            token = null;
-        }
-        return adapter;
-    }
+	public AccessTokenWrapper setToken(String token) {
+		this.token = token;
+		return this;
+	}
 
-    @Override
-    public JSONObjectAdapter writeToJSONObject(JSONObjectAdapter adapter)
-        throws JSONObjectAdapterException
-    {
-        if (adapter == null) {
-            throw new IllegalArgumentException(ObjectSchema.OBJECT_ADAPTER_CANNOT_BE_NULL);
-        }
-        if (token!= null) {
-            adapter.put(_KEY_TOKEN, token);
-        }
-        return adapter;
-    }
+	@Override
+	public JSONObjectAdapter initializeFromJSONObject(JSONObjectAdapter adapter) throws JSONObjectAdapterException {
+		if (adapter == null) {
+			throw new IllegalArgumentException(ObjectSchema.OBJECT_ADAPTER_CANNOT_BE_NULL);
+		}
+		if (!adapter.isNull(_KEY_TOKEN)) {
+			token = adapter.getString(_KEY_TOKEN);
+		} else {
+			token = null;
+		}
+		return adapter;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = ((prime*result)+((token == null)? 0 :token.hashCode()));
-        return result;
-    }
+	@Override
+	public JSONObjectAdapter writeToJSONObject(JSONObjectAdapter adapter) throws JSONObjectAdapterException {
+		if (adapter == null) {
+			throw new IllegalArgumentException(ObjectSchema.OBJECT_ADAPTER_CANNOT_BE_NULL);
+		}
+		if (token != null) {
+			adapter.put(_KEY_TOKEN, token);
+		}
+		return adapter;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (this.getClass()!= obj.getClass()) {
-            return false;
-        }
-        AccessTokenWrapper other = ((AccessTokenWrapper) obj);
-        if (token == null) {
-            if (other.token!= null) {
-                return false;
-            }
-        } else {
-            if (!token.equals(other.token)) {
-                return false;
-            }
-        }
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = ((prime * result) + ((token == null) ? 0 : token.hashCode()));
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder result;
-        result = new StringBuilder();
-        result.append("");
-        result.append("org.sagebionetworks.web.shared.AccessTokenWrapper");
-        result.append(" [");
-        result.append("sessionToken=");
-        result.append(token);
-        result.append(" ");
-        result.append("]");
-        return result.toString();
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		AccessTokenWrapper other = ((AccessTokenWrapper) obj);
+		if (token == null) {
+			if (other.token != null) {
+				return false;
+			}
+		} else {
+			if (!token.equals(other.token)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder result;
+		result = new StringBuilder();
+		result.append("");
+		result.append("org.sagebionetworks.web.shared.AccessTokenWrapper");
+		result.append(" [");
+		result.append("sessionToken=");
+		result.append(token);
+		result.append(" ");
+		result.append("]");
+		return result.toString();
+	}
 }
