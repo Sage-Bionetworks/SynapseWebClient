8c3a28e4ee0dde12e25248823d2cd1708c8d447e
Jay Hodgson <jay.hodgson@sagebase.org>
2022-03-01 11:16:07 -0800

    more command cleanup if Materialized View

--- src/main/java/org/sagebionetworks/web/client/widget/entity/controller/EntityActionControllerImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/controller/EntityActionControllerImpl.java
@@ -770,14 +770,15 @@ public class EntityActionControllerImpl implements EntityActionController, Actio
 	private void configureTableCommands() {
 		if (entityBundle.getEntity() instanceof Table) {
 			boolean isDataset = entityBundle.getEntity() instanceof Dataset;
+			boolean isMaterializedView = entityBundle.getEntity() instanceof MaterializedView;
 			// For UX reasons, datasets are not editable, even if the user has permissions (SWC-5870, SWC-5903)
-			boolean canEditResults = entityBundle.getPermissions().getCanCertifiedUserEdit() && !isDataset;
+			boolean canEditResults = entityBundle.getPermissions().getCanCertifiedUserEdit() && !isDataset && !isMaterializedView;
 			actionMenu.setActionVisible(Action.UPLOAD_TABLE_DATA, canEditResults);
 			actionMenu.setActionText(Action.UPLOAD_TABLE_DATA, "Upload Data to " + entityTypeDisplay);
 			actionMenu.setActionVisible(Action.EDIT_TABLE_DATA, canEditResults);
 			actionMenu.setActionVisible(Action.DOWNLOAD_TABLE_QUERY_RESULTS, true);
 			actionMenu.setActionVisible(Action.SHOW_TABLE_SCHEMA, true);
-			actionMenu.setActionVisible(Action.SHOW_VIEW_SCOPE, !(entityBundle.getEntity() instanceof TableEntity) && !isDataset);
+			actionMenu.setActionVisible(Action.SHOW_VIEW_SCOPE, !(entityBundle.getEntity() instanceof TableEntity) && !isDataset && !isMaterializedView);
 			actionMenu.setActionVisible(Action.EDIT_DATASET_ITEMS, isDataset && isCurrentVersion);
 		} else {
 			actionMenu.setActionVisible(Action.UPLOAD_TABLE_DATA, false);
