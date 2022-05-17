1060696118e84997447b403327fcd57be27277b1
Jay Hodgson <jay.hodgson@sagebase.org>
2022-05-17 14:10:50 -0700

    fix build, use Jsoup basic safelist (which includes br)

--- src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java
+++ src/main/java/org/sagebionetworks/web/server/servlet/SynapseClientImpl.java
@@ -20,11 +20,12 @@ import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.entity.ContentType;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document.OutputSettings;
+import org.jsoup.safety.Safelist;
 import org.jsoup.safety.Whitelist;
 import org.sagebionetworks.client.exceptions.SynapseException;
 import org.sagebionetworks.repo.model.ACCESS_TYPE;
@@ -114,8 +115,10 @@ import org.sagebionetworks.web.shared.exceptions.NotFoundException;
 import org.sagebionetworks.web.shared.exceptions.RestServiceException;
 import org.sagebionetworks.web.shared.exceptions.UnauthorizedException;
 import org.sagebionetworks.web.shared.exceptions.UnknownErrorException;
+
 import com.google.gwt.thirdparty.guava.common.base.Supplier;
 import com.google.gwt.thirdparty.guava.common.base.Suppliers;
+
 import au.com.bytecode.opencsv.CSVReader;
 
 public class SynapseClientImpl extends SynapseClientBase implements SynapseClient, TokenProvider {
@@ -1240,7 +1243,7 @@ public class SynapseClientImpl extends SynapseClientBase implements SynapseClien
 			message.setRecipients(recipients);
 			message.setSubject(subject);
 			message.setNotificationUnsubscribeEndpoint(getSignedTokenEndpoint(hostPageBaseURL));
-			String cleanedMessageBody = Jsoup.clean(messageBody, "", Whitelist.simpleText().addTags("br"), new OutputSettings().prettyPrint(false));
+			String cleanedMessageBody = Jsoup.clean(messageBody, Safelist.basic());
 			MessageToUser sentMessage = synapseClient.sendStringMessage(message, cleanedMessageBody);
 			JSONObjectAdapter sentMessageJson = sentMessage.writeToJSONObject(adapterFactory.createNew());
 			return sentMessageJson.toJSONString();
