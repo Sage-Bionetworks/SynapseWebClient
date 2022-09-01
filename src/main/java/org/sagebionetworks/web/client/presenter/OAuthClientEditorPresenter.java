e239ed11f291ecf1fb717c51e30b80b83a8f0b1a
Yongjoo Yoo <yjoo.eyoo@gmail.com>
2022-09-01 09:57:37 -0700

    SWC-6264

--- src/main/java/org/sagebionetworks/web/client/presenter/OAuthClientEditorPresenter.java
+++ src/main/java/org/sagebionetworks/web/client/presenter/OAuthClientEditorPresenter.java
@@ -1,6 +1,6 @@
 package org.sagebionetworks.web.client.presenter;
 
-import org.sagebionetworks.web.client.GlobalApplicationState;
+import org.sagebionetworks.web.client.context.SynapseContextPropsProvider;
 import org.sagebionetworks.web.client.place.OAuthClientEditorPlace;
 import org.sagebionetworks.web.client.view.OAuthClientEditorView;
 
@@ -13,21 +13,23 @@ public class OAuthClientEditorPresenter extends AbstractActivity implements Pres
 
     private OAuthClientEditorView view;
     private OAuthClientEditorPlace place;
+    private SynapseContextPropsProvider propsProvider;
 
     @Inject
-    public OAuthClientEditorPresenter(OAuthClientEditorView view, GlobalApplicationState globalApplicationState) {
+    public OAuthClientEditorPresenter(OAuthClientEditorView view, SynapseContextPropsProvider propsProvider) {
         this.view = view;
+        this.propsProvider = propsProvider;
     }
 
     @Override
     public void start(AcceptsOneWidget panel, EventBus eventBus) {
         panel.setWidget(view);
+        view.createReactComponentWidget(propsProvider);
     }
 
     @Override
     public void setPlace(OAuthClientEditorPlace place){
         this.place = place;
-        this.view.setPresenter(this);
     }
 
     public OAuthClientEditorPlace getPlace() {return place;}
--- src/main/java/org/sagebionetworks/web/client/view/OAuthClientEditorView.java
+++ src/main/java/org/sagebionetworks/web/client/view/OAuthClientEditorView.java
@@ -1,12 +1,9 @@
 package org.sagebionetworks.web.client.view;
 
-import org.sagebionetworks.web.client.presenter.OAuthClientEditorPresenter;
+import org.sagebionetworks.web.client.context.SynapseContextPropsProvider;
 
 import com.google.gwt.user.client.ui.IsWidget;
 
 public interface OAuthClientEditorView extends IsWidget {
-
-    public void setPresenter(OAuthClientEditorPresenter presenter);
-    public void render();
-
+    void createReactComponentWidget(SynapseContextPropsProvider propsProvider);
 }
