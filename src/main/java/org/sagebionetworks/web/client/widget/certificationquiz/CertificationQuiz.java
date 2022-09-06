00fa40609f4c35bba99e3086d770377c8c164b70
Yongjoo Yoo <yjoo.eyoo@gmail.com>
2022-09-06 08:16:57 -0700

    SWC-6272

--- src/main/java/org/sagebionetworks/web/client/widget/certificationquiz/CertificationQuiz.java
+++ src/main/java/org/sagebionetworks/web/client/widget/certificationquiz/CertificationQuiz.java
@@ -2,19 +2,17 @@ package org.sagebionetworks.web.client.widget.certificationquiz;
 
 import org.sagebionetworks.web.client.context.SynapseContextPropsProvider;
 import org.sagebionetworks.web.client.jsinterop.React;
-import org.sagebionetworks.web.client.jsinterop.ReactDOM;
 import org.sagebionetworks.web.client.jsinterop.SRC;
 import org.sagebionetworks.web.client.widget.ReactComponentDiv;
 
 public class CertificationQuiz extends ReactComponentDiv {
     public CertificationQuiz(SynapseContextPropsProvider contextPropsProvider){
-        ReactDOM.render(
+        this.render(
                 React.createElementWithSynapseContext(
                         SRC.SynapseComponents.CertificationQuiz,
                         null,
                         contextPropsProvider.getJsInteropContextProps()
-                ),
-                getElement()
+                )
         );
     }
 }
