c6e8caf441285bb1211a1ecaf6370a0819c2002d
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-11-07 07:49:38 -0500

    SWC-6180
    Account for breaking change in Sage-Bionetworks/Synapse-React-Client#1880

--- src/main/java/org/sagebionetworks/web/client/jsinterop/IconSvgProps.java
+++ src/main/java/org/sagebionetworks/web/client/jsinterop/IconSvgProps.java
@@ -1,18 +1,22 @@
 package org.sagebionetworks.web.client.jsinterop;
 
+import jsinterop.annotations.JsNullable;
 import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
 public class IconSvgProps extends ReactComponentProps {
-	IconSvgOptions options;
-	
+	String icon;
+
+	@JsNullable
+	String label;
+
 	@JsOverlay
-	public static IconSvgProps create(String icon, String color, String size, String padding, String label) {
+	public static IconSvgProps create(String icon, String label) {
 		IconSvgProps props = new IconSvgProps();
-		IconSvgOptions options = IconSvgOptions.create(icon, color, size, padding, label);
-		props.options = options;
+		props.icon = icon;
+		props.label = label;
 		return props;
 	}
 }
--- src/main/java/org/sagebionetworks/web/client/widget/IconSvg.java
+++ src/main/java/org/sagebionetworks/web/client/widget/IconSvg.java
@@ -8,25 +8,20 @@ import org.sagebionetworks.web.client.jsinterop.SRC;
 public class IconSvg extends ReactComponentSpan {
 
 	private String icon;
-	private String color;
-	private String size;
-	private String padding;
+
 	private String label;
 
 	public IconSvg() {
 	}
 	
-	public void configure(String icon, String color, String size, String padding, String label) {
+	public void configure(String icon, String label) {
 		this.icon = icon;
-		this.color = color;
-		this.size = size;
-		this.padding = padding;
 		this.label = label;
 		renderComponent();
 	}
 
 	private void renderComponent() {
-		IconSvgProps props = IconSvgProps.create(icon, color, size, padding, label);
+		IconSvgProps props = IconSvgProps.create(icon, label);
 		ReactNode component = React.createElement(SRC.SynapseComponents.IconSvg, props);
 		this.render(component);
 	}
