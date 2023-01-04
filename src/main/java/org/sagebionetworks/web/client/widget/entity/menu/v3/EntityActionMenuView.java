cc92151c77f8b8e4b05fb8b2e424a1c9b3efc433
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-01-04 15:33:09 -0500

    Update src/main/java/org/sagebionetworks/web/client/widget/entity/menu/v3/EntityActionMenuView.java

--- src/main/java/org/sagebionetworks/web/client/widget/entity/menu/v3/EntityActionMenuView.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/menu/v3/EntityActionMenuView.java
@@ -1,7 +1,6 @@
 package org.sagebionetworks.web.client.widget.entity.menu.v3;
 
 import com.google.gwt.user.client.ui.IsWidget;
-import org.sagebionetworks.web.client.jsinterop.entity.actionmenu.EntityActionMenuPropsJsInterop;
 
 public interface EntityActionMenuView extends IsWidget {
   void configure(EntityActionMenuProps props);
