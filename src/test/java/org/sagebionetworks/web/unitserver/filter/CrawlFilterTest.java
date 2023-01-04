b5e01db8930e82ee8a7b422bedd2fec20b4e4618
Jay Hodgson <jay.hodgson@sagebase.org>
2023-01-04 10:48:31 -0800

    with import

--- src/test/java/org/sagebionetworks/web/unitserver/filter/CrawlFilterTest.java
+++ src/test/java/org/sagebionetworks/web/unitserver/filter/CrawlFilterTest.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.web.unitserver.filter;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
