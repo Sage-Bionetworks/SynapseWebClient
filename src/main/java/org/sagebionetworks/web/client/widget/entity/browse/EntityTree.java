fd6ea18c40650f513d33ba60b0d8a14acf974469
Jay Hodgson <jay.hodgson@sagebase.org>
2023-06-08 10:56:56 -0700

    PORTALS-2688: quick accessibility fix for the Files tab after Lighthouse test

--- src/main/java/org/sagebionetworks/web/client/widget/entity/browse/EntityTree.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/browse/EntityTree.java
@@ -1,11 +1,17 @@
 package org.sagebionetworks.web.client.widget.entity.browse;
 
+import com.google.gwt.aria.client.Roles;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.Tree;
 
 public class EntityTree extends Tree {
 
   public EntityTree(Resources resources) {
     super(resources);
+    Roles.getTreeRole().set(getElement());
+    Element treeElement = getElement();
+    Element treeContentElem = treeElement.getFirstChildElement();
+    treeContentElem.removeAttribute("role");
   }
 
   @Override
