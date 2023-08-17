b28a204fc0ec1b5e973bc6222168e90ad893bb04
Hallie Swan <26949006+hallieswan@users.noreply.github.com>
2023-08-17 16:14:41 -0700

    SWC-6514: clean up test users in afterAll on failure, so setup can be retried

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -6,7 +6,7 @@
 // - in CI: set in repo GitHub secrets, read via env in workflow
 // - locally: set in .env file, read by dotenv
 
-import { expect, test as setup } from '@playwright/test'
+import { Page, expect, test as setup } from '@playwright/test'
 import { v4 as uuidv4 } from 'uuid'
 import { ADMIN_STORAGE_STATE, USER_STORAGE_STATE } from '../playwright.config'
 import { getEndpoint } from './helpers/http'
@@ -14,75 +14,99 @@ import { setLocalStorage } from './helpers/localStorage'
 import {
   USER_NAME_LOCALSTORAGE_KEY,
   createTestUser,
+  deleteTestUser,
   getAccessTokenFromCookie,
   getAdminUserCredentials,
   loginTestUser,
 } from './helpers/testUser'
 import { TestUser } from './helpers/types'
 
-setup('authenticate users', async ({ browser }) => {
-  setup.slow()
+let testUserId: string | undefined
+let adminAccessToken: string | undefined
+let adminPage: Page
+let userPage: Page
 
-  const { adminAccessToken, adminPage } = await setup.step(
-    'authenticate admin',
-    async () => {
+setup.describe('Setup', () => {
+  setup.describe.configure({ retries: 2 })
+
+  setup.beforeAll(async ({ browser }) => {
+    adminPage = await browser.newPage()
+    userPage = await browser.newPage()
+  })
+
+  setup('authenticate users', async () => {
+    setup.slow()
+
+    await setup.step('authenticate admin', async () => {
       const { adminUserName, adminUserPassword } = getAdminUserCredentials()
 
-      const adminPage = await browser.newPage()
       await loginTestUser(adminPage, adminUserName!, adminUserPassword!)
       await adminPage.context().storageState({ path: ADMIN_STORAGE_STATE })
 
-      const adminAccessToken = await getAccessTokenFromCookie(adminPage)
+      adminAccessToken = await getAccessTokenFromCookie(adminPage)
+    })
 
-      return { adminAccessToken, adminPage }
-    },
-  )
+    const { testUserName, testUserPassword } = await setup.step(
+      'create test user',
+      async () => {
+        const testUserName = 'swc-e2e-' + uuidv4()
+        const testUserPassword = 'password-' + uuidv4()
+        /* 
+        const testUserName = 'swc-e2e-test'
+        const testUserPassword = process.env.USER_PASSWORD!
+        expect(testUserPassword).not.toBeUndefined() 
+        */
 
-  const { userPage, testUserName, testUserPassword } = await setup.step(
-    'create test user',
-    async () => {
-      const userPage = await browser.newPage()
+        const testUserEmail = `${testUserName}@test.com`
+        const testUserIsValidated = false
 
-      const testUserName = 'swc-e2e-' + uuidv4()
-      const testUserPassword = 'password-' + uuidv4()
-      /* 
-      const testUserName = 'swc-e2e-test'
-      const testUserPassword = process.env.USER_PASSWORD!
-      expect(testUserPassword).not.toBeUndefined()
-      */
+        const testUser: TestUser = {
+          username: testUserName,
+          email: testUserEmail,
+          password: testUserPassword,
+          tou: true,
+          validatedUser: testUserIsValidated,
+        }
 
-      const testUserEmail = `${testUserName}@test.com`
-      const testUserIsValidated = false
+        testUserId = await createTestUser(
+          getEndpoint(),
+          testUser,
+          adminAccessToken,
+        )
+        expect(testUserId).not.toBeUndefined()
 
-      const testUser: TestUser = {
-        username: testUserName,
-        email: testUserEmail,
-        password: testUserPassword,
-        tou: true,
-        validatedUser: testUserIsValidated,
-      }
+        await setLocalStorage(
+          userPage,
+          USER_NAME_LOCALSTORAGE_KEY,
+          testUserName,
+        )
+        await userPage.context().storageState({ path: USER_STORAGE_STATE })
 
-      const testUserId = await createTestUser(
-        getEndpoint(),
-        testUser,
-        adminAccessToken,
-      )
-      expect(testUserId).not.toBeUndefined()
+        return { testUserName, testUserPassword }
+      },
+    )
 
-      await setLocalStorage(userPage, USER_NAME_LOCALSTORAGE_KEY, testUserName)
+    await setup.step('authenticate test user', async () => {
+      await loginTestUser(userPage, testUserName, testUserPassword)
       await userPage.context().storageState({ path: USER_STORAGE_STATE })
-
-      return { userPage, testUserName, testUserPassword }
-    },
-  )
-
-  await setup.step('authenticate test user', async () => {
-    await loginTestUser(userPage, testUserName, testUserPassword)
-    await userPage.context().storageState({ path: USER_STORAGE_STATE })
+    })
   })
 
-  await setup.step('close pages', async () => {
+  setup.afterAll(async ({}, { expectedStatus, status }) => {
+    // clean up pages
     await adminPage.close()
     await userPage.close()
+
+    // delete test user if setup wasn't successful:
+    // ...cleanup isn't run until after last retry
+    // ...so we delete here, so we can retry setup without leaving behind test users
+    if (status !== expectedStatus && testUserId !== undefined) {
+      const result = await deleteTestUser(
+        getEndpoint(),
+        testUserId!,
+        adminAccessToken,
+      )
+      expect(result).toEqual(testUserId)
+    }
   })
 })
