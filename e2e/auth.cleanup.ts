ef6c4b9025e923ad7a9b79088c2c4496a196a666
Hallie Swan <26949006+hallieswan@users.noreply.github.com>
2023-08-22 17:19:55 -0700

    SWC-6514: close pages in afterAll to ensure that pages are cleaned up on retry

--- e2e/auth.cleanup.ts
+++ e2e/auth.cleanup.ts
@@ -1,4 +1,4 @@
-import { test as cleanup, expect } from '@playwright/test'
+import { Page, test as cleanup, expect } from '@playwright/test'
 import { ADMIN_STORAGE_STATE, USER_STORAGE_STATE } from '../playwright.config'
 import { getEndpoint } from './helpers/http'
 import {
@@ -9,49 +9,49 @@ import {
   logoutTestUser,
 } from './helpers/testUser'
 
-cleanup('cleanup', async ({ browser }) => {
-  const { adminPage, userPage } = await cleanup.step(
-    'create pages',
-    async () => {
-      const adminPage = await browser.newPage({
-        storageState: ADMIN_STORAGE_STATE,
-      })
-      const userPage = await browser.newPage({
-        storageState: USER_STORAGE_STATE,
-      })
+let adminPage: Page
+let userPage: Page
 
-      return { adminPage, userPage }
-    },
-  )
-
-  const { adminAccessToken, testUserId } = await cleanup.step(
-    'get credentials',
-    async () => {
-      const adminAccessToken = await getAccessTokenFromCookie(adminPage)
-      const testUserId = await getUserIdFromLocalStorage(userPage)
-
-      return { adminAccessToken, testUserId }
-    },
-  )
-
-  await cleanup.step('logout users', async () => {
-    await goToDashboard(userPage)
-    await logoutTestUser(userPage)
-
-    await goToDashboard(adminPage)
-    await logoutTestUser(adminPage)
+cleanup.describe('Clean up', () => {
+  cleanup.beforeAll(async ({ browser }) => {
+    adminPage = await browser.newPage({
+      storageState: ADMIN_STORAGE_STATE,
+    })
+    userPage = await browser.newPage({
+      storageState: USER_STORAGE_STATE,
+    })
   })
 
-  await cleanup.step('delete test user', async () => {
-    const result = await deleteTestUser(
-      getEndpoint(),
-      testUserId!,
-      adminAccessToken,
+  cleanup('clean up users', async () => {
+    const { adminAccessToken, testUserId } = await cleanup.step(
+      'get credentials',
+      async () => {
+        const adminAccessToken = await getAccessTokenFromCookie(adminPage)
+        const testUserId = await getUserIdFromLocalStorage(userPage)
+
+        return { adminAccessToken, testUserId }
+      },
     )
-    expect(result).toEqual(testUserId)
+
+    await cleanup.step('logout users', async () => {
+      await goToDashboard(userPage)
+      await logoutTestUser(userPage)
+
+      await goToDashboard(adminPage)
+      await logoutTestUser(adminPage)
+    })
+
+    await cleanup.step('delete test user', async () => {
+      const result = await deleteTestUser(
+        getEndpoint(),
+        testUserId!,
+        adminAccessToken,
+      )
+      expect(result).toEqual(testUserId)
+    })
   })
 
-  await cleanup.step('close pages', async () => {
+  cleanup.afterAll(async () => {
     await adminPage.close()
     await userPage.close()
   })
