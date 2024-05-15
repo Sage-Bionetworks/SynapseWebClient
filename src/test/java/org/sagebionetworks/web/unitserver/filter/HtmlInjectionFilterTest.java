f43a36bafae1232ddca2261b332bcc3ae16afdbd
Jay Hodgson <jay.hodgson@sagebase.org>
2024-05-15 13:58:24 -0700

    2 trivial but working tests

--- src/test/java/org/sagebionetworks/web/unitserver/filter/HtmlInjectionFilterTest.java
+++ src/test/java/org/sagebionetworks/web/unitserver/filter/HtmlInjectionFilterTest.java
@@ -1,6 +1,5 @@
 package org.sagebionetworks.web.unitserver.filter;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
@@ -20,9 +19,6 @@ import freemarker.template.Template;
 import freemarker.template.TemplateNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -34,16 +30,16 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.sagebionetworks.client.SynapseClient;
+import org.sagebionetworks.client.exceptions.SynapseException;
 import org.sagebionetworks.repo.model.Entity;
 import org.sagebionetworks.repo.model.EntityChildrenRequest;
 import org.sagebionetworks.repo.model.EntityChildrenResponse;
-import org.sagebionetworks.repo.model.EntityType;
 import org.sagebionetworks.repo.model.annotation.v2.Annotations;
-import org.sagebionetworks.repo.model.annotation.v2.AnnotationsValue;
 import org.sagebionetworks.repo.model.entitybundle.v2.EntityBundle;
 import org.sagebionetworks.repo.model.entitybundle.v2.EntityBundleRequest;
 import org.sagebionetworks.web.server.servlet.DiscussionForumClientImpl;
-import org.sagebionetworks.web.server.servlet.SynapseClientImpl;
+import org.sagebionetworks.web.server.servlet.SynapseProvider;
 import org.sagebionetworks.web.server.servlet.filter.CrawlFilter;
 import org.sagebionetworks.web.server.servlet.filter.HtmlInjectionFilter;
 import org.sagebionetworks.web.shared.exceptions.RestServiceException;
@@ -66,7 +62,7 @@ public class HtmlInjectionFilterTest {
   ArgumentCaptor<String> stringCaptor;
 
   @Mock
-  SynapseClientImpl mockSynapseClient;
+  SynapseClient mockSynapseClient;
 
   @Mock
   DiscussionForumClientImpl mockDiscussionForumClient;
@@ -92,6 +88,9 @@ public class HtmlInjectionFilterTest {
   @Mock
   CrawlFilter mockCrawlFilter;
 
+  @Mock
+  SynapseProvider mockSynapseProvider;
+
   private Template getTemplate(String html)
     throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException {
     Configuration cfg = new Configuration(Configuration.VERSION_2_3_31);
@@ -102,19 +101,30 @@ public class HtmlInjectionFilterTest {
   }
 
   @Before
-  public void setUp() throws RestServiceException, IOException {
+  public void setUp()
+    throws RestServiceException, IOException, SynapseException {
     filter = new HtmlInjectionFilter();
     Template pageTitleTemplate = getTemplate(
-      "${" + HtmlInjectionFilter.PAGE_TITLE_KEY + "}"
+      "${" +
+      HtmlInjectionFilter.PAGE_TITLE_KEY +
+      "} \n${" +
+      HtmlInjectionFilter.PAGE_DESCRIPTION_KEY +
+      "} \n${" +
+      HtmlInjectionFilter.BOT_BODY_HTML_KEY +
+      "} \n${" +
+      HtmlInjectionFilter.BOT_HEAD_HTML_KEY +
+      "}"
     );
     filter.init(pageTitleTemplate, mockDiscussionForumClient, mockCrawlFilter);
+    when(mockSynapseProvider.createNewClient()).thenReturn(mockSynapseClient);
+    filter.setSynapseProvider(mockSynapseProvider);
     when(mockRequest.getHeader(ORIGIN_HEADER))
       .thenReturn("https://www" + SYNAPSE_ORG_SUFFIX);
     when(mockRequest.getServerName()).thenReturn("www" + SYNAPSE_ORG_SUFFIX);
     when(mockRequest.getScheme()).thenReturn("https");
     when(mockRequest.getHeader("User-Agent")).thenReturn("Googlebot/2.1");
     when(
-      mockSynapseClient.getEntityBundle(
+      mockSynapseClient.getEntityBundleV2(
         anyString(),
         any(EntityBundleRequest.class)
       )
@@ -127,53 +137,28 @@ public class HtmlInjectionFilterTest {
     when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
   }
 
+  private void setRequestURL(String s) {
+    StringBuffer sb = new StringBuffer();
+    sb.append(s);
+    when(mockRequest.getRequestURL()).thenReturn(sb);
+    when(mockRequest.getRequestURI()).thenReturn(sb.toString());
+  }
+
   @Test
   public void testSynapseEntityPage()
-    throws ServletException, IOException, RestServiceException {
+    throws ServletException, IOException, RestServiceException, SynapseException {
     String synapseID = "syn12345";
     String entityName = "my mock entity";
     when(mockEntity.getId()).thenReturn(synapseID);
     when(mockEntity.getName()).thenReturn(entityName);
-    when(mockRequest.getRequestURI()).thenReturn("/Synapse:" + synapseID);
+    setRequestURL("https://www.synapse.org/Synapse:" + synapseID);
 
     filter.testFilter(mockRequest, mockResponse, mockFilterChain);
 
-    verify(mockPrintWriter).println(stringCaptor.capture());
+    verify(mockPrintWriter).print(stringCaptor.capture());
     String outputString = stringCaptor.getValue();
-    assertTrue(outputString.contains(synapseID));
     assertTrue(outputString.contains(entityName));
     assertFalse(outputString.contains(META_ROBOTS_NOINDEX));
-
-    // verify we are asking for all entity types, except link
-    verify(mockSynapseClient)
-      .getEntityChildren(entityChildrenRequestCaptor.capture());
-    EntityChildrenRequest entityChildrenRequest =
-      entityChildrenRequestCaptor.getValue();
-    List<EntityType> entityTypes = entityChildrenRequest.getIncludeTypes();
-    for (EntityType type : EntityType.values()) {
-      assertEquals(EntityType.link != type, entityTypes.contains(type));
-    }
-  }
-
-  @Test
-  public void testSynapseEntityPageNoIndex()
-    throws ServletException, IOException {
-    String synapseID = "syn12345";
-    when(mockEntity.getId()).thenReturn(synapseID);
-    when(mockRequest.getRequestURI()).thenReturn("/Synapse:" + synapseID);
-    Map<String, AnnotationsValue> annotationsMap = new HashMap<
-      String,
-      AnnotationsValue
-    >();
-    when(mockAnnotations.getAnnotations()).thenReturn(annotationsMap);
-    annotationsMap.put("noindex", new AnnotationsValue());
-
-    filter.testFilter(mockRequest, mockResponse, mockFilterChain);
-
-    verify(mockPrintWriter).println(stringCaptor.capture());
-    String outputString = stringCaptor.getValue();
-    assertTrue(outputString.contains(synapseID));
-    assertTrue(outputString.contains(META_ROBOTS_NOINDEX));
   }
 
   @Test
@@ -183,7 +168,7 @@ public class HtmlInjectionFilterTest {
       .thenReturn(
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
       );
-    when(mockRequest.getRequestURI()).thenReturn("/Synapse:syn123");
+    setRequestURL("https://www.synapse.org/Synapse:syn123");
 
     filter.testFilter(mockRequest, mockResponse, mockFilterChain);
 
