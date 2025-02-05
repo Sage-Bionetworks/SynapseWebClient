ee4f29bf70ad4859aca9f6c1a6fba32f068651c5
afwillia <anthony.williams@sagebase.org>
2025-02-05 09:51:26 -0800

    Reuse click handlers for project and team searches

--- src/main/java/org/sagebionetworks/web/client/view/ProfileViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/view/ProfileViewImpl.java
@@ -270,18 +270,15 @@ public class ProfileViewImpl extends Composite implements ProfileView {
       }
     });
 
-    teamSearchButton.addClickHandler(event ->
-      presenter.goTo(new TeamSearch(teamSearchTextBox.getValue()))
-    );
-    teamSearchButtonSmall.addClickHandler(event ->
-      presenter.goTo(new TeamSearch(teamSearchTextBox.getValue()))
-    );
-    projectSearchButton.addClickHandler(event ->
-      presenter.goTo(new Search(getCurrentProjectSearchJSON()))
-    );
-    projectSearchButtonSmall.addClickHandler(event ->
-      presenter.goTo(new Search(getCurrentProjectSearchJSON()))
-    );
+    ClickHandler teamSearchHandler = event ->
+      presenter.goTo(new TeamSearch(teamSearchTextBox.getValue()));
+    teamSearchButton.addClickHandler(teamSearchHandler);
+    teamSearchButtonSmall.addClickHandler(teamSearchHandler);
+
+    ClickHandler projectSearchHandler = event ->
+      presenter.goTo(new Search(projectSearchTextBox.getValue()));
+    projectSearchButton.addClickHandler(projectSearchHandler);
+    projectSearchButtonSmall.addClickHandler(projectSearchHandler);
 
     moreChallengesButton.addClickHandler(event -> presenter.getMoreChallenges()
     );
