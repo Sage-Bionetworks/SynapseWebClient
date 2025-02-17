63a2a0aa4115a594ee4c403ce58f0f7efd621172
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-02-17 10:16:05 -0500

    SWC-6754
    - VersionsServlet cache now uses request origin as a cache key, preventing cache poisoning.
    - Use DI, introduce StackVersionProvider to improve testability

---
+++ src/test/java/org/sagebionetworks/web/server/servlet/StackVersionProviderImplTest.java
@@ -0,0 +1,72 @@
+package org.sagebionetworks.web.server.servlet;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.sagebionetworks.client.SynapseClient;
+import org.sagebionetworks.client.exceptions.SynapseException;
+import org.sagebionetworks.repo.model.versionInfo.SynapseVersionInfo;
+import org.sagebionetworks.web.shared.exceptions.RestServiceException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StackVersionProviderImplTest {
+
+  @Mock
+  SynapseProvider mockSynapseProvider;
+
+  @Mock
+  SynapseClient mockProdSynapseClient;
+
+  @Mock
+  SynapseClient mockStagingSynapseClient;
+
+  @Mock
+  SynapseVersionInfo mockSynapseVersionInfo;
+
+  StackVersionProviderImpl stackVersionProviderImpl;
+
+  @Before
+  public void setUp() throws SynapseException {
+    when(mockSynapseProvider.createNewClient("www.synapse.org"))
+      .thenReturn(mockProdSynapseClient);
+    when(mockSynapseProvider.createNewClient("staging.synapse.org"))
+      .thenReturn(mockStagingSynapseClient);
+    when(mockProdSynapseClient.getVersionInfo())
+      .thenReturn(mockSynapseVersionInfo);
+    when(mockStagingSynapseClient.getVersionInfo())
+      .thenReturn(mockSynapseVersionInfo);
+    when(mockSynapseVersionInfo.getVersion()).thenReturn("mockSynapseVersion");
+
+    stackVersionProviderImpl =
+      new StackVersionProviderImpl(mockSynapseProvider);
+  }
+
+  @Test
+  public void testGetVersion() throws RestServiceException, SynapseException {
+    String result = stackVersionProviderImpl.get("www.synapse.org");
+    assertEquals("develop-SNAPSHOT,mockSynapseVersion", result);
+
+    verify(mockSynapseProvider).createNewClient("www.synapse.org");
+    verify(mockProdSynapseClient).getVersionInfo();
+
+    // Call again, make sure cache is used
+    stackVersionProviderImpl.get("www.synapse.org");
+
+    verify(mockSynapseProvider, times(1)).createNewClient("www.synapse.org");
+    verify(mockProdSynapseClient, times(1)).getVersionInfo();
+
+    // Call again with a different origin, cache should NOT be used
+    stackVersionProviderImpl.get("staging.synapse.org");
+
+    verify(mockSynapseProvider, times(1))
+      .createNewClient("staging.synapse.org");
+    verify(mockStagingSynapseClient, times(1)).getVersionInfo();
+  }
+}
