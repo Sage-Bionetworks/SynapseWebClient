5fbf184e3237578cfa971cbf97c1f5357e66e6f0
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-03-12 16:03:43 -0400

    SWC-7258
    - Collect custom analytic events for entity search

---
+++ src/main/java/org/sagebionetworks/web/client/jsinterop/analytics/SearchQueryEventData.java
@@ -0,0 +1,28 @@
+package org.sagebionetworks.web.client.jsinterop.analytics;
+
+import jsinterop.annotations.JsNullable;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+public class SearchQueryEventData {
+
+  /** The search term that the user entered */
+  public String query_term;
+  /** The context in which the user performed the search */
+  public String search_context;
+
+  /** Serialized boolean filter(s) that may have been applied along with the query_term */
+  @JsNullable
+  public String serialized_boolean_query;
+
+  /** Serialized range filter(s) that may have been applied along with the query_term */
+  @JsNullable
+  public String serialized_range_query;
+
+  /** The offset of the first result of the page. 0-indexed */
+  public Double start_index;
+
+  /** The page number of the displayed search results. 1-indexed */
+  public Double page_index;
+}
--- src/test/java/org/sagebionetworks/web/unitclient/presenter/SearchPresenterTest.java
+++ src/test/java/org/sagebionetworks/web/unitclient/presenter/SearchPresenterTest.java
@@ -5,22 +5,29 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.sagebionetworks.repo.model.search.Hit;
+import org.sagebionetworks.repo.model.search.SearchResults;
 import org.sagebionetworks.repo.model.search.query.KeyRange;
 import org.sagebionetworks.repo.model.search.query.KeyValue;
 import org.sagebionetworks.repo.model.search.query.SearchQuery;
@@ -29,13 +36,19 @@ import org.sagebionetworks.schema.adapter.org.json.JSONObjectAdapterImpl;
 import org.sagebionetworks.web.client.ClientProperties;
 import org.sagebionetworks.web.client.GlobalApplicationState;
 import org.sagebionetworks.web.client.PlaceChanger;
+import org.sagebionetworks.web.client.SynapseJSNIUtils;
 import org.sagebionetworks.web.client.SynapseJavascriptClient;
+import org.sagebionetworks.web.client.analytics.SearchAnalyticsClient;
+import org.sagebionetworks.web.client.jsinterop.analytics.SearchQueryEventData;
+import org.sagebionetworks.web.client.jsinterop.analytics.SearchResultEventData;
+import org.sagebionetworks.web.client.jsinterop.analytics.SearchResultPageReturnedEventData;
 import org.sagebionetworks.web.client.place.PeopleSearch;
 import org.sagebionetworks.web.client.place.Search;
 import org.sagebionetworks.web.client.place.Synapse;
 import org.sagebionetworks.web.client.presenter.SearchPresenter;
 import org.sagebionetworks.web.client.presenter.SearchUtil;
 import org.sagebionetworks.web.client.security.AuthenticationController;
+import org.sagebionetworks.web.client.utils.Callback;
 import org.sagebionetworks.web.client.view.SearchView;
 import org.sagebionetworks.web.client.widget.LoadMoreWidgetContainer;
 import org.sagebionetworks.web.client.widget.entity.controller.SynapseAlert;
@@ -69,8 +82,33 @@ public class SearchPresenterTest {
   @Mock
   LoadMoreWidgetContainer mockLoadMoreWidgetContainer;
 
+  @Mock
+  SearchAnalyticsClient mockSearchAnalyticsClient;
+
+  @Mock
+  SynapseJSNIUtils mockJsniUtils;
+
+  @Mock
+  ClickEvent mockClickEvent;
+
+  @Captor
+  ArgumentCaptor<Callback> loadMoreCallbackCaptor;
+
+  @Captor
+  ArgumentCaptor<AsyncCallback> asyncCallbackCaptor;
+
+  @Captor
+  ArgumentCaptor<SearchQueryEventData> searchQuerySubmittedEventDataCaptor;
+
+  @Captor
+  ArgumentCaptor<
+    SearchResultPageReturnedEventData
+  > searchResultPageReturnedEventDataCaptor;
+
+  @Captor
+  ArgumentCaptor<SearchResultEventData> searchResultEventDataArgumentCaptor;
+
   String exampleTerm;
-  String exampleTermSearchQueryJson;
   SearchQuery exampleTermSearchQuery;
 
   @Before
@@ -86,7 +124,9 @@ public class SearchPresenterTest {
         mockJsClient,
         new JSONObjectAdapterImpl(),
         mockSynAlert,
-        mockLoadMoreWidgetContainer
+        mockLoadMoreWidgetContainer,
+        mockSearchAnalyticsClient,
+        mockJsniUtils
       );
 
     exampleTerm = "searchQueryTerm";
@@ -94,10 +134,12 @@ public class SearchPresenterTest {
     exampleTermSearchQuery.setQueryTerm(
       Arrays.asList(new String[] { exampleTerm })
     );
-    exampleTermSearchQueryJson =
-      exampleTermSearchQuery
-        .writeToJSONObject(jsonObjectAdapter.createNew())
-        .toJSONString();
+  }
+
+  private String getTermSearchQueryJson(SearchQuery query) throws Exception {
+    return query
+      .writeToJSONObject(jsonObjectAdapter.createNew())
+      .toJSONString();
   }
 
   @Test
@@ -132,10 +174,246 @@ public class SearchPresenterTest {
   @SuppressWarnings("unchecked")
   @Test
   public void testSearchQuery() throws Exception {
-    Search place = new Search(exampleTermSearchQueryJson);
+    // Use a smaller initial page size to simplify test setup
+    exampleTermSearchQuery.setSize(2L);
+
+    Search place = new Search(getTermSearchQueryJson(exampleTermSearchQuery));
+    searchPresenter.setPlace(place);
+
+    verify(mockJsClient)
+      .getSearchResults(
+        eq(exampleTermSearchQuery),
+        asyncCallbackCaptor.capture()
+      );
+
+    verify(mockSearchAnalyticsClient)
+      .sendSearchQuerySubmittedEvent(
+        searchQuerySubmittedEventDataCaptor.capture()
+      );
+
+    SearchQueryEventData searchSubmittedEventData =
+      searchQuerySubmittedEventDataCaptor.getValue();
+    assertEquals("searchQueryTerm", searchSubmittedEventData.query_term);
+    assertEquals("synapse_entity", searchSubmittedEventData.search_context);
+    assertEquals(null, searchSubmittedEventData.serialized_boolean_query);
+    assertEquals(null, searchSubmittedEventData.serialized_range_query);
+    assertEquals(Double.valueOf(1), searchSubmittedEventData.page_index);
+    assertEquals(Double.valueOf(0), searchSubmittedEventData.start_index);
+
+    // Simulate first page of results
+    List<Hit> hits = new ArrayList<Hit>();
+    hits.add(new Hit());
+    hits.get(0).setId("syn123");
+    hits.get(0).setName("name1");
+    hits.add(new Hit());
+    hits.get(1).setId("syn456");
+    hits.get(1).setName("name2");
+    SearchResults searchResults = new SearchResults();
+    searchResults.setFound(3L);
+    searchResults.setStart(0L);
+    searchResults.setHits(hits);
+    asyncCallbackCaptor.getValue().onSuccess(searchResults);
+
+    verify(mockView).setSearchResults(eq(searchResults), eq(exampleTerm));
+    verify(mockSearchAnalyticsClient)
+      .sendSearchResultPageReturnedEvent(
+        searchResultPageReturnedEventDataCaptor.capture()
+      );
+
+    SearchResultPageReturnedEventData pageReturnedEventData =
+      searchResultPageReturnedEventDataCaptor.getValue();
+
+    assertEquals("searchQueryTerm", pageReturnedEventData.query_term);
+    assertEquals("synapse_entity", pageReturnedEventData.search_context);
+    assertNull(pageReturnedEventData.serialized_boolean_query);
+    assertNull(pageReturnedEventData.serialized_range_query);
+    assertEquals(Double.valueOf(1), pageReturnedEventData.page_index);
+    assertEquals(Double.valueOf(0), pageReturnedEventData.start_index);
+    assertEquals(Double.valueOf(3), pageReturnedEventData.total_results);
+
+    verify(mockSearchAnalyticsClient, times(2))
+      .sendSearchResultReturnedEvent(
+        searchResultEventDataArgumentCaptor.capture()
+      );
+
+    List<SearchResultEventData> resultReturnedEventData =
+      searchResultEventDataArgumentCaptor.getAllValues();
+
+    resultReturnedEventData.forEach(eventData -> {
+      assertEquals("searchQueryTerm", eventData.query_term);
+      assertEquals("synapse_entity", eventData.search_context);
+      assertNull(eventData.serialized_boolean_query);
+      assertNull(eventData.serialized_range_query);
+
+      assertEquals("entity", eventData.item_type);
+      assertEquals(Double.valueOf(1), eventData.page_index);
+    });
+
+    assertEquals("syn123", resultReturnedEventData.get(0).item_id);
+    assertEquals(Double.valueOf(1), resultReturnedEventData.get(0).rank);
+    assertEquals("syn456", resultReturnedEventData.get(1).item_id);
+    assertEquals(Double.valueOf(2), resultReturnedEventData.get(1).rank);
+
+    // Request a second page of results
+    verify(mockLoadMoreWidgetContainer)
+      .configure(loadMoreCallbackCaptor.capture());
+    loadMoreCallbackCaptor.getValue().invoke();
+
+    exampleTermSearchQuery.setStart(2L);
+    verify(mockJsClient, times(2))
+      .getSearchResults(
+        eq(exampleTermSearchQuery),
+        asyncCallbackCaptor.capture()
+      );
+
+    verify(mockSearchAnalyticsClient, times(2))
+      .sendSearchQuerySubmittedEvent(
+        searchQuerySubmittedEventDataCaptor.capture()
+      );
+
+    searchSubmittedEventData = searchQuerySubmittedEventDataCaptor.getValue();
+    assertEquals("searchQueryTerm", searchSubmittedEventData.query_term);
+    assertEquals("synapse_entity", searchSubmittedEventData.search_context);
+    assertEquals(null, searchSubmittedEventData.serialized_boolean_query);
+    assertEquals(null, searchSubmittedEventData.serialized_range_query);
+    assertEquals(Double.valueOf(2), searchSubmittedEventData.page_index);
+    assertEquals(Double.valueOf(2), searchSubmittedEventData.start_index);
+
+    // Second page of results is returned
+    List<Hit> hitsPage2 = Collections.singletonList(new Hit());
+    hitsPage2.get(0).setId("syn789");
+    hitsPage2.get(0).setName("name3");
+    SearchResults searchResultsPage2 = new SearchResults();
+    searchResultsPage2.setFound(3L);
+    searchResultsPage2.setStart(2L);
+    searchResultsPage2.setHits(hitsPage2);
+
+    asyncCallbackCaptor.getValue().onSuccess(searchResultsPage2);
+
+    // Verify analytics events for the 2nd page were sent
+    verify(mockSearchAnalyticsClient, times(2))
+      .sendSearchResultPageReturnedEvent(
+        searchResultPageReturnedEventDataCaptor.capture()
+      );
+    verify(mockSearchAnalyticsClient, times(3))
+      .sendSearchResultReturnedEvent(
+        searchResultEventDataArgumentCaptor.capture()
+      );
+
+    pageReturnedEventData = searchResultPageReturnedEventDataCaptor.getValue();
+
+    assertEquals("searchQueryTerm", pageReturnedEventData.query_term);
+    assertEquals("synapse_entity", pageReturnedEventData.search_context);
+    assertNull(pageReturnedEventData.serialized_boolean_query);
+    assertNull(pageReturnedEventData.serialized_range_query);
+    assertEquals(Double.valueOf(2), pageReturnedEventData.page_index);
+    assertEquals(Double.valueOf(2), pageReturnedEventData.start_index);
+    assertEquals(Double.valueOf(3), pageReturnedEventData.total_results);
+
+    SearchResultEventData lastResultReturnedEventData =
+      searchResultEventDataArgumentCaptor.getValue();
+
+    assertEquals("searchQueryTerm", lastResultReturnedEventData.query_term);
+    assertEquals("synapse_entity", lastResultReturnedEventData.search_context);
+    assertNull(lastResultReturnedEventData.serialized_boolean_query);
+    assertNull(lastResultReturnedEventData.serialized_range_query);
+    assertEquals("syn789", lastResultReturnedEventData.item_id);
+    assertEquals(Double.valueOf(3), lastResultReturnedEventData.rank);
+    assertEquals("entity", lastResultReturnedEventData.item_type);
+    assertEquals(Double.valueOf(2), lastResultReturnedEventData.page_index);
+
+    // Verify that clicking the results sends expected events
+
+    searchPresenter
+      .getSearchResultClickedHandler(hits.get(0))
+      .onClick(mockClickEvent);
+
+    verify(mockSearchAnalyticsClient)
+      .sendSearchResultClickedEvent(
+        searchResultEventDataArgumentCaptor.capture()
+      );
+
+    SearchResultEventData clickedEventData =
+      searchResultEventDataArgumentCaptor.getValue();
+    assertEquals("searchQueryTerm", clickedEventData.query_term);
+    assertEquals("synapse_entity", clickedEventData.search_context);
+    assertEquals("entity", clickedEventData.item_type);
+    assertEquals("syn123", clickedEventData.item_id);
+    assertEquals(Double.valueOf(1), clickedEventData.rank);
+    assertEquals(Double.valueOf(1), clickedEventData.page_index);
+
+    searchPresenter
+      .getSearchResultClickedHandler(hitsPage2.get(0))
+      .onClick(mockClickEvent);
+
+    verify(mockSearchAnalyticsClient, times(2))
+      .sendSearchResultClickedEvent(
+        searchResultEventDataArgumentCaptor.capture()
+      );
+
+    clickedEventData = searchResultEventDataArgumentCaptor.getValue();
+    assertEquals("searchQueryTerm", clickedEventData.query_term);
+    assertEquals("synapse_entity", clickedEventData.search_context);
+    assertEquals("entity", clickedEventData.item_type);
+    assertEquals("syn789", clickedEventData.item_id);
+    assertEquals(Double.valueOf(3), clickedEventData.rank);
+    assertEquals(Double.valueOf(2), clickedEventData.page_index);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testSearchQueryEmptyFirstPage() throws Exception {
+    Search place = new Search(getTermSearchQueryJson(exampleTermSearchQuery));
     searchPresenter.setPlace(place);
     verify(mockJsClient)
-      .getSearchResults(eq(exampleTermSearchQuery), any(AsyncCallback.class));
+      .getSearchResults(
+        eq(exampleTermSearchQuery),
+        asyncCallbackCaptor.capture()
+      );
+
+    verify(mockSearchAnalyticsClient)
+      .sendSearchQuerySubmittedEvent(
+        searchQuerySubmittedEventDataCaptor.capture()
+      );
+
+    SearchQueryEventData searchSubmittedEventData =
+      searchQuerySubmittedEventDataCaptor.getValue();
+    assertEquals("searchQueryTerm", searchSubmittedEventData.query_term);
+    assertEquals("synapse_entity", searchSubmittedEventData.search_context);
+    assertEquals(null, searchSubmittedEventData.serialized_boolean_query);
+    assertEquals(null, searchSubmittedEventData.serialized_range_query);
+    assertEquals(Double.valueOf(1), searchSubmittedEventData.page_index);
+    assertEquals(Double.valueOf(0), searchSubmittedEventData.start_index);
+
+    // Simulate first page of results
+    SearchResults searchResults = new SearchResults();
+    searchResults.setHits(new ArrayList<Hit>());
+    searchResults.setFound(0L);
+    searchResults.setStart(0L);
+    asyncCallbackCaptor.getValue().onSuccess(searchResults);
+
+    // Verify page return analytics event is sent, but there are no individual result events
+
+    verify(mockSearchAnalyticsClient)
+      .sendSearchResultPageReturnedEvent(
+        searchResultPageReturnedEventDataCaptor.capture()
+      );
+
+    SearchResultPageReturnedEventData pageReturnedEventData =
+      searchResultPageReturnedEventDataCaptor.getValue();
+
+    assertEquals("searchQueryTerm", pageReturnedEventData.query_term);
+    assertEquals("synapse_entity", pageReturnedEventData.search_context);
+    assertNull(pageReturnedEventData.serialized_boolean_query);
+    assertNull(pageReturnedEventData.serialized_range_query);
+    assertEquals(Double.valueOf(1), pageReturnedEventData.page_index);
+    assertEquals(Double.valueOf(0), pageReturnedEventData.start_index);
+    assertEquals(Double.valueOf(0), pageReturnedEventData.total_results);
+
+    verify(mockSearchAnalyticsClient, never())
+      .sendSearchResultReturnedEvent(
+        searchResultEventDataArgumentCaptor.capture()
+      );
   }
 
   @SuppressWarnings("unchecked")
@@ -213,6 +491,16 @@ public class SearchPresenterTest {
     verify(mockLoadMoreWidgetContainer).setIsProcessing(true); // 4
     verify(mockJsClient)
       .getSearchResults(any(SearchQuery.class), any(AsyncCallback.class)); // 4
+    verify(mockSearchAnalyticsClient)
+      .sendSearchQuerySubmittedEvent(
+        searchQuerySubmittedEventDataCaptor.capture()
+      );
+    SearchQueryEventData eventData =
+      searchQuerySubmittedEventDataCaptor.getValue();
+    assertEquals(
+      "[{\"key\":\"createdOn\",\"min\":\"1\"}]",
+      eventData.serialized_range_query
+    );
 
     // verify setting the time facet to another value clears the previous
     facetValue = "2";
@@ -229,6 +517,16 @@ public class SearchPresenterTest {
     verify(mockJsClient, times(2))
       .getSearchResults(any(SearchQuery.class), any(AsyncCallback.class)); // 4
     assertTrue(getTimeFacet(facetName).isEmpty());
+
+    verify(mockSearchAnalyticsClient, times(2))
+      .sendSearchQuerySubmittedEvent(
+        searchQuerySubmittedEventDataCaptor.capture()
+      );
+    eventData = searchQuerySubmittedEventDataCaptor.getValue();
+    assertEquals(
+      "[{\"key\":\"createdOn\",\"min\":\"2\"}]",
+      eventData.serialized_range_query
+    );
   }
 
   @Test
@@ -244,6 +542,9 @@ public class SearchPresenterTest {
     searchPresenter.setPlace(new Search(searchQueryJson));
 
     verify(mockPlaceChanger).goTo(new Synapse(term));
+
+    verify(mockSearchAnalyticsClient, never())
+      .sendSearchQuerySubmittedEvent(any());
   }
 
   @Test
@@ -255,5 +556,15 @@ public class SearchPresenterTest {
     );
     verify(mockJsClient)
       .getSearchResults(eq(expectedQuery), any(AsyncCallback.class));
+  }
+
+  @Test
+  public void testGetSearchResultClickedHandler() throws Exception {
+    Search place = new Search(getTermSearchQueryJson(exampleTermSearchQuery));
+    searchPresenter.setPlace(place);
+
+    Hit hit = new Hit();
+    hit.setId("syn123");
+    hit.setName("name1");
   }
 }
