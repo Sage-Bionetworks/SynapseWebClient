bb28c06f0f9a4c6be740e0a936a5d9dacd071501
“kianamcc” <user.email=kianamccu@gmail.com>
2025-05-21 17:01:39 -0700

    cleanup, remove feature flag

--- src/main/java/org/sagebionetworks/web/client/widget/entity/browse/FilesBrowserViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/browse/FilesBrowserViewImpl.java
@@ -1,6 +1,5 @@
 package org.sagebionetworks.web.client.widget.entity.browse;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.IsWidget;
@@ -10,14 +9,8 @@ import org.gwtbootstrap3.client.ui.Heading;
 import org.gwtbootstrap3.client.ui.html.Div;
 import org.sagebionetworks.web.client.DisplayUtils;
 import org.sagebionetworks.web.client.FeatureFlagConfig;
-import org.sagebionetworks.web.client.FeatureFlagKey;
 import org.sagebionetworks.web.client.context.SynapseReactClientFullContextPropsProvider;
-import org.sagebionetworks.web.client.jsinterop.EntityFileBrowserProps;
-import org.sagebionetworks.web.client.jsinterop.React;
-import org.sagebionetworks.web.client.jsinterop.ReactElement;
-import org.sagebionetworks.web.client.jsinterop.SRC;
 import org.sagebionetworks.web.client.utils.CallbackP;
-import org.sagebionetworks.web.client.widget.ReactComponent;
 
 public class FilesBrowserViewImpl implements FilesBrowserView {
 
@@ -27,7 +20,6 @@ public class FilesBrowserViewImpl implements FilesBrowserView {
   private EntityTreeBrowser entityTreeBrowser;
   private Widget widget;
   SynapseReactClientFullContextPropsProvider propsProvider;
-  private FeatureFlagConfig featureFlagConfig;
 
   @UiField
   Div files;
@@ -44,9 +36,6 @@ public class FilesBrowserViewImpl implements FilesBrowserView {
   @UiField
   Heading title;
 
-  @UiField
-  Div reactFileBrowserContainer;
-
   CallbackP<String> entityClickedCallback;
 
   @Inject
@@ -57,7 +46,6 @@ public class FilesBrowserViewImpl implements FilesBrowserView {
     FeatureFlagConfig featureFlagConfig
   ) {
     widget = binder.createAndBindUi(this);
-    this.featureFlagConfig = featureFlagConfig;
     this.propsProvider = propsProvider;
     this.entityTreeBrowser = entityTreeBrowser;
     Widget etbW = entityTreeBrowser.asWidget();
@@ -67,38 +55,9 @@ public class FilesBrowserViewImpl implements FilesBrowserView {
 
   @Override
   public void configure(String entityId) {
-    boolean isReactFileBrowser = featureFlagConfig.isFeatureEnabled(
-      FeatureFlagKey.REACT_FILE_BROWSER
-    );
     title.setVisible(false);
-    reactFileBrowserContainer.setVisible(isReactFileBrowser);
-    files.setVisible(!isReactFileBrowser);
-    if (isReactFileBrowser) {
-      rerenderFileBrowser(entityId);
-    } else {
-      entityTreeBrowser.configure(entityId);
-    }
-  }
-
-  public void rerenderFileBrowser(String parentContainerId) {
-    // TODO: Should not need to remount, but currently the parentContainer is not shown if the existing react component is updated
-    // TODO: Bug: The tree is not always auto-expanding (to the path to the parent container Id)
-    reactFileBrowserContainer.clear();
-    ReactComponent componentContainer = new ReactComponent();
-    EntityFileBrowserProps props = EntityFileBrowserProps.create(
-      parentContainerId,
-      ref -> {
-        String entityId = ref.getTargetId();
-        entityClickedCallback.invoke(entityId);
-      }
-    );
-    ReactElement component = React.createElementWithSynapseContext(
-      SRC.SynapseComponents.EntityFileBrowser,
-      props,
-      propsProvider.getJsInteropContextProps()
-    );
-    reactFileBrowserContainer.add(componentContainer);
-    componentContainer.render(component);
+    files.setVisible(true);
+    entityTreeBrowser.configure(entityId);
   }
 
   @Override
