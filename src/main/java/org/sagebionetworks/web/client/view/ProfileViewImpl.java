280d5db54cbe06af5eafdef8dd37a9cb4fd0c0ac
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-07-16 16:29:27 -0400

    SWC-7004
    Proof of concept MUI button using globally-provided theme context

--- src/main/java/org/sagebionetworks/web/client/view/ProfileViewImpl.java
+++ src/main/java/org/sagebionetworks/web/client/view/ProfileViewImpl.java
@@ -130,7 +130,7 @@ public class ProfileViewImpl extends Composite implements ProfileView {
   TextBox projectSearchTextBox;
 
   @UiField
-  Button createProjectButton;
+  org.sagebionetworks.web.client.jsinterop.mui.Button createProjectButton;
 
   @UiField
   DivElement createProjectUI;
