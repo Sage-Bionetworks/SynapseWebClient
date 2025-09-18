0f8766cdc0a3349d4a1faa9b53433db7a7db768f
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-09-18 09:09:19 -0400

    SWC-7467
    - Add comment to ReactRef/ReactRefCallback types

--- src/main/java/org/sagebionetworks/web/client/jsinterop/ReactRefCallback.java
+++ src/main/java/org/sagebionetworks/web/client/jsinterop/ReactRefCallback.java
@@ -2,6 +2,15 @@ package org.sagebionetworks.web.client.jsinterop;
 
 import jsinterop.annotations.JsFunction;
 
+/**
+ * A React Ref callback, which is invoked with the referenced node when it changes.
+ *
+ * While this functional interface represents a ReactRef, this Java interface does not extend the {@link ReactRef}
+ * interface because of limitations in JsInterop.
+ *
+ * For more information about ref callbacks, see: https://react.dev/reference/react-dom/components/common#ref-callback
+ * @param <T>
+ */
 @JsFunction
 public interface ReactRefCallback<T> {
   void run(T node);
