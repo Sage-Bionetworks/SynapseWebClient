00a7b4c1f5956a848522c51bc1a01032e2eccfc0
Kiana <kianamccu@gmail.com>
2025-10-27 16:04:41 -0700

    Merge pull request #5800 from kianamcc/SWC-7515
    SWC-7515: Integrate "Share this page" dialog in Synapse.org entity page

---
+++ src/main/java/org/sagebionetworks/web/client/widget/ShareThisPageImpl.java
@@ -0,0 +1,34 @@
+package org.sagebionetworks.web.client.widget;
+
+import com.google.inject.Inject;
+import org.sagebionetworks.web.client.jsinterop.React;
+import org.sagebionetworks.web.client.jsinterop.ReactElement;
+import org.sagebionetworks.web.client.jsinterop.SRC;
+import org.sagebionetworks.web.client.jsinterop.ShareThisPageProps;
+import org.sagebionetworks.web.client.jsinterop.ShareThisPageProps.Callback;
+
+public class ShareThisPageImpl extends ReactComponent implements ShareThisPage {
+
+  @Inject
+  public ShareThisPageImpl() {}
+
+  public void configure(
+    String shortIoPublicApiKey,
+    String domain,
+    boolean open,
+    Callback onClose,
+    String triggerMode
+  ) {
+    ShareThisPageProps props = ShareThisPageProps.create();
+    props.setOpen(open);
+    props.setOnClose(onClose);
+    props.setShortIoPublicApiKey(shortIoPublicApiKey);
+    props.setDomain(domain);
+    props.setTriggerMode(triggerMode);
+    ReactElement component = React.createElementWithSynapseContext(
+      SRC.SynapseComponents.ShareThisPage,
+      props
+    );
+    this.render(component);
+  }
+}
