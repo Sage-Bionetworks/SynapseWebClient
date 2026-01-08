1a0a001c06f67ec254d98947ca57f343dc5dde9b
lakikowolfe <loren.wolfe@sagebase.org>
2026-01-08 12:44:47 -0800

    remove unnessisary redirect

--- src/main/java/org/sagebionetworks/web/client/place/SearchV2Place.java
+++ src/main/java/org/sagebionetworks/web/client/place/SearchV2Place.java
@@ -3,18 +3,12 @@ package org.sagebionetworks.web.client.place;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceTokenizer;
 import com.google.gwt.place.shared.Prefix;
-import com.google.gwt.user.client.Window;
 
 public class SearchV2Place extends Place {
 
   public static final String QUERY = "query";
 
-  public SearchV2Place(String token) {
-    if (token != null && !token.startsWith("default")) {
-      // Redirect /SearchV2:{token} to /SearchV2:default?query={token}
-      Window.Location.assign("/SearchV2:default?" + QUERY + "=" + token);
-    }
-  }
+  public SearchV2Place(String token) {}
 
   @Prefix("SearchV2")
   public static class Tokenizer implements PlaceTokenizer<SearchV2Place> {
