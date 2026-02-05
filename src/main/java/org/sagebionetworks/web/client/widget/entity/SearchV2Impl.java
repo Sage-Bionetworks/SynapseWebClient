bb4ae80e5a66600db41c0adb4a5c448b12ff51cd
“kianamcc” <user.email=kianamccu@gmail.com>
2026-02-05 11:02:29 -0800

    configure header for searchv2

--- src/main/java/org/sagebionetworks/web/client/widget/entity/SearchV2Impl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/SearchV2Impl.java
@@ -5,13 +5,19 @@ import org.sagebionetworks.web.client.jsinterop.React;
 import org.sagebionetworks.web.client.jsinterop.ReactElement;
 import org.sagebionetworks.web.client.jsinterop.SRC;
 import org.sagebionetworks.web.client.widget.ReactComponent;
+import org.sagebionetworks.web.client.widget.header.Header;
 
 public class SearchV2Impl extends ReactComponent {
 
+  private Header headerWidget;
+
   @Inject
-  public SearchV2Impl() {}
+  public SearchV2Impl(Header headerWidget) {
+    this.headerWidget = headerWidget;
+  }
 
   public void configure() {
+    headerWidget.configure();
     ReactElement component = React.createElementWithSynapseContext(
       SRC.SynapseComponents.SynapseSearchPage
     );
