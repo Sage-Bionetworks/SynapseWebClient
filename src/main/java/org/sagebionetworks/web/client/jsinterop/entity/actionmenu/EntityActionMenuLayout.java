a8320c7f97a3286100a58f450a9f90fcda7836e8
“kianamcc” <user.email=kianamccu@gmail.com>
2026-07-01 13:32:27 -0700

    titlebar wip

--- src/main/java/org/sagebionetworks/web/client/jsinterop/entity/actionmenu/EntityActionMenuLayout.java
+++ src/main/java/org/sagebionetworks/web/client/jsinterop/entity/actionmenu/EntityActionMenuLayout.java
@@ -16,8 +16,8 @@ public class EntityActionMenuLayout {
   /* Only strings that map to synapse-react-client Icons */
   private String primaryMenuEndIcon;
   private Object primaryMenuEndIconSx;
-
   private Object menuButtonSx;
+  private String downloadMenuVariant;
 
   private EntityActionMenuLayout() {
     // Private constructor to force using the JsOverlay create
@@ -84,5 +84,10 @@ public class EntityActionMenuLayout {
   @JsOverlay
   public final void setMenuButtonSx(Object menuButtonSx) {
     this.menuButtonSx = menuButtonSx;
+  }
+
+  @JsOverlay
+  public final void setDownloadMenuVariant(String downloadMenuVariant) {
+    this.downloadMenuVariant = downloadMenuVariant;
   }
 }
--- src/main/java/org/sagebionetworks/web/client/widget/entity/menu/v3/DefaultEntityActionMenuLayoutUtil.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/menu/v3/DefaultEntityActionMenuLayoutUtil.java
@@ -20,11 +20,14 @@ public class DefaultEntityActionMenuLayoutUtil {
   private static final String ICON_COLOR = "#1C1B1F";
   private static final String ICON_BORDER_COLOR = "#9EAAB7";
   private static final String ENTITY_MENU_BUTTON_COLOR = "#4D535A";
+  private static final String ENTITY_MENU_BUTTON_BACKGROUND_COLOR =
+    "rgba(255, 255, 255, 0.6)";
 
   static SxProps entityMenuButtonSx = SxProps
     .create()
     .setBorderColor(ICON_BORDER_COLOR)
-    .setColor(ENTITY_MENU_BUTTON_COLOR);
+    .setColor(ENTITY_MENU_BUTTON_COLOR)
+    .setBackgroundColor(ENTITY_MENU_BUTTON_BACKGROUND_COLOR);
 
   static SxProps entityMenuEndIconSx = SxProps.create().setColor(ICON_COLOR);
 
@@ -79,6 +82,7 @@ public class DefaultEntityActionMenuLayoutUtil {
           )
         );
         layout.setPrimaryMenuActions(Collections.emptyList());
+        layout.setDownloadMenuVariant("contained");
         layout.setDownloadMenuActions(
           Collections.singletonList(
             Arrays.asList(
@@ -87,6 +91,7 @@ public class DefaultEntityActionMenuLayoutUtil {
             )
           )
         );
+
         break;
       case DATASETS:
         layout.setPrimaryMenuText("New...");
@@ -268,6 +273,7 @@ public class DefaultEntityActionMenuLayoutUtil {
       case file:
       case recordset:
         layout.setPrimaryMenuText(TOOLS_SUFFIX);
+        layout.setDownloadMenuVariant("contained");
         layout.setButtonActions(
           Arrays.asList(
             ActionViewProps
